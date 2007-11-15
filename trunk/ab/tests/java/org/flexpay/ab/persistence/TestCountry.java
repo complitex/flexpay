c69921b473f72b043b01c88d634f35f0e38fc5f0
alex.chistyakov <alex.chistyakov@f554a4e9-133d-0410-ac35-5129da6e6cf8>
2007-11-15 09:45:58 +0000

    Тесты для объекта Country

---
+++ trunk/ab/tests/java/org/flexpay/ab/persistence/TestCountry.java
@@ -0,0 +1,23 @@
+package org.flexpay.ab.persistence;
+
+import junit.framework.TestCase;
+
+public class TestCountry extends TestCase {
+
+    public void testCountryName(){
+        Country country = new Country();
+        String lang = "english";
+        String longName = "Russia";
+        String shortName = "RUS";
+
+        country.setCountryName(longName,shortName,lang);
+        assertEquals(country.getCountryName(lang).getLongValue(),longName);
+        assertEquals(country.getCountryName(lang).getShortValue(),shortName);
+
+        country.setCountryName("","",lang);
+        assertEquals(country.getCountryName(lang).getLongValue(),"");
+        assertEquals(country.getCountryName(lang).getShortValue(),"");
+    }
+
+    
+}
