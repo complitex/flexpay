9b2c0e1b24940e35dceab61f707f75e092656fc7
alex.chistyakov <alex.chistyakov@f554a4e9-133d-0410-ac35-5129da6e6cf8>
2007-11-15 09:39:23 +0000

    Хелпер для тест кейсов

---
+++ trunk/common/tests/java/org/flexpay/common/test/TestHelper.java
@@ -0,0 +1,19 @@
+package org.flexpay.common.test;
+
+import java.lang.reflect.Field;
+
+public class TestHelper {
+    /**
+     * ћетод предназначен дл€ присвоени€ значени€ приватным свойствам объекта.
+     * @param o ќбъект
+     * @param fieldName им€ свойства объекта
+     * @param fieldValue значение свойства объекта
+     * @throws NoSuchFieldException когда свойство не найдено
+     * @throws IllegalAccessException когда права доступа не позвол€ют изменить свойство
+     */
+    public static void setPrivateField(Object o, String fieldName, Object fieldValue) throws NoSuchFieldException, IllegalAccessException {
+        Field field = o.getClass().getDeclaredField(fieldName);
+        field.setAccessible(true);
+        field.set(o, fieldValue);
+    }
+}
