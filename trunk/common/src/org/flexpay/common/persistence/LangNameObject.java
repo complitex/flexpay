27268cba66db6a08b16af565558f13a894b7e4e7
alex.chistyakov <alex.chistyakov@f554a4e9-133d-0410-ac35-5129da6e6cf8>
2007-11-17 07:05:24 +0000

    перенес в другой пакедж

---
+++ trunk/common/src/org/flexpay/common/persistence/LangNameObject.java
@@ -0,0 +1,39 @@
+package org.flexpay.common.persistence;
+
+import java.util.Map;
+import java.util.HashMap;
+
+
+public class LangNameObject {
+    private Map<String, LongShortValue> names = new HashMap<String, LongShortValue>();
+
+    /**
+     * Setter for country names
+     * @param names массив из коротких и длинных названий для имени.
+     */
+    public void setNames(Map<String, LongShortValue> names) {
+        this.names = names;
+    }
+
+    public LongShortValue getName(String lang){
+        LongShortValue lsv = this.names.get(lang);
+        if (lsv == null) lsv = new LongShortValue("","");
+        return lsv;
+    }
+
+    public void setName(String longName, String shortName, String lang){
+        if (lang == null || lang.length() == 0) {
+            throw new NullPointerException("setName: invalid lang : name=" + longName + " lang=" + lang);
+        }
+        if (longName !=null && longName.length()>0){
+            setName(new LongShortValue(longName,shortName),lang);
+        } else {
+            this.names.remove(lang);
+        }
+    }
+
+    public void setName(LongShortValue countryName, String lang){
+        this.names.put(lang,countryName);
+    }
+
+}
