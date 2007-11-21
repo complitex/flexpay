02b38120ae8e7875042d6665a14ef5adee57aa89
vladislavtsoy <vladislavtsoy@f554a4e9-133d-0410-ac35-5129da6e6cf8>
2007-11-21 04:57:31 +0000

    changed project structure and added security(testing version)

---
+++ trunk/ab/web/logout.jsp
@@ -0,0 +1,53 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN">
+
+<html>
+
+
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8"/>
+    <title>fp</title>
+    <link rel="stylesheet" type="text/css" href="style/fp.css"/>
+</head>
+
+<table border="1" cellpadding="0" cellspacing="0" border="0" width="100%">
+    <tr class="verytop">
+        <td><a href="/"><img src="img/logo.gif" width="123" height="37" alt="FlexPay" border="0" hspace="25"
+                                   vspace="6"/></a>
+        </td>
+        <%
+            if(request.getUserPrincipal() != null)
+            {
+                request.getSession().invalidate();
+            }
+        %>
+    </tr>
+</table>
+
+</br>
+
+<center>
+<form method="POST" action="j_security_check">
+    <table border="0">
+        <tr>
+            <td>Username:</td>
+            <td>
+                <input type="text" name="j_username"/>
+            </td>
+        </tr>
+        <tr>
+            <td>Password:</td>
+            <td>
+                <input type="password" name="j_password"/>
+            </td>
+        </tr>
+        <tr>
+            <td colspan="2" align="center">
+                <input type="submit" value="Submit"/>
+
+            </td>
+        </tr>
+    </table>
+</form>
+</center>
+
+</html>
---
+++ trunk/common/src/org/flexpay/common/struts/interceptors/FlexpayRolesInterceptor.java
@@ -0,0 +1,65 @@
+package org.flexpay.common.struts.interceptors;
+
+import com.opensymphony.xwork2.ActionInvocation;
+import org.apache.struts2.ServletActionContext;
+import org.apache.struts2.interceptor.RolesInterceptor;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: vlad
+ * Date: 21.11.2007
+ * Time: 10:28:04
+ */
+public class FlexpayRolesInterceptor extends RolesInterceptor {
+
+    private static final String DEFAULT_FORBIDDEN_URL = "/forbidden.jsp";
+    private static final String DEFAULT_LOGIN_URL = "/login.jsp";
+
+    private String forbiddenURL = null;
+    private String loginURL = null;
+
+    public void setForbiddenURL(String forbiddenURL) {
+        this.forbiddenURL = forbiddenURL;
+    }
+
+    public void setLoginURL(String loginURL) {
+        this.loginURL = loginURL;
+    }
+
+    public String intercept(ActionInvocation invocation) throws Exception {
+        HttpServletRequest request = ServletActionContext.getRequest();
+        HttpServletResponse response = ServletActionContext.getResponse();
+        String result = null;
+        if (request.getRemoteUser() == null) {
+            //redirectToLogin(invocation, request, response);
+        } else if (!isAllowed(request, invocation.getAction())) {
+            redirectToForbidden(invocation, request, response);
+        } else {
+            result = invocation.invoke();
+        }
+        return result;
+    }
+
+    /*protected void redirectToLogin(ActionInvocation invocation,
+                                   HttpServletRequest request,
+                                   HttpServletResponse response)
+            throws Exception {
+        String lastRequestedURL = request.getRequestURI();
+        if (lastRequestedURL != null && !"".equals(lastRequestedURL)) {
+            request.getSession().setAttribute(LastRequestFilter.LAST_REQUESTED_URL_ATTR_NAME, lastRequestedURL);
+        }
+        String redirecrURL = request.getContextPath() + (loginURL == null || "".equals(loginURL) ? DEFAULT_LOGIN_URL : loginURL);
+        response.sendRedirect(redirecrURL);
+    }*/
+
+    protected String redirectToForbidden(ActionInvocation invocation,
+                                         HttpServletRequest request,
+                                         HttpServletResponse response)
+            throws Exception {
+        response.sendRedirect(forbiddenURL == null || "".equals(forbiddenURL) ? request.getContextPath() + DEFAULT_FORBIDDEN_URL : request.getContextPath() + forbiddenURL); // response.sendError(HttpServletResponse.SC_FORBIDDEN);
+        return null;
+    }
+}
