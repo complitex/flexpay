9e600f511eb4bcd21a8ea75a92cb6416b3d61ac6
alex.chistyakov <alex.chistyakov@f554a4e9-133d-0410-ac35-5129da6e6cf8>
2008-02-03 10:15:21 +0000

    Добавил данные для таблицы OSZN_tbl

--- common/etc/sql/init_db.sql
+++ common/etc/sql/init_db.sql
@@ -3850,7 +3850,8 @@ INSERT INTO sz_file_types_tbl (id, file_mask, description)
 	VALUES (6,'\\d{8}\\u002E(d|D)\\d{2}', 'sz.file_type.srv_types_response');
 INSERT INTO sz_file_types_tbl (id, file_mask, description)
 	VALUES (7,'.*\\d{4}\\u002E(d|D)(b|B)(f|F)', 'sz.file_type.subsidy');	
-
+INSERT INTO sz_file_types_tbl (file_mask, description)
+	VALUES ('\\d{8}\\u002E(a|A)\\d{2}', 'sz.file_type.subsidy');
 -- Sz file status
 INSERT INTO sz_file_status_tbl (id, description)
 	VALUES (1, 'sz.file_status.imported');
@@ -3871,3 +3872,21 @@ INSERT INTO sz_file_actuality_status_tbl (id, description)
 INSERT INTO sz_file_actuality_status_tbl (id, description)
 	VALUES (2, 'sz.file_actuality_status.actualy');	
 
+-- OSZN orgs
+insert into oszn_tbl (id, district_id,description) values (1, 1,'Заельцовский');
+insert into oszn_tbl (id, district_id,description) values (2, 2,'Дзержинский');
+insert into oszn_tbl (id, description, district_id) values (3, 'Железнодорожный', 3);
+insert into oszn_tbl (id, description, district_id) values (4, 'Калининский', 4);
+insert into oszn_tbl (id, description, district_id) values (5, 'Кировский', 5);
+insert into oszn_tbl (id, description, district_id) values (6, 'Лениниский', 6);
+insert into oszn_tbl (id, description, district_id) values (7, 'Октябрьский', 7);
+insert into oszn_tbl (id, description, district_id) values (8, 'Первомайский', 8);
+insert into oszn_tbl (id, description, district_id) values (9, 'Советский', 9);
+insert into oszn_tbl (id, description, district_id) values (10, 'Центральный', 10);
+insert into oszn_tbl (id, description, district_id) values (11, 'Дёмский', 11);
+insert into oszn_tbl (id, description, district_id) values (12, 'Калиниский', 12);
+insert into oszn_tbl (id, description, district_id) values (13, 'Кировский', 13);
+insert into oszn_tbl (id, description, district_id) values (14, 'Лениниский', 14);
+insert into oszn_tbl (id, description, district_id) values (15, 'Октябрьский', 15);
+insert into oszn_tbl (id, description, district_id) values (16, 'Орджоникидзевский', 16);
+insert into oszn_tbl (id, description, district_id) values (17, 'Советский', 17);
