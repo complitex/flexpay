5841ad25b3238b20b80e7aef1b1c95daa5b185b4
vladislavtsoy <vladislavtsoy@f554a4e9-133d-0410-ac35-5129da6e6cf8>
2008-02-08 17:26:46 +0000

    made file upload progress

---
+++ trunk/common/src/org/flexpay/common/actions/FileUploadProgressAction.java
@@ -0,0 +1,47 @@
+package org.flexpay.common.actions;
+
+import java.util.Map;
+
+import org.apache.struts2.interceptor.SessionAware;
+
+import com.davidjc.ajaxfileupload.multipart.ProgressMonitor;
+
+public class FileUploadProgressAction implements SessionAware {
+
+	private Map sessionMap;
+	private String rnd;
+	private String stringResult;
+
+	public FileUploadProgressAction() {
+	}
+
+	public String execute() {
+		Object mon_obj = sessionMap
+				.get("com.davidjc.ajaxfileupload.multipart.ProgressMonitor");
+		ProgressMonitor monitor = (ProgressMonitor) mon_obj;
+		String progressInfo = monitor == null ? "" : monitor.percentComplete();
+		setStringResult(progressInfo);
+
+		return "success";
+	}
+
+	public void setSession(Map sessionMap) {
+		this.sessionMap = sessionMap;
+	}
+
+	public void setRnd(String rnd) {
+		this.rnd = rnd;
+	}
+
+	public String getRnd() {
+		return rnd;
+	}
+
+	public void setStringResult(String stringResult) {
+		this.stringResult = stringResult;
+	}
+
+	public String getStringResult() {
+		return stringResult;
+	}
+}
