9833c37de2550aa489251cb6ba5a06ea97310d58
vladislavtsoy <vladislavtsoy@f554a4e9-133d-0410-ac35-5129da6e6cf8>
2008-03-05 13:55:35 +0000

    fixed/changed project structure

--- common/src/org/flexpay/common/util/StringUtil.java
+++ common/src/org/flexpay/common/util/StringUtil.java
@@ -1,8 +1,24 @@
 package org.flexpay.common.util;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class StringUtil {
 	public static String getRandomString() {
 		return System.currentTimeMillis() + "-" + Math.random();
+	}
+	
+	public static List<String> tokenize(String line, String delim) {
+		int ind1 = 0;
+		int ind2 = -1;
+		List<String> fieldList = new ArrayList<String>();
+		while((ind2 = line.indexOf(delim, ind1)) != -1) {
+		    fieldList.add(line.substring(ind1, ind2));
+		    ind1 = ind2 + 1;
+		}
+		fieldList.add(line.substring(ind1, line.length()));
+		
+		return fieldList;
 	}
 
 }
