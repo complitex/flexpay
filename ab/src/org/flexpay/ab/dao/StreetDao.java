fc9c1b7389297a3173daaa8a93cd8940822cf1f0
vladislavtsoy <vladislavtsoy@f554a4e9-133d-0410-ac35-5129da6e6cf8>
2008-03-13 19:53:30 +0000

    [FP-65] made street autocomplete

--- ab/src/org/flexpay/ab/dao/StreetDao.java
+++ ab/src/org/flexpay/ab/dao/StreetDao.java
@@ -1,7 +1,10 @@
 package org.flexpay.ab.dao;
 
+import java.util.List;
+
 import org.flexpay.ab.persistence.Street;
 import org.flexpay.common.dao.NameTimeDependentDao;
 
 public interface StreetDao extends NameTimeDependentDao<Street, Long> {
+	List<Street> findByTownAndName(Long townId, String name);
 }
