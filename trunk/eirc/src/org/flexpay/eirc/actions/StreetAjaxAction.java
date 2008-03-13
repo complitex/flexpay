b12ccf1d361715553e885d1d3db6dd3e5fcd2596
vladislavtsoy <vladislavtsoy@f554a4e9-133d-0410-ac35-5129da6e6cf8>
2008-03-13 19:52:59 +0000

    [FP-65] made street autocomplete

--- trunk/eirc/src/org/flexpay/eirc/actions/StreetAjaxAction.java
+++ trunk/eirc/src/org/flexpay/eirc/actions/StreetAjaxAction.java
@@ -1,37 +1,64 @@
 package org.flexpay.eirc.actions;
 
-import java.util.ArrayList;
 import java.util.List;
+import java.util.Set;
 
+import org.flexpay.ab.actions.CommonAction;
+import org.flexpay.ab.persistence.Street;
+import org.flexpay.ab.persistence.StreetNameTranslation;
+import org.flexpay.ab.service.StreetService;
 import org.flexpay.common.exception.FlexPayException;
+import org.flexpay.common.persistence.Language;
+import org.flexpay.common.persistence.Translation;
+import org.flexpay.common.util.LanguageUtil;
+import org.flexpay.common.util.config.ApplicationConfig;
 
-public class StreetAjaxAction {
-	
+public class StreetAjaxAction extends CommonAction {
+
+	private StreetService streetService;
+
 	private String streetVar;
-	private List<StreetStub> streetList;
-	
-	
+	private List<Street> streetList;
+
 	public String execute() throws FlexPayException {
-		streetList = getStreetByVar(streetVar);
+		streetList = getStreetListByVar(streetVar);
 
 		return "success";
 	}
-	
-	private List<StreetStub> getStreetByVar(String streetVar) {
-		// TODO realize logic
-		
-		List<StreetStub> streetList = new ArrayList<StreetStub>();
-		
-		streetList.add(new StreetStub(1L, "Banana street"));
-		streetList.add(new StreetStub(2L, "Apple street"));
-		streetList.add(new StreetStub(3L, "Wall street"));
-		streetList.add(new StreetStub(4L, "Bulvar Kapucinov"));
-		
+
+	private List<Street> getStreetListByVar(String streetVar)
+			throws FlexPayException {
+		List<Street> streetList = streetService.findByTownAndName(27L,
+				streetVar + "%");
+
 		return streetList;
 	}
 
+	public Translation getTranslation(Street street) throws FlexPayException {
+		Set<StreetNameTranslation> translationSet = street.getCurrentName()
+				.getTranslations();
+		Language language = LanguageUtil.getLanguage(userPreferences
+				.getLocale());
+		Language defaultLang = ApplicationConfig.getInstance()
+				.getDefaultLanguage();
+		Translation result = null;
+		for (StreetNameTranslation translation : translationSet) {
+			if (language.equals(translation.getLang())) {
+				result = translation;
+				break;
+			} else if (defaultLang.equals(translation.getLang())) {
+				result = translation;
+			} else if (result == null) {
+				result = translation;
+			}
+		}
+
+		return result;
+	}
+
 	/**
-	 * @param streetVar the streetVar to set
+	 * @param streetVar
+	 *            the streetVar to set
 	 */
 	public void setStreetVar(String streetVar) {
 		this.streetVar = streetVar;
@@ -40,46 +67,16 @@ public class StreetAjaxAction {
 	/**
 	 * @return the streetList
 	 */
-	public List<StreetStub> getStreetList() {
+	public List<Street> getStreetList() {
 		return streetList;
 	}
-	
-	static class StreetStub {
-		private Long id;
-		private String name;
-		
-		StreetStub(Long id, String name) {
-			this.id = id;
-			this.name = name;
-		}
-		
-		/**
-		 * @return the id
-		 */
-		public Long getId() {
-			return id;
-		}
-		/**
-		 * @param id the id to set
-		 */
-		public void setId(Long id) {
-			this.id = id;
-		}
-		/**
-		 * @return the name
-		 */
-		public String getName() {
-			return name;
-		}
-		/**
-		 * @param name the name to set
-		 */
-		public void setName(String name) {
-			this.name = name;
-		}
-		
+
+	/**
+	 * @param streetService
+	 *            the streetService to set
+	 */
+	public void setStreetService(StreetService streetService) {
+		this.streetService = streetService;
 	}
-
-
 
 }
--- trunk/eirc/web/WEB-INF/jsp/eirc/data/correspondence/street_ajax.jsp
+++ trunk/eirc/web/WEB-INF/jsp/eirc/data/correspondence/street_ajax.jsp
@@ -3,6 +3,6 @@
 
 {"results": [
 <s:iterator value="streetList">
-  {"id": "<s:property value="id" />", "value": "<s:property value="name" />", "info": ""},
+  {"id": "<s:property value="id" />", "value": "<s:property value="%{getTranslation(top).name}" />", "info": ""},
 </s:iterator>
 ]}
