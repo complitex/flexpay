426bd3c04f36c3bbe5c4f1f4b0bac296bf78e4cb
alex.chistyakov <alex.chistyakov@f554a4e9-133d-0410-ac35-5129da6e6cf8>
2008-05-16 15:17:48 +0000

    [FP-31] тесты для JobManager

---
+++ common/tests/java/org/flexpay/common/process/job/MockJob.java
@@ -0,0 +1,11 @@
+package org.flexpay.common.process.job;
+
+import org.flexpay.common.exception.FlexPayException;
+
+import java.util.HashMap;
+
+public class MockJob extends Job{
+    public String execute(HashMap parameters) throws FlexPayException {
+        return Job.NEXT;
+    }
+}
