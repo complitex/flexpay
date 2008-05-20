827640d46115350f100f4270b4c5472c2aaec20e
mfedko <mfedko@f554a4e9-133d-0410-ac35-5129da6e6cf8>
2008-05-20 08:44:27 +0000

    Ab sync Tests fix

--- ab_sync/tests/java/org/flexpay/ab/sync/TestDumpHistoryJob.java
+++ ab_sync/tests/java/org/flexpay/ab/sync/TestDumpHistoryJob.java
@@ -3,17 +3,17 @@ package org.flexpay.ab.sync;
 import org.flexpay.common.test.SpringBeanAwareTestCase;
 import org.flexpay.ab.service.SyncService;
 import org.flexpay.ab.service.HistoryDumpService;
+import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 
 public class TestDumpHistoryJob extends SpringBeanAwareTestCase {
 
-	@Override
-	protected void runTest() throws Throwable {
-		testDumpHistory();
-	}
+	@Autowired
+	protected HistoryDumpService dumpService;
 
+	@Test
 	public void testDumpHistory() throws Throwable {
 
-		HistoryDumpService dumpService = (HistoryDumpService) applicationContext.getBean("historyDumpService");
 		dumpService.dumpHistory();
 	}
 }
