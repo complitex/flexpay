1bf1660fdb0efe6b866f90e6fd28d6140d4fea90
alex.chistyakov <alex.chistyakov@f554a4e9-133d-0410-ac35-5129da6e6cf8>
2008-05-23 08:58:23 +0000

    [FP-95] добавлен переходный скрипт. # заменил на $

--- ab/etc/sql/update/2008_05_23.sql
+++ ab/etc/sql/update/2008_05_23.sql
@@ -8,13 +8,19 @@ create table ab_person_registrations_tbl (
     );
 	
 alter table ab_person_registrations_tbl 
-        add index INDX_ab_person_registrations#apartment_id (apartment_id), 
-        add constraint FP_ab_person_registrations#apartment_id
+        add index INDX_ab_person_registrations$apartment_id (apartment_id),
+        add constraint FP_ab_person_registrations$apartment_id
         foreign key (apartment_id) 
         references apartments_tbl (id);
 
 alter table ab_person_registrations_tbl 
-        add index INDX_ab_person_registrations#person_id (person_id), 
-        add constraint FP_ab_person_registrations#person_id 
+        add index INDX_ab_person_registrations$person_id (person_id),
+        add constraint FP_ab_person_registrations$person_id
         foreign key (person_id) 
         references persons_tbl (id);
+
+insert into ab_person_registrations_tbl (person_id,apartment_id, begin_date,end_date)
+ (SELECT id, apartment_id, current_date AS BEGIN_DATE, DATE('2100-01-01') AS END_DATE FROM persons_tbl p where apartment_id is not null);
+
+alter table persons_tbl drop index FKE7B2AFBDDEF75687, drop foreign key FKE7B2AFBDDEF75687;
+alter table persons_tbl drop column apartment_id;
