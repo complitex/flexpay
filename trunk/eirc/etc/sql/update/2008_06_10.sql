2d0e690de983ac8e12c605c7d5a96493484543e5
mfedko <mfedko@f554a4e9-133d-0410-ac35-5129da6e6cf8>
2008-06-11 10:07:51 +0000

    index to eirc_account_id instead of apartment_id

--- trunk/eirc/etc/sql/update/2008_06_10.sql
+++ trunk/eirc/etc/sql/update/2008_06_10.sql
@@ -2,7 +2,7 @@ ALTER TABLE eirc_consumers_tbl
     ADD COLUMN eirc_account_id BIGINT COMMENT 'Eirc account reference';
 
 alter table eirc_consumers_tbl 
-        add index FK_eirc_eirc_consumers_eirc_account_id (apartment_id),
+        add index FK_eirc_eirc_consumers_eirc_account_id (eirc_account_id),
         add constraint FK_eirc_eirc_consumers_eirc_account_id
         foreign key (eirc_account_id) 
         references eirc_eirc_accounts_tbl (id);
