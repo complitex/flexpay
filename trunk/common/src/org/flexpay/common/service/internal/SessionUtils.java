aa86a194838e16bcc38d5ed637ecbbe5797d755a
mfedko <mfedko@f554a4e9-133d-0410-ac35-5129da6e6cf8>
2008-06-30 12:12:08 +0000

    FP-99 Save fix

--- trunk/common/src/org/flexpay/common/service/internal/SessionUtils.java
+++ trunk/common/src/org/flexpay/common/service/internal/SessionUtils.java
@@ -5,11 +5,13 @@ import org.springframework.transaction.annotation.Transactional;
 import org.springframework.transaction.annotation.Propagation;
 import org.apache.log4j.Logger;
 
+import java.util.Collection;
+
 /**
  *
  */
 @Transactional
-public class SessionFlasher {
+public class SessionUtils {
 
 	private Logger log = Logger.getLogger(getClass());
 
@@ -29,6 +31,24 @@ public class SessionFlasher {
 		log.debug("Clearing");
 
 		hibernateTemplate.clear();
+	}
+
+	@Transactional(propagation = Propagation.REQUIRED)
+	public void evict(Object o) {
+
+		log.debug("Evicting");
+
+		hibernateTemplate.evict(o);
+	}
+
+	@Transactional(propagation = Propagation.REQUIRED)
+	public void evict(Collection c) {
+
+		log.debug("Evicting collection");
+
+		for (Object o : c) {
+			hibernateTemplate.evict(o);
+		}
 	}
 
 	public void setHibernateTemplate(HibernateTemplate hibernateTemplate) {
