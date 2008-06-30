bdd6499cddbaffac660daaaa6331a7fc17b1772e
mfedko <mfedko@f554a4e9-133d-0410-ac35-5129da6e6cf8>
2008-06-30 11:52:51 +0000

    FP-99 Code duplication check added. Service type intersections check added.

---
+++ trunk/eirc/tests/java/org/flexpay/eirc/service/TestServiceDao.java
@@ -0,0 +1,41 @@
+package org.flexpay.eirc.service;
+
+import org.flexpay.common.test.SpringBeanAwareTestCase;
+import org.flexpay.common.util.config.ApplicationConfig;
+import org.flexpay.eirc.dao.ServiceDaoExt;
+import org.flexpay.eirc.persistence.Service;
+import org.flexpay.eirc.persistence.ServiceProvider;
+import org.flexpay.eirc.persistence.ServiceType;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.List;
+
+public class TestServiceDao extends SpringBeanAwareTestCase {
+
+	@Autowired
+	private ServiceDaoExt serviceDaoExt;
+	@Autowired
+	private SPService spService;
+	@Autowired
+	private ServiceTypeService serviceTypeService;
+
+	@Test
+	public void testGetIntersectionServices() {
+
+		// Find CN service provider
+		ServiceProvider provider = spService.getProvider(10L);
+		assertNotNull("Cannot find service provider", provider);
+
+		// Find service type by code
+		ServiceType type = serviceTypeService.getServiceType(1);
+		assertNotNull("Cannot find service type", type);
+
+		List<Service> services = serviceDaoExt.findIntersectingServices(provider.getId(), type.getId(),
+				ApplicationConfig.getInstance().getPastInfinite(),
+				ApplicationConfig.getInstance().getFutureInfinite());
+		assertEquals("Invalid number of services found", 1, services.size());
+	}
+}
