cc829943fb67563d2606554fba45e72e2a1a5347
mfedko <mfedko@f554a4e9-133d-0410-ac35-5129da6e6cf8>
2008-07-08 04:50:41 +0000

    tests cleanup

--- ab_sync/tests/java/org/flexpay/ab/sync/TestDumpHistoryJob.java
+++ ab_sync/tests/java/org/flexpay/ab/sync/TestDumpHistoryJob.java
@@ -1,8 +1,7 @@
 package org.flexpay.ab.sync;
 
-import org.flexpay.common.test.SpringBeanAwareTestCase;
-import org.flexpay.ab.service.SyncService;
 import org.flexpay.ab.service.HistoryDumpService;
+import org.flexpay.common.test.SpringBeanAwareTestCase;
 import org.junit.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 
--- ab_sync/tests/java/org/flexpay/ab/sync/TestSyncAbJob.java
+++ ab_sync/tests/java/org/flexpay/ab/sync/TestSyncAbJob.java
@@ -3,13 +3,16 @@ package org.flexpay.ab.sync;
 import org.flexpay.ab.service.SyncService;
 import org.flexpay.common.test.SpringBeanAwareTestCase;
 import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 
 public class TestSyncAbJob extends SpringBeanAwareTestCase {
+
+	@Autowired
+	private SyncService syncService;
 
 	@Test
 	public void testSyncAb() throws Throwable {
 
-		SyncService syncService = (SyncService) applicationContext.getBean("abSyncService");
 		syncService.syncAB();
 	}
 }
