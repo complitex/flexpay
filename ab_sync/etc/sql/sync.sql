10acbbd6fc9479f095e925fed159761d06ebd0b2
mfedko <mfedko@f554a4e9-133d-0410-ac35-5129da6e6cf8>
2008-07-23 03:18:43 +0000

    index updated

--- ab_sync/etc/sql/sync.sql
+++ ab_sync/etc/sql/sync.sql
@@ -22,4 +22,4 @@ create table ab_sync_changes_tbl (
 	order_weight INTEGER NOT NULL DEFAULT 0
 );
 
-ALTER TABLE ab_sync_changes_tbl ADD INDEX Index_record_id (record_id);
+ALTER TABLE ab_sync_changes_tbl ADD INDEX Index_weight_id_date (order_weight, record_id, record_date);
