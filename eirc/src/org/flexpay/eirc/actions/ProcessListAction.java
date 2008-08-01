f5cc135a1400fe93e94bbbbd9837d663f4e6db25
alex.chistyakov <alex.chistyakov@f554a4e9-133d-0410-ac35-5129da6e6cf8>
2008-08-01 13:42:26 +0000

    Process List delete action added (not complited yet)

--- eirc/src/org/flexpay/eirc/actions/ProcessListAction.java
+++ eirc/src/org/flexpay/eirc/actions/ProcessListAction.java
@@ -2,6 +2,8 @@ package org.flexpay.eirc.actions;
 
 import java.util.Collections;
 import java.util.List;
+import java.util.Set;
+import java.util.HashSet;
 
 import org.flexpay.common.process.Process;
 import org.flexpay.common.process.ProcessManager;
@@ -10,18 +12,21 @@ import org.flexpay.common.process.ProcessStateComparator;
 public class ProcessListAction {
 	
 	private List<Process> processList;
-	
-	public String execute() {
-		processList = getProcessListMethod();
-		
+	private Set<Long> objectIds = new HashSet<Long>();
+    private String submited = "";
+
+    public String execute() {
+        if (objectIds != null && objectIds.size() >0){
+            ProcessManager.getInstance().deleteProcessInstanceList(objectIds);
+        }else{
+            processList = getProcessListMethod();
+        }
 		return "success";
 	}
 	
 	private List<Process> getProcessListMethod() {
 		List<Process> processes = ProcessManager.getInstance().getProcessList();
         Collections.sort(processes, new ProcessStateComparator());
-      
-        
         return processes;
 	}
 
@@ -32,4 +37,11 @@ public class ProcessListAction {
 		return processList;
 	}
 
+    public void setObjectIds(Set<Long> objectIds) {
+        this.objectIds = objectIds;
+    }
+
+    public void setSubmited(String submited) {
+        this.submited = submited;
+    }
 }
