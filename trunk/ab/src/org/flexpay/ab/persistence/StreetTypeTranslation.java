d8852263b299856ecb21230cf63e07864cb0eace
mfedko <mfedko@f554a4e9-133d-0410-ac35-5129da6e6cf8>
2008-08-11 15:56:04 +0000

    Street types search fixed, additional types added.

--- trunk/ab/src/org/flexpay/ab/persistence/StreetTypeTranslation.java
+++ trunk/ab/src/org/flexpay/ab/persistence/StreetTypeTranslation.java
@@ -1,6 +1,9 @@
 package org.flexpay.ab.persistence;
 
 import org.flexpay.common.persistence.Translation;
+import org.jetbrains.annotations.NotNull;
+import org.apache.commons.lang.builder.ToStringBuilder;
+import org.apache.commons.lang.builder.ToStringStyle;
 
 /**
  * StreetTypeTranslation is a translation of StreetType to particular language
@@ -17,18 +20,18 @@ public class StreetTypeTranslation extends Translation {
 
 	/**
 	 * Getter for property 'shortName'.
-	 * 
+	 *
 	 * @return Value for property 'shortName'.
 	 */
+	@NotNull
 	public String getShortName() {
 		return shortName != null ? shortName : "";
 	}
 
 	/**
 	 * Setter for property 'shortName'.
-	 * 
-	 * @param shortName
-	 *            Value to set for property 'shortName'.
+	 *
+	 * @param shortName Value to set for property 'shortName'.
 	 */
 	public void setShortName(String shortName) {
 		this.shortName = shortName;
@@ -39,7 +42,11 @@ public class StreetTypeTranslation extends Translation {
 	 */
 	@Override
 	public String toString() {
-		return super.toString();
+		return new ToStringBuilder(this, ToStringStyle.DEFAULT_STYLE)
+				.append("id", getId())
+				.append("name", getName())
+				.append("shortName", getShortName())
+				.toString();
 	}
 
 	/**
