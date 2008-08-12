c956cc5834ae6af82bb417cf72f9842da3d27fd7
mfedko <mfedko@f554a4e9-133d-0410-ac35-5129da6e6cf8>
2008-08-12 06:27:27 +0000

    Streets sync with CN.
    To create streets.
    0. import init_db.sql from common/etc it has updated street types structure.
    1. run PrepareStreetsSql test
    ant -f ab_build.xml ab.runtests -Dtest.class.name=org.flexpay.ab.service.importexport.PrepareStreetsSql
    
    2. import generated sqls to db (see _build/web folder)
    mysql -u flexpay_user -p flexpay_db < strcorr.sql
    mysql -u flexpay_user -p flexpay_db < strtypecorr.sql
    
    3. import streets.sql with CN streets
    mysql -u flexpay_user -p flexpay_db < streets.sql
    
    4. import CN street types to AB street types corrections table
    mysql -u flexpay_user -p flexpay_db < type_corr.sql
    
    5. run CreateStreets test, do not repeat, no any checkes performed
    ant -f ab_build.xml ab.runtests -Dtest.class.name=org.flexpay.ab.service.importexport.CreateStreets

---
+++ ab/tests/java/org/flexpay/ab/service/importexport/CreateStreets.java
@@ -0,0 +1,103 @@
+package org.flexpay.ab.service.importexport;
+
+import org.flexpay.common.test.SpringBeanAwareTestCase;
+import org.junit.Ignore;
+import org.junit.Test;
+import org.springframework.jdbc.core.PreparedStatementCreator;
+import org.springframework.jdbc.core.RowCallbackHandler;
+import org.springframework.jdbc.support.GeneratedKeyHolder;
+import org.springframework.jdbc.support.KeyHolder;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class CreateStreets extends SpringBeanAwareTestCase {
+
+	private static final String sqlGetStreets =
+			"select s.name, sc.cn_obj_id, tt.int_id " +
+			"from Streets s " +
+			"	inner join StreetTypes st on s.TypeId=st.id" +
+			"	inner join tmp_types_corr tt on st.id=tt.ext_id" +
+			"	left join tmp_street_corr sc on sc.street_id=s.id";
+
+	private static final String sqlInsertStreet = "insert into ab_streets_tbl (status, town_id) values (0, ?)";
+	private static final String sqlInsertStreetName = "insert into ab_street_names_tbl (street_id) values (?)";
+	private static final String sqlInsertStreetNameTemporal =
+			"insert into ab_street_names_temporal_tbl (street_id, street_name_id, begin_date, end_date, create_date, invalid_date)\n" +
+			"values (?, ?, '1900-01-01', '2100-12-31', '2008-08-11', '2100-12-31')";
+	private static final String sqlInsertStreetTypeTemporal =
+			"insert into ab_street_types_temporal_tbl (street_id, street_type_id, begin_date, end_date, create_date, invalid_date)\n" +
+			"values (?, ?, '1900-01-01', '2100-12-31', '2008-08-11', '2100-12-31')";
+	private static final String sqlInsertStreetNameTr =
+			"insert into ab_street_name_translations_tbl (name, street_name_id, language_id) values (?, ?, ?)";
+	private static final String sqlInsertStreetCorrection =
+			"insert into common_data_corrections_tbl (internal_object_id, object_type, external_object_id, data_source_description_id)" +
+			"values (?, ?, ?, ?)";
+
+	private static final Long townId = 1L;
+	private static final Long ruId = 1L;
+	private static final Long dataSourceId = 1L;
+	private static final int streetTypeNumber = 0x06;
+
+	@Test
+	@Ignore
+	public void testInsertStreets() throws Exception {
+
+		jdbcTemplate.query(sqlGetStreets, new RowCallbackHandler() {
+			public void processRow(ResultSet rs) throws SQLException {
+				String name = rs.getString("name");
+				Long cnObjectId = rs.getLong("cn_obj_id");
+				Long internalTypeId = rs.getLong("int_id");
+
+				KeyHolder keyHolder = new GeneratedKeyHolder();
+				jdbcTemplate.update(new StreetInsertCreator(), keyHolder);
+
+				Number streetId = keyHolder.getKey();
+				keyHolder = new GeneratedKeyHolder();
+				jdbcTemplate.update(new StreetNameInsertCreator(streetId.longValue()), keyHolder);
+
+				Number streetNameId = keyHolder.getKey();
+				Object[] params = {name, streetNameId.longValue(), ruId};
+				jdbcTemplate.update(sqlInsertStreetNameTr, params);
+
+				Object[] paramsNT = {streetId.longValue(), streetNameId.longValue()};
+				jdbcTemplate.update(sqlInsertStreetNameTemporal, paramsNT);
+
+				Object[] paramsTT = {streetId.longValue(), internalTypeId};
+				jdbcTemplate.update(sqlInsertStreetTypeTemporal, paramsTT);
+
+				// add correction
+				if (cnObjectId != 0) {
+					Object[] params2 = {streetId.longValue(), streetTypeNumber, cnObjectId, dataSourceId};
+					jdbcTemplate.update(sqlInsertStreetCorrection, params2);
+				}
+			}
+		});
+	}
+
+	private static final class StreetInsertCreator implements PreparedStatementCreator {
+
+		public PreparedStatement createPreparedStatement(Connection con) throws SQLException {
+			PreparedStatement ps = con.prepareStatement(sqlInsertStreet);
+			ps.setLong(1, townId);
+			return ps;
+		}
+	}
+
+	private static final class StreetNameInsertCreator implements PreparedStatementCreator {
+
+		private Long streetId;
+
+		private StreetNameInsertCreator(Long streetId) {
+			this.streetId = streetId;
+		}
+
+		public PreparedStatement createPreparedStatement(Connection con) throws SQLException {
+			PreparedStatement ps = con.prepareStatement(sqlInsertStreetName);
+			ps.setLong(1, streetId);
+			return ps;
+		}
+	}
+}
