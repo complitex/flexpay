50b01f49901864ea34d1c5fde00d4a725593653e
mfedko <mfedko@f554a4e9-133d-0410-ac35-5129da6e6cf8>
2008-09-29 08:05:17 +0000

    Measure units added

---
+++ trunk/common/src/org/flexpay/common/persistence/MeasureUnitName.java
@@ -0,0 +1,10 @@
+package org.flexpay.common.persistence;
+
+public class MeasureUnitName extends Translation {
+
+	@Override
+	public boolean equals(Object o) {
+		return o instanceof MeasureUnitName && super.equals(o);
+	}
+
+}
