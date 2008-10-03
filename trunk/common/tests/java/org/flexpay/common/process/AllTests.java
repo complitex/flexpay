0a26fd3be1df28bca048a478405c0de0b43b0186
alex.chistyakov <alex.chistyakov@f554a4e9-133d-0410-ac35-5129da6e6cf8>
2008-10-03 05:03:47 +0000

    process logger removed

--- trunk/common/tests/java/org/flexpay/common/process/AllTests.java
+++ trunk/common/tests/java/org/flexpay/common/process/AllTests.java
@@ -13,7 +13,6 @@ public class AllTests extends TestCase {
 		TestSuite suite = new TestSuite();
 //        suite.addTest(org.flexpay.common.process.job.AllTests.suite());
 //        suite.addTest(new JUnit4TestAdapter(TestProcessManager.class));
-		suite.addTest(new JUnit4TestAdapter(TestProcessLogger.class));
         return suite;
 	}
 }
