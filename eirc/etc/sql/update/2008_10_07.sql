10a67fa7fe6ca96e9d261e9d661db4985e9aedf6
mfedko <mfedko@f554a4e9-133d-0410-ac35-5129da6e6cf8>
2008-10-07 08:55:42 +0000

    Two services added

---
+++ eirc/etc/sql/update/2008_10_07.sql
@@ -0,0 +1,25 @@
+SELECT @service_provider_cn:=1;
+select @ru_id:=id from common_languages_tbl where lang_iso_code='ru';
+
+INSERT INTO eirc_service_types_tbl (status, code) VALUES (0, 240);
+SELECT @service_type_240:=last_insert_id();
+INSERT INTO eirc_service_type_name_translations_tbl (name, description, language_id, service_type_id)
+	VALUES ('Погреба', '', @ru_id, @service_type_240);
+
+INSERT INTO eirc_service_types_tbl (status, code) VALUES (0, 250);
+SELECT @service_type_250:=last_insert_id();
+INSERT INTO eirc_service_type_name_translations_tbl (name, description, language_id, service_type_id)
+	VALUES ('Содержание животных', '', @ru_id, @service_type_250);
+
+
+INSERT INTO eirc_services_tbl (provider_id, external_code, measure_unit_id, type_id, begin_date, end_date)
+	VALUES (@service_provider_cn, '240', null, @service_type_240, '1900-01-01', '2100-12-31');
+SELECT @service_240:=last_insert_id();
+INSERT INTO eirc_service_descriptions_tbl (name, language_id, service_id)
+	VALUES ('Погреба', @ru_id, @service_240);
+
+INSERT INTO eirc_services_tbl (provider_id, external_code, measure_unit_id, type_id, begin_date, end_date)
+	VALUES (@service_provider_cn, '250', null, @service_type_250, '1900-01-01', '2100-12-31');
+SELECT @service_250:=last_insert_id();
+INSERT INTO eirc_service_descriptions_tbl (name, language_id, service_id)
+	VALUES ('Погреба', @ru_id, @service_250);
