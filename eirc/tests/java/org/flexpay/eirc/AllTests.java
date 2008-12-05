bc626714286d03c5aec27513c95b189fcbdbd9e3
mfedko <mfedko@f554a4e9-133d-0410-ac35-5129da6e6cf8>
2008-12-05 13:21:00 +0000

    Test added to all

--- eirc/tests/java/org/flexpay/eirc/AllTests.java
+++ eirc/tests/java/org/flexpay/eirc/AllTests.java
@@ -12,6 +12,7 @@ import org.junit.runners.Suite;
 @Suite.SuiteClasses({
 		TestProcess.class,
 		org.flexpay.eirc.actions.AllTests.class,
+		org.flexpay.eirc.dao.AllTests.class,
 		org.flexpay.eirc.service.AllTests.class,
 		org.flexpay.eirc.util.config.AllTests.class,
 		org.flexpay.eirc.sp.AllTests.class,
