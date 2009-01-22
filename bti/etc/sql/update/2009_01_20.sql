5f3e6fe86a9d06c936de80e9ab2c3fcf9558ce88
semenov.alexei <semenov.alexei@f554a4e9-133d-0410-ac35-5129da6e6cf8>
2009-01-22 07:55:17 +0000

    FP-295
    SQL update for 20th January layout fixed

--- bti/etc/sql/update/2009_01_20.sql
+++ bti/etc/sql/update/2009_01_20.sql
@@ -1,41 +1,29 @@
-create table
-        bti_sewer_material_type_translations_tbl (
+create table bti_sewer_material_type_translations_tbl (
     id bigint not null auto_increment comment 'Primary key identifier',
     name varchar(255) not null comment 'Type name translation',
     description varchar(255) comment 'Type description translation',
-    sewer_material_type_id bigint not null comment
-            'Sewer material type reference',
+    sewer_material_type_id bigint not null comment 'Sewer material type reference',
     language_id bigint not null comment 'Language reference',
     primary key (id),
     unique (sewer_material_type_id, language_id)
-)
-        comment='Sewer material type translations';
+) comment='Sewer material type translations';
 
 create table bti_sewer_material_types_tbl (
     id bigint not null auto_increment comment 'Primary key identifier',
-    status integer not null comment
-            'Shows whether entity is active or disabled',
+    status integer not null comment 'Shows whether entity is active or disabled',
     primary key (id)
-)
-        comment='Table contains sewer material types information';
+) comment='Table contains sewer material types information';
 
-alter table
-        bti_sewer_material_type_translations_tbl
-add index
-        bti_sewer_material_type_tbl_sewer_material_type_id (sewer_material_type_id),
-add constraint
-        bti_sewer_material_type_tbl_sewer_material_type_id
-foreign key (sewer_material_type_id)
-references bti_sewer_material_types_tbl (id);
+alter table bti_sewer_material_type_translations_tbl
+    add index bti_sewer_material_type_tbl_sewer_material_type_id (sewer_material_type_id),
+    add constraint bti_sewer_material_type_tbl_sewer_material_type_id
+    foreign key (sewer_material_type_id)
+    references bti_sewer_material_types_tbl (id);
 
-alter table
-        bti_sewer_material_type_translations_tbl
-add index
-        lang_sewer_material_type_pair_language_id (language_id),
-add constraint
-        lang_sewer_material_type_pair_language_id
-foreign key (language_id)
-references common_languages_tbl (id);
+alter table bti_sewer_material_type_translations_tbl
+    add index lang_sewer_material_type_pair_language_id (language_id),
+    add constraint lang_sewer_material_type_pair_language_id
+    foreign key (language_id)
+    references common_languages_tbl (id);
 
-update common_version_tbl
-        set last_modified_date='2009-01-20', date_version=0;
+update common_version_tbl set last_modified_date='2009-01-20', date_version=0;
