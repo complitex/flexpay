076342714584d7e1d851f11977005770c29d8515
AlexuyT <AlexuyT@f554a4e9-133d-0410-ac35-5129da6e6cf8>
2009-01-23 20:15:05 +0000

    FP-318 (продолжение) Полностью реализован справочник файлов правил расчета тарифов

---
+++ trunk/tc/etc/sql/update/2009_01_23.sql
@@ -0,0 +1,8 @@
+alter table tc_tariff_calc_rules_files_tbl drop foreign key FKDA48352F62EB9A29;
+alter table tc_tariff_calc_rules_files_tbl drop column status_id;
+
+delete from common_file_statuses_tbl where module_id in (select (id) from common_flexpay_modules where name = 'tc');
+
+update common_file_types_tbl set file_mask='*.{8}\\u002E(d|D)(r|R)(l|L)' where name = 'tc.file_type.tariff_rules';
+
+update common_version_tbl set last_modified_date='2009-01-23', date_version=0;
---
+++ trunk/tc/src/org/flexpay/tc/actions/tariff/TariffCalcRulesFileEditAction.java
@@ -0,0 +1,183 @@
+package org.flexpay.tc.actions.tariff;
+
+import org.flexpay.common.actions.FPActionSupport;
+import org.flexpay.common.persistence.FPFile;
+import org.flexpay.common.persistence.FPFileType;
+import org.flexpay.common.persistence.Language;
+import static org.flexpay.common.persistence.Stub.stub;
+import org.flexpay.common.service.FPFileService;
+import org.flexpay.common.util.CollectionUtils;
+import org.flexpay.common.util.FPFileUtil;
+import org.flexpay.common.util.config.ApplicationConfig;
+import org.flexpay.tc.persistence.TariffCalculationRulesFile;
+import org.flexpay.tc.persistence.TariffCalculationRulesFileTranslation;
+import org.flexpay.tc.service.TariffCalculationRulesFileService;
+import org.jetbrains.annotations.NotNull;
+import org.springframework.beans.factory.annotation.Required;
+
+import java.io.File;
+import java.util.Map;
+
+public class TariffCalcRulesFileEditAction extends FPActionSupport {
+
+	private TariffCalculationRulesFile rulesFile = new TariffCalculationRulesFile();
+	private Map<Long, String> names = CollectionUtils.treeMap();
+	private Map<Long, String> descriptions = CollectionUtils.treeMap();
+
+	private File upload;
+	private String uploadFileName;
+	private String uploadContentType;
+
+    private String moduleName;
+	private TariffCalculationRulesFileService tariffCalculationRulesFileService;
+    private FPFileService fpFileService;
+
+	@NotNull
+	public String doExecute() {
+
+		if (rulesFile.getId() == null) {
+			addActionError(getText("error.no_id"));
+			return REDIRECT_SUCCESS;
+		}
+
+		TariffCalculationRulesFile file = rulesFile.isNew() ? rulesFile : tariffCalculationRulesFileService.read(stub(rulesFile));
+		if (file == null) {
+			addActionError(getText("error.invalid_id"));
+			return REDIRECT_SUCCESS;
+		}
+
+		if (!isSubmit()) {
+			rulesFile = file;
+			initNames();
+			return INPUT;
+		}
+
+		if (uploadFileName == null) {
+			log.warn("Error: uploadFileName is null");
+			addActionError(getText("tc.error.upload_file_name_is_null"));
+			return INPUT;
+		}
+
+		FPFileType fileType = fpFileService.getTypeByFileName(uploadFileName, moduleName);
+		if (fileType == null) {
+			log.warn("Unknown file type");
+			addActionError(getText("tc.error.unknown_file_type"));
+			return INPUT;
+		}
+
+		// init translations
+		for (Map.Entry<Long, String> name : names.entrySet()) {
+			String value = name.getValue();
+			Language lang = getLang(name.getKey());
+			TariffCalculationRulesFileTranslation translation = new TariffCalculationRulesFileTranslation();
+			translation.setLang(lang);
+			translation.setName(value);
+			translation.setDescription(descriptions.get(name.getKey()));
+			file.setTranslation(translation);
+		}
+
+		if (rulesFile.isNew()) {
+
+			file.setType(fileType);
+			FPFile fileOnServer = new FPFile();
+			fileOnServer.setModule(fpFileService.getModuleByName(moduleName));
+			fileOnServer.setOriginalName(uploadFileName);
+			fileOnServer.setUserName(getUserPreferences().getUserName());
+			try {
+				File fileOnSystem = FPFileUtil.saveToFileSystem(fileOnServer, upload);
+				fileOnServer.setNameOnServer(fileOnSystem.getName());
+				fileOnServer.setSize(fileOnSystem.length());
+			} catch (Exception e) {
+				log.error("Error creating file on server", e);
+				addActionError(getText("tc.error.cant_create_file_on_system"));
+				return INPUT;
+			}
+			file.setFile(fileOnServer);
+			file.setUserName(getUserPreferences().getUserName());
+
+		}
+
+		tariffCalculationRulesFileService.save(file);
+
+		log.info("Tariff calculation rules file created {}", file);
+
+		return REDIRECT_SUCCESS;
+	}
+
+	private void initNames() {
+		for (TariffCalculationRulesFileTranslation name : rulesFile.getTranslations()) {
+			names.put(name.getLang().getId(), name.getName());
+			descriptions.put(name.getLang().getId(), name.getDescription());
+		}
+
+		for (Language lang : ApplicationConfig.getLanguages()) {
+			if (names.containsKey(lang.getId())) {
+				continue;
+			}
+			names.put(lang.getId(), "");
+			descriptions.put(lang.getId(), "");
+		}
+	}
+
+	@NotNull
+	@Override
+	protected String getErrorResult() {
+		return SUCCESS;
+	}
+
+	public TariffCalculationRulesFile getRulesFile() {
+		return rulesFile;
+	}
+
+	public void setRulesFile(TariffCalculationRulesFile rulesFile) {
+		this.rulesFile = rulesFile;
+	}
+
+	public Map<Long, String> getNames() {
+		return names;
+	}
+
+	public void setNames(Map<Long, String> names) {
+		this.names = names;
+	}
+
+	public Map<Long, String> getDescriptions() {
+		return descriptions;
+	}
+
+	public void setDescriptions(Map<Long, String> descriptions) {
+		this.descriptions = descriptions;
+	}
+
+	public void setUpload(File upload) {
+		this.upload = upload;
+	}
+
+	public void setUploadFileName(String uploadFileName) {
+		this.uploadFileName = uploadFileName;
+	}
+
+	public String getUploadContentType() {
+		return uploadContentType;
+	}
+
+	public void setUploadContentType(String uploadContentType) {
+		this.uploadContentType = uploadContentType;
+	}
+
+	@Required
+	public void setTariffCalculationRulesFileService(TariffCalculationRulesFileService tariffCalculationRulesFileService) {
+		this.tariffCalculationRulesFileService = tariffCalculationRulesFileService;
+	}
+
+	@Required
+    public void setModuleName(String moduleName) {
+        this.moduleName = moduleName;
+    }
+
+	@Required
+	public void setFpFileService(FPFileService fpFileService) {
+		this.fpFileService = fpFileService;
+	}
+
+}
