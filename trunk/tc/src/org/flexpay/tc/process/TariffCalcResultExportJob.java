54196e729da694b579f439cb71716b53b2926add
AlexuyT <AlexuyT@f554a4e9-133d-0410-ac35-5129da6e6cf8>
2009-01-30 16:50:37 +0000

    FP-321 (продолжение 5) Доработка сервиса экспорта результатов вычислений в модуле TC

--- trunk/tc/src/org/flexpay/tc/process/TariffCalcResultExportJob.java
+++ trunk/tc/src/org/flexpay/tc/process/TariffCalcResultExportJob.java
@@ -15,6 +15,7 @@ import org.slf4j.Logger;
 import org.springframework.beans.factory.annotation.Required;
 
 import java.io.Serializable;
+import java.sql.*;
 import java.util.Date;
 import java.util.List;
 import java.util.Map;
@@ -24,6 +25,11 @@ public class TariffCalcResultExportJob extends Job {
 	@NonNls
 	private Logger pLog = ProcessLogger.getLogger(getClass());
 
+	private String jdbcDriverClassName;
+	private String jdbcUrl;
+	private String jdbcUsername;
+	private String jdbcPassword;
+	private String procedure; 
 	private Long dataSourceDescriptionId;
 	private LockManager lockManager;
 	private ClassToTypeRegistry classToTypeRegistry;
@@ -39,45 +45,83 @@ public class TariffCalcResultExportJob extends Job {
 			return RESULT_NEXT;
 		}
 
-		Date calcDate = (Date) parameters.get("date");
+		Connection conn = null;
+		try {
+			conn = getConnection();
+			pLog.info("Connection with DB (url - {}) create successfully", jdbcUrl);
+			conn.setAutoCommit(false);
 
-		log.debug("CalcDate - {}", calcDate);
+			Date calcDate = (Date) parameters.get("date");
+			java.sql.Date sqlCalcDate = new java.sql.Date(calcDate.getTime());
 
-		List<Long> addressIds = tariffCalculationResultService.getAddressIds(calcDate);
+			List<Long> addressIds = tariffCalculationResultService.getAddressIds(calcDate);
 
-		log.debug("AdressIds - {}", addressIds);
+			for (Long addressId : addressIds) {
 
-		for (Long addressId : addressIds) {
+				String externalId = correctionsService.getExternalId(addressId, classToTypeRegistry.getType(BuildingAddress.class), dataSourceDescriptionId);
+				if (externalId == null) {
+					pLog.debug("ExternalId for building address with id={} not found", addressId);
+					continue;
+				}
 
-			String externalId = correctionsService.getExternalId(addressId, classToTypeRegistry.getType(BuildingAddress.class), dataSourceDescriptionId);
-			if (externalId == null) {
-				pLog.debug("ExternalId for building address with id={} not found", addressId);
-				continue;
-			}
-			log.debug("InternalId - {}, ExternalId - {}", addressId, externalId);
+				Integer intExtId = Integer.parseInt(externalId);
 
-			List<TariffCalculationResult> tcrs = tariffCalculationResultService.getTariffCalcResultsByCalcDateAndAddressId(calcDate, addressId);
+				List<TariffCalculationResult> tcrs = tariffCalculationResultService.getTariffCalcResultsByCalcDateAndAddressId(calcDate, addressId);
 
-			log.debug("TCRs - {}", tcrs);
+				try {
 
-			for (TariffCalculationResult tcr : tcrs) {
-				//TODO: Replace '1' to setFactTariff stored procedure
-				int exportResult = 1;
+					for (TariffCalculationResult tcr : tcrs) {
 
-				if (exportResult == 0) {
-					pLog.warn("Tariff {} for building with id={} not exists", tcr.getTariff(), tcr.getBuilding().getId());
-				} else if (exportResult == -1) {
-					pLog.warn("Building with id={} for caluculation result {} not found", tcr.getBuilding().getId(), tcr);
-				} else if (exportResult == -2) {
-					pLog.warn("Error: Can't create record in history for calculation result {}", tcr);
-				} else if (exportResult == -3) {
-					pLog.warn("Locking exception: Can't export calculcation result {}", tcr);
-				} else {
-					pLog.debug("Tariff calculation result {} exported succesfully", tcr);
+						CallableStatement cs = conn.prepareCall(procedure);
+						try {
+							cs.registerOutParameter(1, Types.INTEGER);
+							cs.setInt(2, intExtId);
+							cs.setString(3, tcr.getTariff().getSubServiceCode());
+							cs.setBigDecimal(4, tcr.getValue());
+							cs.setDate(5, sqlCalcDate);
+							cs.executeUpdate();
+
+							int exportResult = cs.getInt(1);
+
+							if (exportResult == 0) {
+								pLog.warn("Tariff {} for building with id={} not exists", tcr.getTariff(), tcr.getBuilding().getId());
+							} else if (exportResult == -1) {
+								pLog.warn("Building with id={} for caluculation result {} not found", tcr.getBuilding().getId(), tcr);
+							} else if (exportResult == -2) {
+								pLog.warn("Error: Can't create record in history for calculation result {}", tcr);
+							} else if (exportResult == -3) {
+								pLog.warn("Locking exception: Can't export calculcation result {}", tcr);
+							} else {
+								pLog.debug("Tariff calculation result {} exported succesfully", tcr);
+							}
+						} finally {
+							cs.close();
+						}
+					}
+
+
+					conn.commit();
+				} catch (Exception e) {
+					pLog.error("SQL error for adressId=" + addressId, e);
+					try {
+						conn.rollback();
+					} catch (SQLException e1) {
+						// do nothing
+					}
 				}
 
 			}
 
+		} catch (Exception e) {
+			pLog.error("SQL error", e);
+		} finally {
+			try {
+				if (conn != null && !conn.isClosed()) {
+					conn.close();
+				}
+			} catch (SQLException e) {
+				// do nothing
+			}
 		}
 
 		lockManager.releaseLock(Resources.BUILDING_ATTRIBUTES);
@@ -85,6 +129,36 @@ public class TariffCalcResultExportJob extends Job {
 		pLog.debug("Tariff calculation result export procces finished");
 
 		return RESULT_NEXT;
+	}
+
+	private Connection getConnection() throws Exception {
+		Class.forName(jdbcDriverClassName);
+		return DriverManager.getConnection(jdbcUrl, jdbcUsername, jdbcPassword);
+	}
+
+	@Required
+	public void setJdbcDriverClassName(String jdbcDriverClassName) {
+		this.jdbcDriverClassName = jdbcDriverClassName;
+	}
+
+	@Required
+	public void setJdbcUrl(String jdbcUrl) {
+		this.jdbcUrl = jdbcUrl;
+	}
+
+	@Required
+	public void setJdbcUsername(String jdbcUsername) {
+		this.jdbcUsername = jdbcUsername;
+	}
+
+	@Required
+	public void setJdbcPassword(String jdbcPassword) {
+		this.jdbcPassword = jdbcPassword;
+	}
+
+	@Required
+	public void setProcedure(String procedure) {
+		this.procedure = procedure;
 	}
 
 	@Required
