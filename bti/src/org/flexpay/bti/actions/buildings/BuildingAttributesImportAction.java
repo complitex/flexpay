d5369727e78769b398d4eafe8e90a9111384d8a3
mfedko <mfedko@f554a4e9-133d-0410-ac35-5129da6e6cf8>
2009-02-02 06:18:20 +0000

    Building attributes import from CSV process added, no GUI yet

---
+++ bti/src/org/flexpay/bti/actions/buildings/BuildingAttributesImportAction.java
@@ -0,0 +1,74 @@
+package org.flexpay.bti.actions.buildings;
+
+import org.flexpay.bti.process.BuildingAttributesImportJob;
+import org.flexpay.common.actions.FPFileActionSupport;
+import org.flexpay.common.persistence.filter.BeginDateFilter;
+import org.flexpay.common.process.ProcessManager;
+import org.flexpay.common.util.CollectionUtils;
+import org.flexpay.common.util.DateUtil;
+import org.jetbrains.annotations.NotNull;
+import org.springframework.beans.factory.annotation.Required;
+
+import java.io.Serializable;
+import java.util.Map;
+
+public class BuildingAttributesImportAction extends FPFileActionSupport {
+
+	private ProcessManager processManager;
+
+	private BeginDateFilter beginDateFilter = new BeginDateFilter(DateUtil.now());
+
+	/**
+	 * Perform action execution.
+	 * <p/>
+	 * If return code starts with a {@link #PREFIX_REDIRECT} all error messages are stored in a session
+	 *
+	 * @return execution result code
+	 * @throws Exception if failure occurs
+	 */
+	@NotNull
+	protected String doExecute() throws Exception {
+
+		if (fpFile.isNew()) {
+			addActionError(getText("bti.error.building.attribute.import.no_file"));
+			return SUCCESS;
+		}
+		if (!beginDateFilter.needFilter()) {
+			addActionError(getText("bti.error.building.attribute.import.no_date"));
+			return SUCCESS;
+		}
+
+		Map<Serializable, Serializable> params = CollectionUtils.map();
+		params.put(BuildingAttributesImportJob.PARAM_FILE_ID, fpFile.getId());
+		params.put(BuildingAttributesImportJob.PARAM_IMPORT_DATE, beginDateFilter.getDate());
+
+		processManager.createProcess("BuildingAttributesImport", params);
+		addActionError(getText("bti.building.attribute.import.started"));
+		return SUCCESS;
+	}
+
+	/**
+	 * Get default error execution result
+	 * <p/>
+	 * If return code starts with a {@link #PREFIX_REDIRECT} all error messages are stored in a session
+	 *
+	 * @return {@link #ERROR} by default
+	 */
+	@NotNull
+	protected String getErrorResult() {
+		return SUCCESS;
+	}
+
+	public BeginDateFilter getBeginDateFilter() {
+		return beginDateFilter;
+	}
+
+	public void setBeginDateFilter(BeginDateFilter beginDateFilter) {
+		this.beginDateFilter = beginDateFilter;
+	}
+
+	@Required
+	public void setProcessManager(ProcessManager processManager) {
+		this.processManager = processManager;
+	}
+}
--- bti/src/org/flexpay/bti/process/BuildingAttributesImportJob.java
+++ bti/src/org/flexpay/bti/process/BuildingAttributesImportJob.java
@@ -7,11 +7,13 @@ import org.flexpay.bti.service.importexport.BuildingAttributesImporter;
 import org.flexpay.common.exception.FlexPayException;
 import org.flexpay.common.process.job.Job;
 import org.flexpay.common.service.FPFileService;
+import org.flexpay.common.util.config.ApplicationConfig;
 import org.springframework.beans.factory.annotation.Required;
 
 import java.io.*;
 import java.util.List;
 import java.util.Map;
+import java.util.Date;
 
 public class BuildingAttributesImportJob extends Job {
 
@@ -25,28 +27,38 @@ public class BuildingAttributesImportJob extends Job {
 	public String execute(Map<Serializable, Serializable> parameters) throws FlexPayException {
 
 		File file = fileService.getFileFromFileSystem((Long) parameters.get(PARAM_FILE_ID));
+		Date beginDate = (Date) parameters.get(PARAM_IMPORT_DATE);
+		if (beginDate == null) {
+			log.error("No import date parameter specified");
+			return RESULT_ERROR;
+		}
 
-		List<BuildingAttributeData> datas = null;
+		List<BuildingAttributeData> datas = fetchData(file, parameters);
+		for (BuildingAttributeData data : datas) {
+
+			try {
+				attributeDataProcessor.processData(beginDate, ApplicationConfig.getFutureInfinite(), data);
+			} catch (Exception e) {
+				log.warn("Failed importing building attributes", e);
+			}
+		}
+
+		return RESULT_NEXT;
+	}
+
+	private List<BuildingAttributeData> fetchData(File file, Map<Serializable, Serializable> parameters)
+		throws FlexPayException {
+
 		InputStream is = null;
 		try {
+			//noinspection IOResourceOpenedButNotSafelyClosed
 			is = new BufferedInputStream(new FileInputStream(file));
-			datas = attributesImporter.readAttributes(is);
+			return attributesImporter.readAttributes(is);
 		} catch (Exception e) {
 			throw new FlexPayException("Failed reading source file #" + parameters.get(PARAM_FILE_ID), e);
 		} finally {
 			IOUtils.closeQuietly(is);
 		}
-
-		for (BuildingAttributeData data : datas) {
-
-			try {
-//				attributeDataProcessor.processData();
-			} catch (Exception e) {
-
-			}
-		}
-
-		return RESULT_NEXT;
 	}
 
 	@Required
