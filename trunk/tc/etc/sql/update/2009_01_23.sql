f75f1d8ee5b845d5d2646afcce94995257e087ec
AlexuyT <AlexuyT@f554a4e9-133d-0410-ac35-5129da6e6cf8>
2009-02-05 16:22:12 +0000

    Исправлены ошибки в апдейтах БД для модуля TC

--- trunk/tc/etc/sql/update/2009_01_23.sql
+++ trunk/tc/etc/sql/update/2009_01_23.sql
@@ -1,7 +1,7 @@
 alter table tc_tariff_calc_rules_files_tbl drop foreign key FKDA48352F62EB9A29;
 alter table tc_tariff_calc_rules_files_tbl drop column status_id;
 
-delete from common_file_statuses_tbl where module_id in (select (id) from common_flexpay_modules where name = 'tc');
+delete from common_file_statuses_tbl where module_id in (select (id) from common_flexpay_modules_tbl where name = 'tc');
 
 update common_file_types_tbl set file_mask='*.{8}\\u002E(d|D)(r|R)(l|L)' where name = 'tc.file_type.tariff_rules';
 
