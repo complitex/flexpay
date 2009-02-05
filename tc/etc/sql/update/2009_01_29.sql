c5a4a7f5867cb8a58c0d6272be83d96691fd7fad
mfedko <mfedko@f554a4e9-133d-0410-ac35-5129da6e6cf8>
2009-02-05 16:10:36 +0000

    db name removed from alter table

--- tc/etc/sql/update/2009_01_29.sql
+++ tc/etc/sql/update/2009_01_29.sql
@@ -1,5 +1,5 @@
 
-ALTER TABLE `flexpay_db`.`tc_tariff_calculation_result_tbl`
-MODIFY COLUMN `calculation_date` DATE NOT NULL COMMENT 'Calculation result calculation date';
+ALTER TABLE tc_tariff_calculation_result_tbl
+MODIFY COLUMN calculation_date DATE NOT NULL COMMENT 'Calculation result calculation date';
 
 update common_version_tbl set last_modified_date='2009-01-29', date_version=0;
