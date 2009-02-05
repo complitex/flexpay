52aecdddedb013e55910c507a866859486082c4e
AlexuyT <AlexuyT@f554a4e9-133d-0410-ac35-5129da6e6cf8>
2009-02-05 16:22:12 +0000

    Исправлены ошибки в апдейтах БД для модуля TC

--- tc/etc/sql/update/2009_01_22_1.sql
+++ tc/etc/sql/update/2009_01_22_1.sql
@@ -50,6 +50,9 @@ alter table tc_tariff_calc_rules_files_tbl
     foreign key (type_id)
     references common_file_types_tbl (id);
 
+INSERT INTO common_flexpay_modules_tbl (name) VALUES ('tc');
+SELECT @module_tc:=last_insert_id();
+
 INSERT INTO common_file_types_tbl (name, file_mask, code, module_id)
 	VALUES ('tc.file_type.tariff_rules', '.{8}\\u002E(d|D)(r|R)(l|L)', 1, @module_tc);
 
