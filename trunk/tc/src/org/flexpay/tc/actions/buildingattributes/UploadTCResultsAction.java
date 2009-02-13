b94a6db78157bfe9493d09b94ed386b3e96d52f6
semenov.alexei <semenov.alexei@f554a4e9-133d-0410-ac35-5129da6e6cf8>
2009-02-13 18:43:25 +0000

    FP-347
    Initial urgent(!) implementation. Should be improved.

---
+++ trunk/tc/src/org/flexpay/tc/actions/buildingattributes/UploadTCResultsAction.java
@@ -0,0 +1,53 @@
+package org.flexpay.tc.actions.buildingattributes;
+
+import org.flexpay.common.actions.FPActionSupport;
+import org.flexpay.common.process.ProcessManager;
+import org.flexpay.common.util.CollectionUtils;
+import org.flexpay.tc.process.TariffCalcResultExportForBuildingJob;
+import org.jetbrains.annotations.NotNull;
+import org.springframework.beans.factory.annotation.Required;
+
+import java.io.Serializable;
+import java.util.Map;
+
+public class UploadTCResultsAction extends FPActionSupport {
+
+    private String buildingId;
+    private String calculationDate;    
+
+    private ProcessManager processManager;
+
+    @NotNull
+    protected String doExecute() throws Exception {
+
+        Map<Serializable, Serializable> contextVariables = CollectionUtils.map();
+        contextVariables.put(TariffCalcResultExportForBuildingJob.CALCULATION_DATE, calculationDate);
+        contextVariables.put(TariffCalcResultExportForBuildingJob.BUILDING_ID, buildingId);
+
+        processManager.createProcess("TariffCalcResultExportForBuildingProcess", contextVariables);
+
+        log.info("TariffCalcResultExportForBuildingProcess started with parameters {} and {}", calculationDate, buildingId);
+
+        return REDIRECT_SUCCESS;
+    }
+
+    @NotNull
+    protected String getErrorResult() {
+        return INPUT;
+    }
+
+    // form fields setters
+    public void setBuildingId(String buildingId) {
+        this.buildingId = buildingId;
+    }
+
+    public void setCalculationDate(String calculationDate) {
+        this.calculationDate = calculationDate;
+    }
+
+    // spring bean properties
+    @Required
+    public void setProcessManager(ProcessManager processManager) {
+        this.processManager = processManager;
+    }
+}
