af41c26985e4b98faee209bb2dc4ab71b95e533f
semenov.alexei <semenov.alexei@f554a4e9-133d-0410-ac35-5129da6e6cf8>
2009-02-13 11:43:34 +0000

    FP-317
    Lazy init failure finally fixed

--- trunk/bti/src/org/flexpay/bti/dao/impl/BtiBuildingDaoExtImpl.java
+++ trunk/bti/src/org/flexpay/bti/dao/impl/BtiBuildingDaoExtImpl.java
@@ -7,58 +7,67 @@ import org.flexpay.ab.persistence.Town;
 import org.flexpay.common.persistence.Stub;
 import org.springframework.dao.support.DataAccessUtils;
 import org.springframework.orm.hibernate3.support.HibernateDaoSupport;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.util.List;
+import java.util.Collections;
 
 public class BtiBuildingDaoExtImpl extends HibernateDaoSupport implements BtiBuildingDaoExt {
 
-	public BtiBuilding readBuildinWithAttributes(Long buildingId) {
+    public BtiBuilding readBuildinWithAttributes(Long buildingId) {
 
-		BtiBuilding building = (BtiBuilding) DataAccessUtils.uniqueResult(getHibernateTemplate()
-				.findByNamedQuery("BtiBuilding.find", buildingId));
+        BtiBuilding building = (BtiBuilding) DataAccessUtils.uniqueResult(getHibernateTemplate()
+                .findByNamedQuery("BtiBuilding.find", buildingId));
 
-		return setupAttributes(building);
-	}
+        return setupAttributes(building);
+    }
 
-	public BtiBuilding readBuildinWithAttributesByAddress(Long addressId) {
-		BtiBuilding building = (BtiBuilding) DataAccessUtils.uniqueResult(getHibernateTemplate()
-				.findByNamedQuery("BtiBuilding.findByAddress", addressId));
-		return setupAttributes(building);
-	}
+    public BtiBuilding readBuildinWithAttributesByAddress(Long addressId) {
+        BtiBuilding building = (BtiBuilding) DataAccessUtils.uniqueResult(getHibernateTemplate()
+                .findByNamedQuery("BtiBuilding.findByAddress", addressId));
+        return setupAttributes(building);
+    }
 
-	private BtiBuilding setupAttributes(BtiBuilding building) {
+    private BtiBuilding setupAttributes(BtiBuilding building) {
 
-		if (building == null) {
-			return null;
-		}
+        if (building == null) {
+            return null;
+        }
 
-		List<?> simpleAttrs = getHibernateTemplate()
-				.findByNamedQuery("BuildingAttributeBase.readSimpleAttributes", building.getId());
-		addAttributes(building, simpleAttrs);
-		List<?> tmpAttrs = getHibernateTemplate()
-				.findByNamedQuery("BuildingAttributeBase.readTmpAttributes", building.getId());
-		addAttributes(building, tmpAttrs);
+        List<?> simpleAttrs = getHibernateTemplate()
+                .findByNamedQuery("BuildingAttributeBase.readSimpleAttributes", building.getId());
+        addAttributes(building, simpleAttrs);
+        List<?> tmpAttrs = getHibernateTemplate()
+                .findByNamedQuery("BuildingAttributeBase.readTmpAttributes", building.getId());
+        addAttributes(building, tmpAttrs);
 
-		return building;
-	}
+        if (building.getAttributes().isEmpty()) {
+            building.setAttributes(Collections.EMPTY_SET);
+        }
 
-	private void addAttributes(BtiBuilding building, List<?> attributes) {
+        return building;
+    }
+
+    private void addAttributes(BtiBuilding building, List<?> attributes) {
+
         if (attributes == null || attributes.isEmpty()) {
             return;
         }
 
-		for (Object obj : attributes) {
-			BuildingAttributeBase attribute = (BuildingAttributeBase) obj;
-			building.addAttribute(attribute);
-		}
-	}
+        for (Object obj : attributes) {
+            BuildingAttributeBase attribute = (BuildingAttributeBase) obj;
+            building.addAttribute(attribute);
+        }
+    }
 
-	/**
-	 * Find all BtiBuilding in the town
-	 * @param town town to search
-	 * @return BtiBuilding list in town
-	 */
-	public List<BtiBuilding> findByTown(Stub<Town> town) {
-		return  (List<BtiBuilding>) getHibernateTemplate().findByNamedQuery("BtiBuilding.findByTown", town.getId());
+    /**
+     * Find all BtiBuilding in the town
+     *
+     * @param town town to search
+     * @return BtiBuilding list in town
+     */
+    public List<BtiBuilding> findByTown(Stub<Town> town) {
+        return (List<BtiBuilding>) getHibernateTemplate().findByNamedQuery("BtiBuilding.findByTown", town.getId());
 	}
 }
