8ce9f0f5c5981feca79e8cb4d77f59bbf1770840
alex.chistyakov <alex.chistyakov@f554a4e9-133d-0410-ac35-5129da6e6cf8>
2009-02-19 10:59:52 +0000

    remove tariff's attributes from db

---
+++ bti/etc/sql/update/2009_02_19.sql
@@ -0,0 +1,13 @@
+select @attribute_group_17:=17;
+
+delete from bti_building_attribute_temp_values_tbl
+where attribute_id in (select id from bti_building_attribute_types_tbl where group_id = @attribute_group_17);
+
+delete from bti_building_attribute_type_names_tbl
+where attribute_type_id in (select id from bti_building_attribute_types_tbl where group_id = @attribute_group_17);
+
+delete from bti_building_attribute_types_tbl where group_id = @attribute_group_17;
+
+delete from bti_building_attribute_type_group_names_tbl where group_id =@attribute_group_17;
+
+delete from bti_building_attribute_type_groups_tbl where id =@attribute_group_17;
