404d3db0caa8692633dec805e084151cd6e6ba04
alex.chistyakov <alex.chistyakov@f554a4e9-133d-0410-ac35-5129da6e6cf8>
2009-03-17 13:49:12 +0000

    FP-386 fixed returns codes messages and new return message code added

--- tc/src/org/flexpay/tc/persistence/TariffExportCode.java
+++ tc/src/org/flexpay/tc/persistence/TariffExportCode.java
@@ -7,31 +7,43 @@ import static org.flexpay.common.util.CollectionUtils.map;
 import java.util.Map;
 
 public class TariffExportCode extends DomainObject {
-
+    /**
+     * 1 - все нормально (в т. ч. когда услуги на доме нет, а в пытаемс€ добавить с 0 тарифом)
+0 - не найдена подуслуга в справочнике
+-1 - не найден дом
+-2 - ошибка записи тарифа в историю
+-3 - в ÷Ќ на доме тариф был, а при загрузке он занул€етс€
+-4 - ставка пуста€ или отрицательна€
+-5 - пуста€ дата вступлени€ в действие
+-6 - не найдена подуслуга на доме, а пытаемс€ добавить с ненулевым тарифом
+     */
 	public static final int EXPORTED = 1;
 	public static final int TARIFF_NOT_FOUND_FOR_BUILDING = 0;
 	public static final int BUILDING_NOT_FOUND = -1;
 	public static final int CANNOT_CREATE_HISTORY_RECORD = -2;
-	public static final int LOCK_EXCEPTION = -3;
+	public static final int NULL_NOT_NULL_TARIFF_VALUE = -3;
 	public static final int NEGATIVE_VALUE = -4;
 	public static final int BEGIN_DATE_IS_NULL = -5;
+	public static final int TARIFF_NOT_FOUND_FOR_BUILDING_WHILE_ADDING_NOT_NULL_TARIFF_VALUE = -6;
 	public static final int UNKNOWN_RESULT_CODE = -100;
 
 	private static final Map<Integer, String> typeToName = map(
-			ar(EXPORTED, TARIFF_NOT_FOUND_FOR_BUILDING, BUILDING_NOT_FOUND, CANNOT_CREATE_HISTORY_RECORD, LOCK_EXCEPTION, NEGATIVE_VALUE, BEGIN_DATE_IS_NULL, UNKNOWN_RESULT_CODE),
+			ar(EXPORTED, TARIFF_NOT_FOUND_FOR_BUILDING, BUILDING_NOT_FOUND, CANNOT_CREATE_HISTORY_RECORD,
+                    NULL_NOT_NULL_TARIFF_VALUE, NEGATIVE_VALUE, BEGIN_DATE_IS_NULL, UNKNOWN_RESULT_CODE, TARIFF_NOT_FOUND_FOR_BUILDING_WHILE_ADDING_NOT_NULL_TARIFF_VALUE),
 			ar(
 					"tc.tariff.export_code.exported",
 					"tc.tariff.export_code.tariff_not_found_for_building",
                     "tc.tariff.export_code.building_not_found",
 					"tc.tariff.export_code.cannot_create_history_record",
-					"tc.tariff.export_code.lock_exception",
+					"tc.tariff.export_code.null_not_null_tariff_value",
 					"tc.tariff.export_code.negative_value",
 					"tc.tariff.export_code.begin_date_is_null",
-					"tc.tariff.export_code.unknown_result_code"));
+					"tc.tariff.export_code.unknown_result_code",
+                    "tc.tariff.export_code.tariff_not_found_for_building_while_adding_not_null_tariff_value"));
 
 	private int code;
 
-	/**
+    /**
 	 * Constructs a new TariffExportCode.
 	 */
 	public TariffExportCode() {
