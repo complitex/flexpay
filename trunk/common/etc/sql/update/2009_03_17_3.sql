345e70d4c18606b11d88d1dd42b5c4d8a8c48ce5
alex.chistyakov <alex.chistyakov@f554a4e9-133d-0410-ac35-5129da6e6cf8>
2009-03-21 02:26:32 +0000

    version fix

--- trunk/common/etc/sql/update/2009_03_17_3.sql
+++ trunk/common/etc/sql/update/2009_03_17_3.sql
@@ -4,4 +4,4 @@ alter table common_history_records_tbl
 	add column new_bool_value bit comment 'Optional new bool value';
 
 
-update common_version_tbl set last_modified_date='2009-03-17', date_version=2;
+update common_version_tbl set last_modified_date='2009-03-17', date_version=3;
