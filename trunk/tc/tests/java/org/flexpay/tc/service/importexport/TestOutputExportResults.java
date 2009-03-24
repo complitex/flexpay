88e4046a875d89ab9795356351f652b2651bac8b
semenov.alexei <semenov.alexei@f554a4e9-133d-0410-ac35-5129da6e6cf8>
2009-03-24 07:07:17 +0000

    FP-445 external id is now written to csv result

--- trunk/tc/tests/java/org/flexpay/tc/service/importexport/TestOutputExportResults.java
+++ trunk/tc/tests/java/org/flexpay/tc/service/importexport/TestOutputExportResults.java
@@ -3,12 +3,17 @@ package org.flexpay.tc.service.importexport;
 import org.apache.commons.io.IOUtils;
 import org.flexpay.ab.persistence.Building;
 import org.flexpay.ab.persistence.District;
+import org.flexpay.ab.persistence.BuildingAddress;
 import org.flexpay.ab.service.AddressService;
 import org.flexpay.ab.service.BuildingService;
 import org.flexpay.ab.service.DistrictService;
 import static org.flexpay.common.persistence.Stub.stub;
 import org.flexpay.common.persistence.Stub;
+import org.flexpay.common.persistence.DataSourceDescription;
 import org.flexpay.common.test.SpringBeanAwareTestCase;
+import org.flexpay.common.exception.FlexPayException;
+import org.flexpay.common.service.importexport.CorrectionsService;
+import org.flexpay.common.service.importexport.ClassToTypeRegistry;
 import org.flexpay.tc.persistence.TariffCalculationResult;
 import org.flexpay.tc.persistence.TariffExportLogRecord;
 import org.flexpay.tc.persistence.TariffExportCode;
@@ -17,7 +22,9 @@ import org.flexpay.tc.service.TariffService;
 import org.flexpay.tc.service.TariffCalculationResultService;
 import org.junit.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.support.ReloadableResourceBundleMessageSource;
+import org.jetbrains.annotations.NotNull;
 
 import java.io.*;
 import java.text.SimpleDateFormat;
@@ -27,20 +34,35 @@ import java.math.BigDecimal;
 
 public class TestOutputExportResults extends SpringBeanAwareTestCase {
 
-    @Autowired
-    private BuildingService buildingService;
-    @Autowired
-    private DistrictService districtService;
-    @Autowired
-    private AddressService addressService;
-    @Autowired
-    private TariffService tariffService;
-    @Autowired
-    private TariffCalculationResultService tariffCalculationResultService;
-
     private String delimeter = ",";
 
     private String[] subserviceCodes = {"010", "020", "030", "040", "050", "060", "070", "080", "090", "100", "110", "120", "130", "140", "150", "160"};
+
+    private Long dataSourceDescriptionId = 1L;
+
+    // required services
+    @Autowired
+    private BuildingService buildingService;
+
+    @Autowired
+    private DistrictService districtService;
+
+    @Autowired
+    private AddressService addressService;
+
+    @Autowired
+    private TariffService tariffService;
+
+    @Autowired
+    private TariffCalculationResultService tariffCalculationResultService;
+
+    @Autowired
+    @Qualifier("correctionsServiceAb")
+    private CorrectionsService correctionsService;
+
+    @Autowired
+    @Qualifier("typeRegistryAb")
+    private ClassToTypeRegistry classToTypeRegistry;
 
     @Test
     public void testGenerateTariffs() throws Exception {
@@ -106,13 +128,13 @@ public class TestOutputExportResults extends SpringBeanAwareTestCase {
             IOUtils.closeQuietly(wr);
             IOUtils.closeQuietly(os);
         }
-        
+
         System.out.println("Results printed to file: " + tmpFile.getAbsolutePath());
     }
 
     private void writeCalculationResult(ReloadableResourceBundleMessageSource ms, Writer wr, TariffCalculationResult result) throws Exception {
         // формат записи в файл
-        // 1) Адрес дома (улица, номер дома и так далее) 2) название тарифа 3) значение тарифа 4) код тарифа  5) tariff export code
+        // 1) Адрес дома (улица, номер дома и так далее) 2) код дома в ЦН 3) название тарифа 4) значение тарифа 5) код тарифа 6) tariff export code
 
         Building building = result.getBuilding();
         String address = addressService.getBuildingAddress(stub(building), ApplicationConfig.getDefaultLocale());
@@ -121,9 +143,14 @@ public class TestOutputExportResults extends SpringBeanAwareTestCase {
         String i18nName = result.getLastTariffExportLogRecord().getTariffExportCode().getI18nName();
         Object[] params = {};
 
+        String externalId = getExternalId(stub(building));
+        externalId = (externalId == null) ? "" : externalId;
+        String cnId = externalId .equals("1") ? "" : externalId; // exclude cnId 1
+
         StringBuilder sb = new StringBuilder()
-                .append("\"").append(district.getCurrentName().getDefaultNameTranslation()).append("\"").append(delimeter)
+                .append("\"").append(district.getCurrentName().getDefaultNameTranslation()).append("\"").append(delimeter)                
                 .append("\"").append(address).append("\"").append(delimeter)
+                .append(cnId).append(delimeter)
                 .append("\"").append(result.getTariff().getDefultTranslation()).append("\"").append(delimeter)
                 .append(result.getValue()).append(delimeter)
                 .append(result.getTariff().getSubServiceCode()).append(delimeter)
@@ -141,6 +168,24 @@ public class TestOutputExportResults extends SpringBeanAwareTestCase {
                 return tcr;
             }
         }
+        return null;
+    }
+
+    private String getExternalId(@NotNull Stub<Building> buildingStub) throws FlexPayException {
+
+        Stub<DataSourceDescription> dataSourceDescriptionStub = new Stub<DataSourceDescription>(dataSourceDescriptionId);
+
+        List<BuildingAddress> buildingAddressList = buildingService.getBuildingBuildings(buildingStub);
+
+        for (BuildingAddress buildingAddress : buildingAddressList) {
+
+            String externalId = correctionsService.getExternalId(buildingAddress.getId(), classToTypeRegistry.getType(BuildingAddress.class), dataSourceDescriptionStub);
+
+            if (externalId != null) {
+                return externalId;
+            }
+        }
+
         return null;
     }
 }
