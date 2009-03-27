c8e6df2c7ba2873f09612627454a53712578af2f
mfedko <mfedko@f554a4e9-133d-0410-ac35-5129da6e6cf8>
2009-03-27 12:23:44 +0000

    FP-450
    init data for document types

---
+++ eirc/etc/sql/update/2009_03_27_1.sql
@@ -0,0 +1,9 @@
+-- add init data
+insert into accounting_document_types_tbl (id, code, name)
+	values (1, 1, 'accounting.document.type.cash_payment');
+insert into accounting_document_types_tbl (id, code, name)
+	values (2, 2, 'accounting.document.type.cash_payment_return');
+insert into accounting_document_types_tbl (id, code, name)
+	values (3, 3, 'accounting.document.type.cashless_payment');
+insert into accounting_document_types_tbl (id, code, name)
+	values (4, 4, 'accounting.document.type.cashless_payment_return');
