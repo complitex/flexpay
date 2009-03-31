b91d952c27fdc9b3b8679fd7dbeb5d0680dec797
mfedko <mfedko@f554a4e9-133d-0410-ac35-5129da6e6cf8>
2009-03-31 12:54:45 +0000

    FP-462
    Organisations module skeleton generated

---
+++ orgs/tests/java/org/flexpay/orgs/AllTests.java
@@ -0,0 +1,23 @@
+package org.flexpay.orgs;
+
+import junit.framework.Test;
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
+import junit.framework.JUnit4TestAdapter;
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+@RunWith(Suite.class)
+@Suite.SuiteClasses({
+		org.flexpay.orgs.action.AllTests.class,
+		org.flexpay.orgs.persistence.AllTests.class,
+		org.flexpay.orgs.service.AllTests.class
+		})
+public class AllTests extends TestCase {
+
+    public static Test suite() {
+        TestSuite suite = new TestSuite();
+        suite.addTest(new JUnit4TestAdapter(AllTests.class));
+        return suite;
+    }
+}
