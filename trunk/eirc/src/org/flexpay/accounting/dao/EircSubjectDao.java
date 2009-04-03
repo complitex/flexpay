726a26eb03ed13216bf8e9fcedffc09292428041
mfedko <mfedko@f554a4e9-133d-0410-ac35-5129da6e6cf8>
2009-04-03 09:13:48 +0000

    FP-461
    Payments module skeleton

--- trunk/eirc/src/org/flexpay/accounting/dao/EircSubjectDao.java
+++
@@ -1,8 +0,0 @@
-package org.flexpay.accounting.dao;
-
-import org.flexpay.common.dao.GenericDao;
-import org.flexpay.accounting.persistence.EircSubject;
-
-public interface EircSubjectDao extends GenericDao<EircSubject, Long> {
-
-}
--- trunk/eirc/src/org/flexpay/accounting/dao/OperationDao.java
+++
@@ -1,7 +0,0 @@
-package org.flexpay.accounting.dao;
-
-import org.flexpay.accounting.persistence.operations.Operation;
-import org.flexpay.common.dao.GenericDao;
-
-public interface OperationDao extends GenericDao<Operation, Long> {
-}
