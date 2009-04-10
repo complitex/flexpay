812000932903ea5176c69ab08dd25980887b9c99
mfedko <mfedko@f554a4e9-133d-0410-ac35-5129da6e6cf8>
2009-04-10 11:19:07 +0000

    FP-496
    Services and service types moved to payments

---
+++ trunk/payments/src/org/flexpay/payments/actions/service/ServiceTypeDeleteAction.java
@@ -0,0 +1,49 @@
+package org.flexpay.payments.actions.service;
+
+import org.flexpay.common.actions.FPActionSupport;
+import org.flexpay.payments.service.ServiceTypeService;
+import org.jetbrains.annotations.NotNull;
+import org.springframework.beans.factory.annotation.Required;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class ServiceTypeDeleteAction extends FPActionSupport {
+
+	private Set<Long> objectIds = new HashSet<Long>();
+
+	private ServiceTypeService serviceTypeService;
+
+	@NotNull
+	public String doExecute() throws Exception {
+
+		serviceTypeService.disable(objectIds);
+
+		return REDIRECT_SUCCESS;
+	}
+
+	/**
+	 * Get default error execution result
+	 * <p/>
+	 * If return code starts with a {@link #PREFIX_REDIRECT} all error messages are stored in a session
+	 *
+	 * @return {@link #ERROR} by default
+	 */
+	@NotNull
+	protected String getErrorResult() {
+		return REDIRECT_SUCCESS;
+	}
+
+	public Set<Long> getObjectIds() {
+		return objectIds;
+	}
+
+	public void setObjectIds(Set<Long> objectIds) {
+		this.objectIds = objectIds;
+	}
+
+	@Required
+	public void setServiceTypeService(ServiceTypeService serviceTypeService) {
+		this.serviceTypeService = serviceTypeService;
+	}
+}
