98df0661e551e718813514339733640fb7b0e90a
semenov.alexei <semenov.alexei@f554a4e9-133d-0410-ac35-5129da6e6cf8>
2009-04-13 04:30:19 +0000

    bad SQL statement eliminated

--- trunk/payments/etc/sql/init_db.sql
+++ trunk/payments/etc/sql/init_db.sql
@@ -1,4 +1,2 @@
 -- put here module initialization data
-INSERT INTO common_flexpay_modules_tbl (name) VALUES ('payments');
-
-insert into 
+INSERT INTO common_flexpay_modules_tbl (name) VALUES ('payments'); 
