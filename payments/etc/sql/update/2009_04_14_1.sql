d0a3cd5934735be8c403d4474ecb7a6d5f77f2ee
semenov.alexei <semenov.alexei@f554a4e9-133d-0410-ac35-5129da6e6cf8>
2009-04-14 10:06:25 +0000

    FP-480 SQL update lacked files added

--- payments/etc/sql/update/2009_04_14_1.sql
+++ payments/etc/sql/update/2009_04_14_1.sql
@@ -1,1 +1,3 @@
 ALTER TABLE payments_operations_tbl CHANGE COLUMN registerUser register_user varchar(255) comment 'Register username';
+
+update common_version_tbl set last_modified_date='2009-04-14', date_version=1;
