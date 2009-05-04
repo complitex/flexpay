e813f246d289f78d6a9e0489ea384b285b734a48
mfedko <mfedko@f554a4e9-133d-0410-ac35-5129da6e6cf8>
2009-05-04 15:29:59 +0000

    Test fixed

---
+++ trunk/tc/tests/java/org/flexpay/tc/service/importexport/TestImportTcBuildingAttributes.java
@@ -0,0 +1,63 @@
+package org.flexpay.tc.service.importexport;
+
+import org.apache.commons.io.IOUtils;
+import org.flexpay.bti.process.BuildingAttributesImportJob;
+import org.flexpay.bti.test.BtiSpringBeanAwareTestCase;
+import org.flexpay.bti.service.importexport.TestImportBuildingAttributes;
+import org.flexpay.common.persistence.FPFile;
+import org.flexpay.common.process.job.Job;
+import org.flexpay.common.service.FPFileService;
+import org.flexpay.common.util.CollectionUtils;
+import org.flexpay.common.util.DateUtil;
+import org.flexpay.common.util.FPFileUtil;
+import org.flexpay.tc.test.TcSpringBeanAwareTestCase;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.io.File;
+import java.io.InputStream;
+import java.io.Serializable;
+import java.util.Date;
+import java.util.Map;
+
+public class TestImportTcBuildingAttributes extends TcSpringBeanAwareTestCase {
+
+	@Autowired
+	private BuildingAttributesImportJob job;
+
+	@Autowired
+	private FPFileService fileService;
+
+	@Test
+	public void testImportCSV() throws Exception {
+
+		FPFile fpFile = new FPFile();
+		fpFile.setModule(fileService.getModuleByName("bti"));
+		fpFile.setCreationDate(new Date());
+		fpFile.setDescription("Building attributes file");
+		fpFile.setOriginalName("/org/flexpay/bti/service/importexport/26.12.2008.csv");
+		fpFile.setUserName("--TEST USER--");
+
+		InputStream is = getFileStream("org/flexpay/bti/service/importexport/26.12.2008.csv");
+
+		try {
+			File serverFile = FPFileUtil.saveToFileSystem(fpFile, is);
+			fpFile.setNameOnServer(serverFile.getName());
+			fpFile.setSize(serverFile.length());
+			fileService.create(fpFile);
+			log.info("File uploaded {}", fpFile);
+
+			assertFalse("saveToFileSystem failed, file is still new", fpFile.isNew());
+		} finally {
+			IOUtils.closeQuietly(is);
+		}
+
+		Map<Serializable, Serializable> params = CollectionUtils.map();
+		params.put(BuildingAttributesImportJob.PARAM_FILE_ID, fpFile.getId());
+		params.put(BuildingAttributesImportJob.PARAM_IMPORT_DATE, DateUtil.parseBeginDate("2008/12/26"));
+
+		assertEquals("Import failed", Job.RESULT_NEXT, job.execute(params));
+	}
+}
---
+++ trunk/tc/tests/java/org/flexpay/tc/test/TcSpringBeanAwareTestCase.java
@@ -0,0 +1,40 @@
+package org.flexpay.tc.test;
+
+import static org.flexpay.ab.service.Roles.*;
+import static org.flexpay.tc.service.Roles.*;
+import org.flexpay.common.test.SpringBeanAwareTestCase;
+import org.flexpay.common.util.SecurityUtil;
+import org.junit.BeforeClass;
+import org.springframework.security.Authentication;
+import org.springframework.security.GrantedAuthority;
+import org.springframework.security.context.SecurityContextHolder;
+import org.springframework.security.providers.anonymous.AnonymousAuthenticationToken;
+import org.springframework.security.userdetails.User;
+
+public class TcSpringBeanAwareTestCase extends SpringBeanAwareTestCase {
+
+	/**
+	 * Authenticate test user
+	 */
+	@BeforeClass
+	public static void authenticateTestUser() {
+		GrantedAuthority[] authorities = SecurityUtil.auths(
+				APARTMENT_READ,
+				BUILDING_ATTRIBUTE_TYPE_READ,
+				BUILDING_READ,
+				STREET_READ,
+				DISTRICT_READ,
+				TOWN_READ,
+				REGION_READ,
+				TOWN_TYPE_READ,
+				STREET_TYPE_READ,
+				STREET_TYPE_ADD,
+				TARIFF_READ,
+				TARIFF_CALCULATION_RESULT_READ,
+				TARIFF_CALCULATION_RESULT_ADD
+		);
+		User user = new User("test", "test", true, true, true, true, authorities);
+		Authentication auth = new AnonymousAuthenticationToken("key", user, authorities);
+		SecurityContextHolder.getContext().setAuthentication(auth);
+	}
+}
