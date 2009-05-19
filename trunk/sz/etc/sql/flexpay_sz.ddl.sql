87e83bf53a564bcc09f39678f1b81247e233d810
pavel.sknar <pavel.sknar@f554a4e9-133d-0410-ac35-5129da6e6cf8>
2009-05-19 09:14:03 +0000

    FP-505
    create payments_service_provider_attribute_tbl table

--- trunk/sz/etc/sql/flexpay_sz.ddl.sql
+++ trunk/sz/etc/sql/flexpay_sz.ddl.sql
@@ -1176,6 +1176,16 @@
         unique (language_id, service_id)
     );
 
+    create table payments_service_provider_attribute_tbl (
+        id bigint not null auto_increment comment 'Primary key',
+        version integer not null comment 'Optimistic lock version',
+        name varchar(50) not null comment 'Attribute name',
+        service_provider_id bigint not null comment 'Service provider reference',
+        value varchar(255) comment 'Attribute value',
+        primary key (id),
+        unique (name, service_provider_id)
+    ) comment='ServiceProviderAttribute';
+
     create table payments_service_type_name_translations_tbl (
         id bigint not null auto_increment,
         name varchar(255) not null,
@@ -2471,6 +2481,12 @@
         add constraint FK_payments_service_desciption_language 
         foreign key (language_id) 
         references common_languages_tbl (id);
+
+    alter table payments_service_provider_attribute_tbl 
+        add index FK_payments_service_provider_attribute_tbl_service_provider_id (service_provider_id), 
+        add constraint FK_payments_service_provider_attribute_tbl_service_provider_id 
+        foreign key (service_provider_id) 
+        references orgs_service_providers_tbl (id);
 
     alter table payments_service_type_name_translations_tbl 
         add index FK_payments_service_type_name_translation_service_type (service_type_id), 
