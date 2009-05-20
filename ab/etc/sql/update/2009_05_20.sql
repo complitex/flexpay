e0753a981a2587c48610b1fbea76b9eac20c168f
AlexuyT <AlexuyT@f554a4e9-133d-0410-ac35-5129da6e6cf8>
2009-05-20 07:20:14 +0000

    FP-589 (продолжение) Обработка реестра корректировок от МегаБанка

---
+++ ab/etc/sql/update/2009_05_20.sql
@@ -0,0 +1,6 @@
+
+alter table ab_apartments_tbl
+  add column apartment_type varchar(255) not null comment 'Class hierarchy descriminator, all apartments should have the same value',
+  comment='Apartments';
+
+update common_version_tbl set last_modified_date='2009-05-20', date_version=0;
