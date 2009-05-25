2e4aa0aa22191f406f70fbb0166a13d91bb9165e
semenov.alexei <semenov.alexei@f554a4e9-133d-0410-ac35-5129da6e6cf8>
2009-05-25 11:42:16 +0000

    FP-599 implementation changed to csv (does not actually work)

--- payments/src/org/flexpay/payments/actions/reports/PaymentsReportAction.java
+++ payments/src/org/flexpay/payments/actions/reports/PaymentsReportAction.java
@@ -59,41 +59,48 @@ public class PaymentsReportAction extends FPActionSupport implements PaymentPoin
 
 		reportContent = paymentsReporter.getPaymentsData(begin, end);
 
-// FIXME this stuff is commented out because doesn't actually work
-		List<PaymentReportData> datum = paymentsReporter.getPaymentsData(begin, end);
-//		file = new FPFile();
-//		FPModule module = fpFileService.getModuleByName("payments");
-//		if (module == null) {
-//			throw new Exception("Unknown module payments");
-//		}
-//		file.setModule(module);
-//		SimpleDateFormat df = new SimpleDateFormat("yyyyMMddHHmmss");
-//		file.setOriginalName("payments_" + df.format(begin) + "_" + df.format(end) + ".csv");
-//		file.setUserName(SecurityUtil.getUserName());
-//		FPFileUtil.createEmptyFile(file);
-//
-//		OutputStream os = file.getOutputStream();
-//		Writer w = null;
-//		try {
-//			//noinspection IOResourceOpenedButNotSafelyClosed
-//			w = new OutputStreamWriter(os, "UTF-8");
-//
-//			if (!datum.isEmpty()) {
-//				CSVWriter writer = new CSVWriter(w);
-//				for (PaymentReportData data : datum) {
-//					String[] row = {
-//							String.valueOf(data.getBankId()),
-//							String.valueOf(data.getServiceId()),
-//							String.valueOf(data.getTotalPayed())
-//					};
-//					writer.writeNext(row);
-//				}
-//				writer.close();
-//			}
-//		} finally {
-//			IOUtils.closeQuietly(w);
-//			IOUtils.closeQuietly(os);
-//		}
+		List<PaymentReportData> datum = reportContent;
+		file = new FPFile();
+		FPModule module = fpFileService.getModuleByName("payments");
+		if (module == null) {
+			throw new Exception("Unknown module payments");
+		}
+		file.setModule(module);
+		SimpleDateFormat df = new SimpleDateFormat("yyyyMMddHHmmss");
+		file.setOriginalName("payments_" + df.format(begin) + "_" + df.format(end) + ".csv");
+		file.setUserName(SecurityUtil.getUserName());
+		FPFileUtil.createEmptyFile(file);
+
+		OutputStream os = file.getOutputStream();
+		Writer w = null;
+		try {
+			//noinspection IOResourceOpenedButNotSafelyClosed
+			w = new OutputStreamWriter(os, "UTF-8");
+
+			if (!datum.isEmpty()) {
+				CSVWriter writer = new CSVWriter(w);
+				int documentCount = 1;
+
+				for (PaymentReportData data : datum) {
+					String[] row = {
+							String.valueOf(documentCount++),
+							String.valueOf(data.getPaymentPointId()),
+							String.valueOf(data.getOperationCount()),
+							String.valueOf(data.getOperationId()),
+							String.valueOf(data.getServiceProviderAccount()),
+							String.valueOf(data.getFio()),
+							String.valueOf(data.getServiceTypeCode()),
+							String.valueOf(data.getDocumentSumm()),
+							String.valueOf(data.getDocumentId())							
+					};
+					writer.writeNext(row);
+				}
+				writer.close();
+			}
+		} finally {
+			IOUtils.closeQuietly(w);
+			IOUtils.closeQuietly(os);
+		}
 
 		return SUCCESS;
 	}
