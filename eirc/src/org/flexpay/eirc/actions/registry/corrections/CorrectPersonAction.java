6430ec580e1176095af49db95a004494fb1c93b9
pavel.sknar <pavel.sknar@f554a4e9-133d-0410-ac35-5129da6e6cf8>
2009-06-03 09:28:15 +0000

    FP-614
    move registry UI from eirc to payments

---
+++ eirc/src/org/flexpay/eirc/actions/registry/corrections/CorrectPersonAction.java
@@ -0,0 +1,26 @@
+package org.flexpay.eirc.actions.registry.corrections;
+
+import org.flexpay.common.persistence.DataCorrection;
+import org.flexpay.common.persistence.DataSourceDescription;
+import org.flexpay.eirc.dao.importexport.RawConsumersDataSource;
+import org.flexpay.eirc.service.importexport.RawConsumerData;
+import org.springframework.beans.factory.annotation.Required;
+
+public class CorrectPersonAction extends org.flexpay.payments.actions.registry.corrections.CorrectPersonAction {
+
+	private RawConsumersDataSource consumersDataSource;
+
+    @Override
+    protected void saveCorrection(DataSourceDescription sd) {
+        RawConsumerData data = consumersDataSource.getById(String.valueOf(record.getId()));
+
+        // add correction for apartment
+        DataCorrection correction = correctionsService.getStub(data.getPersonFIOId(), object, sd);
+        correctionsService.save(correction);
+    }
+
+	@Required
+	public void setConsumersDataSource(RawConsumersDataSource consumersDataSource) {
+		this.consumersDataSource = consumersDataSource;
+	}
+}
