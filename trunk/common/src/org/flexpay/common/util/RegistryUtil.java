2244bc89937537b344059ffa9bfa5f564c23207e
AlexuyT <AlexuyT@f554a4e9-133d-0410-ac35-5129da6e6cf8>
2009-06-12 11:24:43 +0000

    Добавление в файлы экспорта реестров хидера и футера

--- trunk/common/src/org/flexpay/common/util/RegistryUtil.java
+++ trunk/common/src/org/flexpay/common/util/RegistryUtil.java
@@ -8,6 +8,16 @@ public class RegistryUtil {
 	public static final String CONTAINER_SEPARATOR = "|";
 
 	public static final String OPERATION_DATE_FORMAT = "ddMMyyyyHHmmss";
-	public static final String MESSAGE_TYPE = "♥";
+	public static final String REGISTRY_RECORD_MESSAGE_TYPE = "" + 0x03;
+
+	public static final String REGISTY_HEADER_MESSAGE_TYPE = "" + 0x0C;
+	public static final String REGISTY_FOOTER_MESSAGE_TYPE = "" + 0x0B;
+	public static final String REGISTRY_CREATION_DATE_FORMAT = "ddMMyyyyHHmmss";
+	public static final String REGISTRY_DATE_FROM_FORMAT = "ddMMyyyyHHmmss";
+	public static final String REGISTRY_DATE_TILL_FORMAT = "ddMMyyyyHHmmss";
+	
+	public static final String EXPORT_FILE_ENCODING = "CP1251";
+
+	public static final String EXPORT_FILE_NAME = "exportBankPayments.txt";
 
 }
