b0e3918ca301d4485bb7eb069e7d4d1afa00d6aa
alex.chistyakov <alex.chistyakov@f554a4e9-133d-0410-ac35-5129da6e6cf8>
2009-06-13 09:26:52 +0000

    FP-617 logger for assignment handlers

--- trunk/payments/src/org/flexpay/payments/process/handlers/PaymentCollectorAssignmentHandler.java
+++ trunk/payments/src/org/flexpay/payments/process/handlers/PaymentCollectorAssignmentHandler.java
@@ -4,14 +4,20 @@ import org.jbpm.taskmgmt.def.AssignmentHandler;
 import org.jbpm.taskmgmt.exe.Assignable;
 import org.jbpm.graph.exe.ExecutionContext;
 import org.flexpay.common.process.JobManagerAssignmentHandler;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 
 public class PaymentCollectorAssignmentHandler implements AssignmentHandler {
-    private static final long serialVersionUID = 1L;
+
+	private static final long serialVersionUID = 1L;
+	protected Logger log = LoggerFactory.getLogger(getClass());
+
     public final static String PAYMENT_COLLECTOR = "PAYMENT_COLLECTOR";
 
     public void assign(Assignable assignable, ExecutionContext executionContext) {
         String autoMode = (String) executionContext.getVariable("AUTO_MODE");
+		log.debug("AUTO_MODE = {}", autoMode);
         if (autoMode != null && autoMode.equals("true")) {
             assignable.setActorId(JobManagerAssignmentHandler.JOB_MANAGER_ACTOR_NAME);
         } else {
