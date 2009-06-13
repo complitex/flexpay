fa8edff72786641be5ce9a04ffcfa29c850b9eab
alex.chistyakov <alex.chistyakov@f554a4e9-133d-0410-ac35-5129da6e6cf8>
2009-06-13 09:26:52 +0000

    FP-617 logger for assignment handlers

--- payments/src/org/flexpay/payments/process/handlers/AccounterAssignmentHandler.java
+++ payments/src/org/flexpay/payments/process/handlers/AccounterAssignmentHandler.java
@@ -4,12 +4,16 @@ import org.jbpm.taskmgmt.def.AssignmentHandler;
 import org.jbpm.taskmgmt.exe.Assignable;
 import org.jbpm.graph.exe.ExecutionContext;
 import org.flexpay.common.process.JobManagerAssignmentHandler;
+import org.slf4j.LoggerFactory;
+import org.slf4j.Logger;
 
 public class AccounterAssignmentHandler implements AssignmentHandler {
+	protected Logger log = LoggerFactory.getLogger(getClass());
     private static final long serialVersionUID = 1L;
     public final static String ACCOUNTER = "ACCOUNTER";
     public void assign(Assignable assignable, ExecutionContext executionContext) {
         String autoMode = (String) executionContext.getVariable("AUTO_MODE");
+		log.debug("AUTO_MODE = {}", autoMode);
         if (autoMode != null && autoMode.equals("true")) {
             assignable.setActorId(JobManagerAssignmentHandler.JOB_MANAGER_ACTOR_NAME);
         } else {
