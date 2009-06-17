e23ce61afb632c867f8918f5141db5b38cabb95a
AlexuyT <AlexuyT@f554a4e9-133d-0410-ac35-5129da6e6cf8>
2009-06-17 05:59:12 +0000

    FP-637 Создание каркаса для нового модуля аренды (rent). Созданы папки, конфигурационные файлы, а также создан iml-файл для идеи

---
+++ rent/src/org/flexpay/rent/service/Roles.java
@@ -0,0 +1,12 @@
+package org.flexpay.rent.service;
+
+/**
+ * Set of predefined security roles for module
+ */
+public abstract class Roles {
+
+	public static final String PREFIX = "ROLE_" + "rent".toUpperCase() + "_";
+
+	public static final String BASIC = PREFIX + "BASIC";
+
+}
---
+++ rent/tests/java/org/flexpay/rent/persistence/AllTests.java
@@ -0,0 +1,11 @@
+package org.flexpay.rent.persistence;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+@RunWith(Suite.class)
+@Suite.SuiteClasses({
+		})
+public class AllTests {
+
+}
