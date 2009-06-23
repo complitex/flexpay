14d7975536b81af4e1e188eb21e31457f37b9861
mfedko <mfedko@f554a4e9-133d-0410-ac35-5129da6e6cf8>
2009-06-23 10:41:28 +0000

    SQL updates comments dump to STDOUT when building list

--- payments/etc/sql/update/2009_06_21_1.sql
+++ payments/etc/sql/update/2009_06_21_1.sql
@@ -1,8 +1,16 @@
+-- alter operations table
 alter table payments_operations_tbl
 	drop foreign key FK_payments_cashboxes_tbl_cashbox_id;
 
+-- drop old tables
 drop table payments_cashbox_name_translations_tbl;
 
+-- second drop 
 drop table payments_cashboxes_tbl;
 
 update common_version_tbl set last_modified_date='2009-06-21', date_version=1;
+
+/*
+ EOF
+*/
+
