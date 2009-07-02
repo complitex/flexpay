920442d9beaf909a8abab44ec394964e611e6bcd
mfedko <mfedko@f554a4e9-133d-0410-ac35-5129da6e6cf8>
2009-07-02 11:25:42 +0000

    Process logger test added (need to check flex 106 process logs disappearance)

---
+++ trunk/common/tests/java/org/flexpay/common/process/TestProcessLogger.java
@@ -0,0 +1,24 @@
+package org.flexpay.common.process;
+
+import org.flexpay.common.test.SpringBeanAwareTestCase;
+import org.junit.Test;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
+import org.slf4j.Logger;
+
+import java.io.File;
+
+public class TestProcessLogger extends SpringBeanAwareTestCase {
+
+	@Test
+	public void testGetLogger() throws Exception {
+		ProcessLogger.setThreadProcessId(-150L);
+		Logger plog = ProcessLogger.getLogger(getClass());
+
+		assertNotNull("Process logger retrival failed", plog);
+
+		plog.info("Hello from test!");
+		File logFile = ProcessLogger.getLogFile(-150L);
+		assertTrue("Log file not found", logFile.exists());
+	}
+}
