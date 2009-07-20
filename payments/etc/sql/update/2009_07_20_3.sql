3585bea994a6e14827832e00c62a47f56c16b402
mfedko <mfedko@f554a4e9-133d-0410-ac35-5129da6e6cf8>
2009-07-20 12:00:17 +0000

    sql updates dates fixed

--- payments/etc/sql/update/2009_07_20_3.sql
+++ payments/etc/sql/update/2009_07_20_3.sql
@@ -1,4 +1,4 @@
 alter table common_users_tbl
 	add column payments_payment_point_id bigint comment 'User payment point';
 
-update common_version_tbl set last_modified_date='2009-07-17', date_version=2;
+update common_version_tbl set last_modified_date='2009-07-20', date_version=3;
