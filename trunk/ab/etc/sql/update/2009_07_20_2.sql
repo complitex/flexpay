c691850309bd233f3fc1b20177e27a0b57864fb7
mfedko <mfedko@f554a4e9-133d-0410-ac35-5129da6e6cf8>
2009-07-20 12:00:17 +0000

    sql updates dates fixed

--- trunk/ab/etc/sql/update/2009_07_20_2.sql
+++ trunk/ab/etc/sql/update/2009_07_20_2.sql
@@ -3,4 +3,4 @@ alter table common_users_tbl
 	add column ab_region_filter varchar(255) comment 'Region filter',
 	add column ab_town_filter varchar(255) comment 'Town filter';
 
-update common_version_tbl set last_modified_date='2009-07-17', date_version=1;
+update common_version_tbl set last_modified_date='2009-07-20', date_version=2;
