eb7776fefcb5870a78326e077f3fdb75eb54e280
semenov.alexei <semenov.alexei@f554a4e9-133d-0410-ac35-5129da6e6cf8>
2009-07-21 03:31:56 +0000

    SQL update for 13/07/2009 (document additions)

--- payments/etc/sql/update/2009_07_13.sql
+++ payments/etc/sql/update/2009_07_13.sql
@@ -1,3 +1,10 @@
 alter table payments_document_addition_types_tbl add column code integer comment 'Document addition type code';
 
+insert into payments_document_addition_types_tbl (id, version, code)
+		values (1, 0, 1);
+insert into payments_document_addition_type_translations_tbl (name, language_id, type_id)
+		values ('ЕРЦ счёт', @ru_id, 1);
+insert into payments_document_addition_type_translations_tbl (name, language_id, type_id)
+		values ('ERC account', @en_id, 1);
+
 update common_version_tbl set last_modified_date='2009-07-13', date_version=0;
