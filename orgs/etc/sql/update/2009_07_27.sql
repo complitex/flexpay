c937b57b83913552a7b7c4942113f9cac31474c1
mfedko <mfedko@f554a4e9-133d-0410-ac35-5129da6e6cf8>
2009-07-27 12:20:56 +0000

    FP-638
    Invalid update fixed

--- orgs/etc/sql/update/2009_07_27.sql
+++ orgs/etc/sql/update/2009_07_27.sql
@@ -1,6 +1,6 @@
 select @orgs_base:=0x4000 + 0;
 select @payments_base:=0x3000 + 0;
-select @cashbox_type_old:=@payments_base + 0x006;
+select @cashbox_type_old:=@orgs_base + 0x006;
 select @cashbox_type_new:=@payments_base + 0x008;
 
 update common_data_corrections_tbl set object_type=@cashbox_type_new where object_type=@cashbox_type_old;
