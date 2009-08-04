1aaba096f91217261384b44d990451b5837c765d
mfedko <mfedko@f554a4e9-133d-0410-ac35-5129da6e6cf8>
2009-08-04 06:25:54 +0000

    invalid update fixed

--- payments/etc/sql/update/2009_08_04.sql
+++ payments/etc/sql/update/2009_08_04.sql
@@ -1,8 +1,13 @@
 -- AHTUNG!!! swap ids of service types КВАРПЛАТА(9) and ВОДООТВЕДЕНИЕ(1)
-select @maxId:=max(id) from payments_service_types_tbl;
-update payments_service_types_tbl set id=@maxId + 1 where id=1;
-update payments_service_types_tbl set id=1 where id=9;
-update payments_service_types_tbl set id=9 where id=@maxId + 1;
+SET FOREIGN_KEY_CHECKS = 0;
+
+update payments_service_types_tbl t, payments_service_type_name_translations_tbl n, payments_services_tbl s
+	set t.id=2000, n.service_type_id=2000, s.type_id=2000 where t.id=1 and n.service_type_id=1 and s.type_id=1;
+update ignore payments_service_types_tbl t, payments_service_type_name_translations_tbl n, payments_services_tbl s
+	set t.id=1, n.service_type_id=1, s.type_id=1 where t.id=9 and n.service_type_id=9 and s.type_id=9;
+update ignore payments_service_types_tbl t, payments_service_type_name_translations_tbl n, payments_services_tbl s
+	set t.id=9, n.service_type_id=9, s.type_id=9 where t.id=2000 and n.service_type_id=2000 and s.type_id=2000;
 
 update common_version_tbl set last_modified_date='2009-08-04', date_version=0;
+
 -- END AHTUNG!!!
