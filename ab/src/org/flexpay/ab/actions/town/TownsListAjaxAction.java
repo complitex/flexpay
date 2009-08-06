1d9b5ca412e700c68b084b8d1e8a112ec4a28ee4
mfedko <mfedko@f554a4e9-133d-0410-ac35-5129da6e6cf8>
2009-08-06 03:52:29 +0000

    missed update

--- ab/src/org/flexpay/ab/actions/town/TownsListAjaxAction.java
+++ ab/src/org/flexpay/ab/actions/town/TownsListAjaxAction.java
@@ -1,12 +1,16 @@
 package org.flexpay.ab.actions.town;
 
+import org.apache.commons.collections.ArrayStack;
 import org.apache.commons.lang.StringUtils;
-import org.flexpay.ab.persistence.Region;
 import org.flexpay.ab.persistence.Town;
-import org.flexpay.ab.persistence.TownName;
+import org.flexpay.ab.persistence.filters.RegionFilter;
+import org.flexpay.ab.persistence.sorter.TownSorterByName;
+import org.flexpay.ab.persistence.sorter.TownSorterByType;
 import org.flexpay.ab.service.TownService;
 import org.flexpay.common.actions.FPActionWithPagerSupport;
-import org.flexpay.common.persistence.Stub;
+import org.flexpay.common.persistence.DomainObject;
+import org.flexpay.common.persistence.sorter.ObjectSorter;
+import org.flexpay.common.util.CollectionUtils;
 import static org.flexpay.common.util.CollectionUtils.list;
 import org.jetbrains.annotations.NotNull;
 import org.springframework.beans.factory.annotation.Required;
@@ -18,6 +22,8 @@ public class TownsListAjaxAction extends FPActionWithPagerSupport<Town> {
 	private String regionId;
 	private List<Town> towns = list();
 
+	private TownSorterByName townSorterByName = new TownSorterByName();
+	private TownSorterByType townSorterByType = new TownSorterByType();
 	private TownService townService;
 
 	@NotNull
@@ -34,8 +40,19 @@ public class TownsListAjaxAction extends FPActionWithPagerSupport<Town> {
 			}
 		}
 
-		towns = townService.findSimple(new Stub<Region>(regionIdLong), getPager());
-		log.info("Total towns found: {}", towns);
+		townSorterByName.setLang(getLanguage());
+		townSorterByType.setLang(getLanguage());
+
+		ArrayStack filters = CollectionUtils.arrayStack(new RegionFilter(regionIdLong));
+		List<ObjectSorter> sorters = CollectionUtils.<ObjectSorter>list(townSorterByName, townSorterByType);
+		List<Town> townStubs = townService.find(filters, sorters, getPager());
+
+		if (log.isDebugEnabled()) {
+			log.info("Total towns found: {}", towns.size());
+		}
+
+		towns = townService.readFull(DomainObject.collectionIds(townStubs), true);
+
 
 		return SUCCESS;
 	}
@@ -59,6 +76,22 @@ public class TownsListAjaxAction extends FPActionWithPagerSupport<Town> {
 
 	public List<Town> getTowns() {
 		return towns;
+	}
+
+	public TownSorterByName getTownSorterByName() {
+		return townSorterByName;
+	}
+
+	public void setTownSorterByName(TownSorterByName townSorterByName) {
+		this.townSorterByName = townSorterByName;
+	}
+
+	public TownSorterByType getTownSorterByType() {
+		return townSorterByType;
+	}
+
+	public void setTownSorterByType(TownSorterByType townSorterByType) {
+		this.townSorterByType = townSorterByType;
 	}
 
 	@Required
