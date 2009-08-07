616767eb7fee960b1b892c67a6d9a00d8a50c674
mfedko <mfedko@f554a4e9-133d-0410-ac35-5129da6e6cf8>
2009-08-07 09:41:26 +0000

    Mixed case in object names

--- ab/tests/java/org/flexpay/ab/sort/TestHQLSortTowns.java
+++ ab/tests/java/org/flexpay/ab/sort/TestHQLSortTowns.java
@@ -3,15 +3,18 @@ package org.flexpay.ab.sort;
 import org.apache.commons.collections.ArrayStack;
 import org.flexpay.ab.persistence.TestData;
 import org.flexpay.ab.persistence.Town;
+import org.flexpay.ab.persistence.TownName;
 import org.flexpay.ab.persistence.filters.RegionFilter;
 import org.flexpay.ab.persistence.sorter.TownSorterByName;
 import org.flexpay.ab.persistence.sorter.TownSorterByType;
 import org.flexpay.ab.service.TownService;
 import org.flexpay.ab.test.AbSpringBeanAwareTestCase;
 import org.flexpay.common.dao.paging.Page;
+import static org.flexpay.common.persistence.Stub.stub;
 import org.flexpay.common.persistence.sorter.ObjectSorter;
 import org.flexpay.common.util.CollectionUtils;
 import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertNotNull;
 import org.junit.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 
@@ -31,6 +34,14 @@ public class TestHQLSortTowns extends AbSpringBeanAwareTestCase {
 		List<Town> towns = townService.find(filters, sorters, pager);
 
 		assertFalse("No towns found", towns.isEmpty());
+
+		for (Town townStub : towns) {
+			Town town = townService.readFull(stub(townStub));
+			assertNotNull("Town found in sorting, but not readFull", town);
+			TownName name = town.getCurrentName();
+			assertNotNull("Town name not found", name);
+			log.debug(name.getDefaultNameTranslation());
+		}
 	}
 
 	@Test
