93288bfefab5ce64d9c67320ccb06048520dcbf1
semenov.alexei <semenov.alexei@f554a4e9-133d-0410-ac35-5129da6e6cf8>
2009-08-13 10:53:43 +0000

    FP-768 Identity type delete action inports optimized

--- trunk/ab/src/org/flexpay/ab/actions/identity/IdentityTypeDeleteAction.java
+++ trunk/ab/src/org/flexpay/ab/actions/identity/IdentityTypeDeleteAction.java
@@ -1,18 +1,11 @@
 package org.flexpay.ab.actions.identity;
 
-import org.flexpay.ab.persistence.IdentityType;
 import org.flexpay.ab.service.IdentityTypeService;
-import org.flexpay.common.util.CollectionUtils;
-import static org.flexpay.common.util.CollectionUtils.set;
-import org.flexpay.common.persistence.Stub;
 import org.flexpay.common.actions.FPActionSupport;
-import org.springframework.beans.factory.annotation.Required;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import static org.flexpay.common.util.CollectionUtils.set;
 import org.jetbrains.annotations.NotNull;
+import org.springframework.beans.factory.annotation.Required;
 
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Set;
 
 public class IdentityTypeDeleteAction extends FPActionSupport {
