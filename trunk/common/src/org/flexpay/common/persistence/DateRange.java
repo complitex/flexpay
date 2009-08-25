5a7545c5b1d7f881a31cde63caecdbc6739301a4
mfedko <mfedko@f554a4e9-133d-0410-ac35-5129da6e6cf8>
2009-08-25 10:01:37 +0000

    Range class introduced

---
+++ trunk/common/src/org/flexpay/common/persistence/DateRange.java
@@ -0,0 +1,10 @@
+package org.flexpay.common.persistence;
+
+import java.util.Date;
+
+public class DateRange extends Range<Date> {
+
+	public DateRange(Date start, Date end) {
+		super(start, end);
+	}
+}
