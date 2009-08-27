930a2df70ed3b2caef2eb9e7d968b97a7700fb82
pavel.sknar <pavel.sknar@f554a4e9-133d-0410-ac35-5129da6e6cf8>
2009-08-27 10:20:45 +0000

    FP-788
    Fix sql script

--- common/etc/sql/update/2009_08_27.sql
+++ common/etc/sql/update/2009_08_27.sql
@@ -45,6 +45,7 @@ INSERT INTO common_registry_fpfiles_tbl (registry_id, fpfile_id, registry_fpfile
                 inner join common_registry_types_tbl rt on r.registry_type_id=rt.id
                 where rt.code!=12);
 
+alter table common_files_tbl drop index FK_common_registries_tbl_file_id;
 alter table common_registries_tbl drop column file_id;
 
 update common_version_tbl set last_modified_date='2009-08-27', date_version=0;
