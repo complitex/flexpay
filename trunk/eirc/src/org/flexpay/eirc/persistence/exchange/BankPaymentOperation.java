973d55ec384e5d543cad6fa23d9c05429cd8f376
pavel.sknar <pavel.sknar@f554a4e9-133d-0410-ac35-5129da6e6cf8>
2009-09-10 04:44:39 +0000

    FP-813 FP-814
    Обработка контейнера "простая оплата"

--- trunk/eirc/src/org/flexpay/eirc/persistence/exchange/BankPaymentOperation.java
+++ trunk/eirc/src/org/flexpay/eirc/persistence/exchange/BankPaymentOperation.java
@@ -15,21 +15,17 @@ import org.jetbrains.annotations.NotNull;
 import java.math.BigDecimal;
 import java.util.List;
 
-public class BankPaymentOperation extends ContainerOperation {
+public class BankPaymentOperation extends PaymentOperation {
 
-	private ServiceOperationsFactory factory;
-	private Long organizationId;
 	private Long operationId;
 	private BigDecimal summ;
 
 	public BankPaymentOperation(ServiceOperationsFactory factory, List<String> datum)
 		throws InvalidContainerException {
 
-		super(Integer.valueOf(datum.get(0)));
-		this.factory = factory;
+		super(factory, datum);
 
 		try {
-			organizationId = Long.parseLong(datum.get(1));
 			operationId = Long.parseLong(datum.get(2));
 			summ = new BigDecimal(datum.get(3));
 		} catch (Exception e) {
@@ -37,117 +33,11 @@ public class BankPaymentOperation extends ContainerOperation {
 		}
 	}
 
-	/**
-	 * Process operation
-	 *
-	 * @param context ProcessingContext
-	 * @return DelayedUpdate object
-	 * @throws org.flexpay.common.exception.FlexPayException
-	 *          if failure occurs
-	 * @throws org.flexpay.common.exception.FlexPayExceptionContainer
-	 *          if failure occurs
-	 */
-	@Override
-	public DelayedUpdate process(@NotNull ProcessingContext context)
-			throws FlexPayException, FlexPayExceptionContainer {
+    Long getOperationId(ProcessingContext context) {
+        return operationId;
+    }
 
-		PaymentOperationDelayedUpdate update = getUpdate(context);
-
-		RegistryRecord record = context.getCurrentRecord();
-		EircRegistryProperties props = (EircRegistryProperties) context.getRegistry().getProperties();
-
-		Document document = new Document();
-		setDocumentType(context, document);
-		document.setDocumentStatus(factory.getDocumentStatusService().read(DocumentStatus.REGISTERED));
-		document.setSumm(record.getAmount());
-		document.setService(factory.getConsumerService().findService(
-				props.getServiceProviderStub(), record.getServiceCode()));
-		document.setCreditorOrganization(props.getSender());
-		document.setDebtorOrganization(props.getRecipient());
-		document.setDebtorId(record.getPersonalAccountExt());
-
-		document.setLastName(record.getLastName());
-		document.setMiddleName(record.getMiddleName());
-		document.setFirstName(record.getFirstName());
-		document.setTown(record.getCity());
-		document.setStreetType(record.getStreetType());
-		document.setStreetName(record.getStreetName());
-		document.setBuildingNumber(record.getBuildingNum());
-		document.setBuildingBulk(record.getBuildingBulkNum());
-		document.setApartmentNumber(record.getApartmentNum());
-
-		update.addDocument(document);
-
-		return DelayedUpdateNope.INSTANCE;
-	}
-
-	private PaymentOperationDelayedUpdate getUpdate(ProcessingContext context) throws FlexPayException {
-
-		final PaymentOperationDelayedUpdate[] holder = {null};
-		// find update first
-		context.visitOperationUpdates(new DelayedUpdateVisitor() {
-			@Override
-			public void apply(DelayedUpdate update) {
-				if (update instanceof PaymentOperationDelayedUpdate) {
-					holder[0] = (PaymentOperationDelayedUpdate) update;
-				}
-			}
-		});
-
-		PaymentOperationDelayedUpdate update = holder[0];
-		if (update == null) {
-			update = new PaymentOperationDelayedUpdate(factory.getOperationService());
-			update.setOperationId(operationId);
-			org.flexpay.payments.persistence.Operation operation = update.getOperation();
-			operation.setOperationSumm(summ);
-			setOperationType(context, operation);
-			operation.setOperationLevel(factory.getOperationLevelService().read(OperationLevel.AVERAGE));
-			operation.setOperationStatus(factory.getOperationStatusService().read(OperationStatus.REGISTERED));
-
-			Organization org = factory.getOrganizationService().readFull(new Stub<Organization>(organizationId));
-			if (org == null) {
-				throw new FlexPayException("Unknown organization id: " + organizationId);
-			}
-			update.getOperation().setCreatorOrganization(org);
-
-			RegistryRecord record = context.getCurrentRecord();
-			update.getOperation().setCreationDate(record.getOperationDate());
-
-			context.addUpdate(update);
-		}
-
-		return update;
-	}
-
-	private void setOperationType(ProcessingContext context, Operation operation) throws FlexPayException {
-
-		RegistryType type = context.getRegistry().getRegistryType();
-		if (type.isCashPayments()) {
-			operation.setOperationType(factory.getOperationTypeService().read(OperationType.SERVICE_CASH_PAYMENT));
-			return;
-		}
-
-		if (type.isCashlessPayments()) {
-			operation.setOperationType(factory.getOperationTypeService().read(OperationType.SERVICE_CASHLESS_PAYMENT));
-			return;
-		}
-
-		throw new FlexPayException("Invalid registry type for bank payments container: #" + type.getCode());
-	}
-
-	private void setDocumentType(ProcessingContext context, Document document) throws FlexPayException {
-
-		RegistryType type = context.getRegistry().getRegistryType();
-		if (type.isCashPayments()) {
-			document.setDocumentType(factory.getDocumentTypeService().read(DocumentType.CASH_PAYMENT));
-			return;
-		}
-
-		if (type.isCashlessPayments()) {
-			document.setDocumentType(factory.getDocumentTypeService().read(DocumentType.CASHLESS_PAYMENT));
-			return;
-		}
-
-		throw new FlexPayException("Invalid registry type for bank payments container: #" + type.getCode());
-	}
+    BigDecimal getOperationSum(ProcessingContext context) {
+        return summ;
+    }
 }
