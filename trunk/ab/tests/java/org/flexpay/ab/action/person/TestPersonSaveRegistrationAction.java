44d1521c30c3ef0ecd291dcb0259bf6859be7808
mfedko <mfedko@f554a4e9-133d-0410-ac35-5129da6e6cf8>
2009-09-18 07:27:58 +0000

    FP-776
    Fixed person registration setup algorithm.

--- trunk/ab/tests/java/org/flexpay/ab/action/person/TestPersonSaveRegistrationAction.java
+++ trunk/ab/tests/java/org/flexpay/ab/action/person/TestPersonSaveRegistrationAction.java
@@ -1,12 +1,14 @@
 package org.flexpay.ab.action.person;
 
 import org.flexpay.ab.actions.person.PersonSaveRegistrationAction;
-import org.flexpay.ab.actions.buildings.BuildingDeleteAction;
+import org.flexpay.ab.persistence.Apartment;
 import org.flexpay.ab.persistence.Person;
 import org.flexpay.ab.test.AbSpringBeanAwareTestCase;
-import org.junit.Test;
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import org.junit.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.annotation.Repeat;
 
 public class TestPersonSaveRegistrationAction extends AbSpringBeanAwareTestCase {
 
@@ -14,6 +16,7 @@ public class TestPersonSaveRegistrationAction extends AbSpringBeanAwareTestCase 
 	private PersonSaveRegistrationAction action;
 
 	@Test
+	@Repeat (5)
 	public void testSaveRegistration() throws Exception {
 
 		Person person = new Person(1L);
@@ -22,14 +25,17 @@ public class TestPersonSaveRegistrationAction extends AbSpringBeanAwareTestCase 
 		action.setBeginDate("2009/01/12");
 		action.setEndDate("2019/03/25");
 
-		assertEquals("Invalid action result", BuildingDeleteAction.SUCCESS, action.execute());
-		assertEquals("Incorrect set apartment id", 1, action.getPerson().getRegistrationApartment().getId().longValue());
+		assertEquals("Invalid action result", PersonSaveRegistrationAction.SUCCESS, action.execute());
+		Apartment apartment = action.getPerson().getRegistrationApartment();
+		assertNotNull("No registration set", apartment);
+		assertEquals("Incorrect set apartment id", Long.valueOf(1L), apartment.getId());
 
 		action.setApartmentFilter(3L);
 
-		assertEquals("Invalid action result", BuildingDeleteAction.SUCCESS, action.execute());
-		assertEquals("Incorrect update apartment id", 3, action.getPerson().getRegistrationApartment().getId().longValue());
+		assertEquals("Invalid action result", PersonSaveRegistrationAction.SUCCESS, action.execute());
+		apartment = action.getPerson().getRegistrationApartment();
+		assertNotNull("No registration set 2", apartment);
 
+		assertEquals("Incorrect update apartment id", Long.valueOf(3), apartment.getId());
 	}
-
 }
