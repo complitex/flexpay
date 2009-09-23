df0cfdebb6f03a15fce8edc4f192fdf5a0dc852d
semenov.alexei <semenov.alexei@f554a4e9-133d-0410-ac35-5129da6e6cf8>
2009-09-23 08:08:01 +0000

    FP-835 trading day magic constants removed

--- trunk/payments/tests/java/org/flexpay/payments/action/TestConfirmationTradingDay.java
+++ trunk/payments/tests/java/org/flexpay/payments/action/TestConfirmationTradingDay.java
@@ -1,21 +1,22 @@
 package org.flexpay.payments.action;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.flexpay.common.process.*;
-import org.flexpay.common.process.exception.ProcessInstanceException;
+import static junit.framework.Assert.assertEquals;
+import static junit.framework.Assert.assertTrue;
+import org.flexpay.common.process.ProcessManager;
+import org.flexpay.common.process.TaskHelper;
 import org.flexpay.common.process.exception.ProcessDefinitionException;
+import org.flexpay.common.process.exception.ProcessInstanceException;
 import org.flexpay.common.test.SpringBeanAwareTestCase;
-import org.flexpay.payments.process.handlers.AccounterAssignmentHandler;
 import org.flexpay.payments.actions.ConfirmationTradingDayServlet;
+import org.flexpay.payments.process.export.TradingDay;
+import org.flexpay.payments.process.handlers.AccounterAssignmentHandler;
 import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 
 import java.io.Serializable;
-import java.util.Map;
 import java.util.HashMap;
+import java.util.Map;
 import java.util.Set;
-
-import static junit.framework.Assert.assertTrue;
-import static junit.framework.Assert.assertEquals;
 
 public class TestConfirmationTradingDay extends SpringBeanAwareTestCase {
 
@@ -38,7 +39,7 @@ public class TestConfirmationTradingDay extends SpringBeanAwareTestCase {
         Set transitions = TaskHelper.getTransitions(processManager, AccounterAssignmentHandler.ACCOUNTER, processId, "Пометить на закрытие", log);
 
         org.flexpay.common.process.Process process = processManager.getProcessInstanceInfo(processId);
-        String  currentStatus = (String) process.getParameters().get("PROCESS_STATUS");
+        String  currentStatus = (String) process.getParameters().get(TradingDay.PROCESS_STATUS);
 
         assertEquals("Ожидает подтверждения", currentStatus);
 
