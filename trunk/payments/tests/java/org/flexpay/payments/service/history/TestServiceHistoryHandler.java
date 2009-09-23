96f0cee7593681639288b853e646c4cdc5f9c1db
semenov.alexei <semenov.alexei@f554a4e9-133d-0410-ac35-5129da6e6cf8>
2009-09-23 10:43:38 +0000

    FP-819: test for test service history handler

---
+++ trunk/payments/tests/java/org/flexpay/payments/service/history/TestServiceHistoryHandler.java
@@ -0,0 +1,25 @@
+package org.flexpay.payments.service.history;
+
+import org.flexpay.common.persistence.history.Diff;
+import org.flexpay.payments.persistence.Service;
+import org.flexpay.payments.service.importexport.impl.ClassToTypeRegistryPayments;
+import org.flexpay.payments.test.PaymentsSpringBeanAwareTestCase;
+import static org.junit.Assert.assertTrue;
+import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+
+public class TestServiceHistoryHandler extends PaymentsSpringBeanAwareTestCase {
+
+	@Autowired
+	private ServiceHistoryHandler serviceHistoryHandler;
+
+	private ClassToTypeRegistryPayments typeRegistry = new ClassToTypeRegistryPayments();
+
+	@Test
+	public void testSupports() {
+
+		Diff serviceDiff = new Diff();
+		serviceDiff.setObjectType(typeRegistry.getType(Service.class));
+		assertTrue("Handler must support this class: " + Service.class, serviceHistoryHandler.supports(serviceDiff));
+	}
+}
