7fbf8dfa6a6fbf223b020290b499ef3ee1b1e444
mfedko <mfedko@f554a4e9-133d-0410-ac35-5129da6e6cf8>
2009-09-28 05:55:14 +0000

    openSSO login sample

---
+++ trunk/common/etc/util/groovy/OpenSSOAuth.groovy
@@ -0,0 +1,25 @@
+//----------------------------------------------------------------------------------
+// using HtmlUnit (htmlunit.sf.net)
+
+
+import com.gargoylesoftware.htmlunit.WebClient
+import com.gargoylesoftware.htmlunit.html.HtmlPage
+
+def webClient = new WebClient()
+def page = webClient.getPage('http://dev.flexpay.complitex:8080/eirc/confirmationTradingDay?paymentPointId=1000&data=2009-06-24') as HtmlPage
+
+def response = page.getWebResponse()
+println "${response.statusCode} ${response.statusMessage} ${response.requestUrl}"
+
+def form1 = page.getFormByName('frm1')
+form1.getInputByName('IDToken1').setValueAttribute('developer')
+def form2 = page.getFormByName('frm2')
+form2.getInputByName('IDToken2').setValueAttribute('developer')
+
+def jsResult = page.executeJavaScript('LoginSubmit("Log In")')
+
+def newPage = jsResult.getNewPage()
+response = newPage.getWebResponse()
+println "${response.statusCode} ${response.statusMessage} ${response.requestUrl}"
+
+webClient.closeAllWindows()
