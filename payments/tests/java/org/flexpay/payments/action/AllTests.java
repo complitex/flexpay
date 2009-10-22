8fbf61fead20fbeee07530f8c402485bb95fed33
pavel.sknar <pavel.sknar@f554a4e9-133d-0410-ac35-5129da6e6cf8>
2009-10-22 11:23:55 +0000

    add TestPaymentPointsListMonitorAction to executable list

--- payments/tests/java/org/flexpay/payments/action/AllTests.java
+++ payments/tests/java/org/flexpay/payments/action/AllTests.java
@@ -6,6 +6,7 @@ import org.junit.runners.Suite;
 @RunWith(Suite.class)
 @Suite.SuiteClasses({
 		org.flexpay.payments.action.reports.AllTests.class,
+        org.flexpay.payments.action.monitor.AllTests.class,
 		TestRecievedPaymentsReportAction.class
 		})
 public class AllTests {
---
+++ payments/tests/java/org/flexpay/payments/action/monitor/AllTests.java
@@ -0,0 +1,12 @@
+package org.flexpay.payments.action.monitor;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+@RunWith(Suite.class)
+@Suite.SuiteClasses ({
+        TestPaymentPointsListMonitorAction.class
+})
+public class AllTests {
+
+}
