bd628717973790afef1a9108c7f6f1e9c524785a
mfedko <mfedko@f554a4e9-133d-0410-ac35-5129da6e6cf8>
2009-10-28 10:45:14 +0000

    Base class fixed for test

--- trunk/bti/tests/java/org/flexpay/bti/service/TestApartmentAttribute.java
+++ trunk/bti/tests/java/org/flexpay/bti/service/TestApartmentAttribute.java
@@ -1,54 +1,66 @@
 package org.flexpay.bti.service;
 
 import org.flexpay.ab.persistence.Apartment;
+import org.flexpay.bti.persistence.apartment.ApartmentAttribute;
 import org.flexpay.bti.persistence.apartment.ApartmentAttributeConfig;
 import org.flexpay.bti.persistence.apartment.ApartmentAttributeType;
 import org.flexpay.bti.persistence.apartment.BtiApartment;
-import org.flexpay.bti.persistence.apartment.ApartmentAttribute;
+import org.flexpay.bti.test.BtiSpringBeanAwareTestCase;
 import org.flexpay.common.persistence.Stub;
-import org.flexpay.common.test.TransactionalSpringBeanAwareTestCase;
 import org.flexpay.common.util.DateUtil;
-import static org.junit.Assert.assertNotNull;
-import static org.junit.Assert.assertEquals;
 import org.junit.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.annotation.NotTransactional;
+import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.TransactionStatus;
 
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
-public class TestApartmentAttribute extends TransactionalSpringBeanAwareTestCase {
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+
+public class TestApartmentAttribute extends BtiSpringBeanAwareTestCase {
 
 	@Autowired
 	private BtiApartmentService apartmentService;
 	@Autowired
 	private ApartmentAttributeTypeService attributeTypeService;
+	@Autowired
+	private PlatformTransactionManager txManager;
 
 	@Test
 	public void testSetApartmentAttributeTx() throws Exception {
 
-		BtiApartment apartment = apartmentService.readWithAttributes(new Stub<Apartment>(1L));
-		assertNotNull("Apartment not found", apartment);
+		TransactionStatus txStatus = txManager.getTransaction(null);
+		try {
+			BtiApartment apartment = apartmentService.readWithAttributes(new Stub<Apartment>(1L));
+			assertNotNull("Apartment not found", apartment);
 
-		ApartmentAttributeType type = attributeTypeService.findTypeByName(ApartmentAttributeConfig.ATTR_LIVE_SQUARE);
-		assertNotNull("Attribute not found", type);
+			ApartmentAttributeType type = attributeTypeService.findTypeByName(ApartmentAttributeConfig.ATTR_LIVE_SQUARE);
+			assertNotNull("Attribute not found", type);
 
-		ApartmentAttribute attribute = new ApartmentAttribute();
-		attribute.setAttributeType(type);
-		attribute.setStringValue("123.23");
-		apartment.setNormalAttribute(attribute);
-		apartmentService.updateAttributes(apartment);
+			ApartmentAttribute attribute = new ApartmentAttribute();
+			attribute.setAttributeType(type);
+			attribute.setStringValue("123.23");
+			apartment.setNormalAttribute(attribute);
+			apartmentService.updateAttributes(apartment);
 
-		Date beginDate = new SimpleDateFormat("yyyy-MM-dd").parse("2009-05-25");
-		attribute = new ApartmentAttribute();
-		attribute.setAttributeType(type);
-		attribute.setStringValue("123.34");
-		apartment.setTmpAttributeForDate(attribute, beginDate);
-		apartmentService.updateAttributes(apartment);
+			Date beginDate = new SimpleDateFormat("yyyy-MM-dd").parse("2009-05-25");
+			attribute = new ApartmentAttribute();
+			attribute.setAttributeType(type);
+			attribute.setStringValue("123.34");
+			apartment.setTmpAttributeForDate(attribute, beginDate);
+			apartmentService.updateAttributes(apartment);
 
-		ApartmentAttribute persisted = apartment.getAttributeForDate(type, DateUtil.nextMonth(beginDate));
-		assertNotNull("just set attribute not found", persisted);
-		assertEquals("Invalid attribute setup", "123.34", persisted.getStringValue());
+			ApartmentAttribute persisted = apartment.getAttributeForDate(type, DateUtil.nextMonth(beginDate));
+			assertNotNull("just set attribute not found", persisted);
+			assertEquals("Invalid attribute setup", "123.34", persisted.getStringValue());
+
+			txManager.commit(txStatus);
+		} catch (Throwable t) {
+			txManager.rollback(txStatus);
+		}
 	}
 
 	@Test
