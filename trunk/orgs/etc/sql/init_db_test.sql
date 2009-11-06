33d3d2a28243a9742010a807a7ab6c37ac836042
mfedko <mfedko@f554a4e9-133d-0410-ac35-5129da6e6cf8>
2009-11-06 09:05:54 +0000

    added test data for organizations and two service provider + services

--- trunk/orgs/etc/sql/init_db_test.sql
+++ trunk/orgs/etc/sql/init_db_test.sql
@@ -1,17 +1,6 @@
 -- put here module test initialization data
--- Init organizations
--- EIRC is the first one, ID=1
--- CN is the fourth one, ID=4
-INSERT INTO common_data_source_descriptions_tbl (description) VALUES ('EIRC DS');
-select @sd:=last_insert_id();
-INSERT INTO orgs_organizations_tbl (id, status, version, juridical_address, postal_address, individual_tax_number, kpp, data_source_description_id)
-	VALUES (1, 0, 0, 'ADDR', 'ADDR', '-------', '123', @sd);
-SELECT @organization_eirc:=1;
-INSERT INTO orgs_organization_descriptions_tbl (name, language_id, organization_id)
-	VALUES ('Eirc itself', @ru_id, @organization_eirc);
-INSERT INTO orgs_organization_names_tbl (name, language_id, organization_id)
-	VALUES ('EIRC', @ru_id, @organization_eirc);
 
+-- Init organizations
 INSERT INTO common_data_source_descriptions_tbl (description) VALUES ('ЖКО DS');
 select @sd:=last_insert_id();
 INSERT INTO orgs_organizations_tbl (id, status, version, juridical_address, postal_address, individual_tax_number, kpp, data_source_description_id)
