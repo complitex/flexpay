7d6cf83a1c2e0e08a2aa5e2798e087be06d95586
pavel.sknar <pavel.sknar@f554a4e9-133d-0410-ac35-5129da6e6cf8>
2009-11-12 08:29:15 +0000

    fix tests: use "set" collection type

--- trunk/ab/tests/java/org/flexpay/ab/action/building/TestBuildingDeleteAction.java
+++ trunk/ab/tests/java/org/flexpay/ab/action/building/TestBuildingDeleteAction.java
@@ -2,10 +2,11 @@ package org.flexpay.ab.action.building;
 
 import org.flexpay.ab.actions.buildings.BuildingDeleteAction;
 import org.flexpay.ab.test.AbSpringBeanAwareTestCase;
-import static org.flexpay.common.util.CollectionUtils.list;
-import static org.junit.Assert.assertEquals;
 import org.junit.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+
+import static org.flexpay.common.util.CollectionUtils.set;
+import static org.junit.Assert.assertEquals;
 
 public class TestBuildingDeleteAction extends AbSpringBeanAwareTestCase {
 
@@ -15,7 +16,7 @@ public class TestBuildingDeleteAction extends AbSpringBeanAwareTestCase {
 	@Test
 	public void testDeleteBuildings() throws Exception {
 
-		action.setObjectIds(list(2L));
+		action.setObjectIds(set(2L));
 
 		assertEquals("Invalid action result", BuildingDeleteAction.SUCCESS, action.execute());
 	}
