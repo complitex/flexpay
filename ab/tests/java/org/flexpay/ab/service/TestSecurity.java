824b8ddd25507a8ff4e6d9aa84e85246ad20cbd5
mfedko <mfedko@f554a4e9-133d-0410-ac35-5129da6e6cf8>
2009-11-16 08:47:47 +0000

    filter added for regions finder

--- ab/tests/java/org/flexpay/ab/service/TestSecurity.java
+++ ab/tests/java/org/flexpay/ab/service/TestSecurity.java
@@ -3,9 +3,11 @@ package org.flexpay.ab.service;
 import org.apache.commons.collections.ArrayStack;
 import org.flexpay.ab.persistence.Country;
 import org.flexpay.ab.persistence.Region;
+import org.flexpay.ab.persistence.filters.CountryFilter;
 import org.flexpay.ab.persistence.sorter.CountrySorter;
 import org.flexpay.ab.persistence.sorter.RegionSorter;
 import org.flexpay.ab.test.AbSpringBeanAwareTestCase;
+import org.flexpay.ab.util.config.ApplicationConfig;
 import org.flexpay.common.dao.paging.Page;
 import org.flexpay.common.persistence.sorter.ObjectSorter;
 import org.flexpay.common.util.CollectionUtils;
@@ -79,7 +81,7 @@ public class TestSecurity extends AbSpringBeanAwareTestCase {
 		Authentication auth = new AnonymousAuthenticationToken("key", user, REGION_AUTHORITIES);
 		SecurityContextHolder.getContext().setAuthentication(auth);
 
-		ArrayStack filters = CollectionUtils.arrayStack();
+		ArrayStack filters = CollectionUtils.arrayStack(new CountryFilter(ApplicationConfig.getDefaultCountryStub()));
 		List<ObjectSorter> sorters = CollectionUtils.list(new RegionSorter().activate());
 		Page<Region> pager = new Page<Region>();
 		regionService.find(filters, sorters, pager);
