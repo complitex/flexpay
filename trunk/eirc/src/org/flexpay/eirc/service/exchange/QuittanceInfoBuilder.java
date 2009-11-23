7e2bc4eb6bc8dd9e3abedf59de1e059e159842e6
mfedko <mfedko@f554a4e9-133d-0410-ac35-5129da6e6cf8>
2009-11-23 09:13:29 +0000

    FP-983
    some logging added

--- trunk/eirc/src/org/flexpay/eirc/service/exchange/QuittanceInfoBuilder.java
+++ trunk/eirc/src/org/flexpay/eirc/service/exchange/QuittanceInfoBuilder.java
@@ -4,225 +4,232 @@ import org.flexpay.ab.persistence.Apartment;
 import org.flexpay.ab.persistence.Person;
 import org.flexpay.ab.service.ApartmentService;
 import org.flexpay.common.persistence.Stub;
-import static org.flexpay.common.persistence.Stub.stub;
 import org.flexpay.common.service.importexport.MasterIndexService;
 import org.flexpay.common.util.CollectionUtils;
-import org.flexpay.common.util.SecurityUtil;
-import org.flexpay.eirc.persistence.ConsumerInfo;
-import org.flexpay.eirc.persistence.QuittancePayment;
-import org.flexpay.eirc.persistence.QuittanceDetailsPayment;
 import org.flexpay.eirc.persistence.Consumer;
-import org.flexpay.eirc.persistence.consumer.ConsumerAttributeTypeBase;
-import org.flexpay.eirc.persistence.consumer.ConsumerAttribute;
+import org.flexpay.eirc.persistence.ConsumerInfo;
+import org.flexpay.eirc.persistence.QuittanceDetailsPayment;
+import org.flexpay.eirc.persistence.QuittancePayment;
 import org.flexpay.eirc.persistence.account.Quittance;
 import org.flexpay.eirc.persistence.account.QuittanceDetails;
+import org.flexpay.eirc.persistence.consumer.ConsumerAttribute;
+import org.flexpay.eirc.persistence.consumer.ConsumerAttributeTypeBase;
 import org.flexpay.eirc.process.QuittanceNumberService;
-import org.flexpay.eirc.service.QuittanceService;
-import org.flexpay.eirc.service.QuittancePaymentService;
 import org.flexpay.eirc.service.ConsumerAttributeTypeService;
-import static org.flexpay.payments.persistence.quittance.QuittanceDetailsResponse.QuittanceInfo;
-import static org.flexpay.payments.persistence.quittance.QuittanceDetailsResponse.QuittanceInfo.ServiceDetails;
-import org.flexpay.payments.persistence.quittance.QuittanceDetailsResponse.ServiceAttribute;
-import org.flexpay.payments.persistence.quittance.ConsumerAttributes;
+import org.flexpay.eirc.service.QuittancePaymentService;
+import org.flexpay.eirc.service.QuittanceService;
 import org.flexpay.payments.persistence.Service;
+import org.flexpay.payments.persistence.quittance.ConsumerAttributes;
+import org.flexpay.payments.persistence.quittance.QuittanceDetailsResponse.ServiceAttribute;
 import org.flexpay.payments.service.SPService;
-import org.jetbrains.annotations.Nullable;
 import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Required;
 
-import java.util.List;
 import java.math.BigDecimal;
+import java.util.List;
+
+import static org.flexpay.common.persistence.Stub.stub;
+import static org.flexpay.payments.persistence.quittance.QuittanceDetailsResponse.QuittanceInfo;
+import static org.flexpay.payments.persistence.quittance.QuittanceDetailsResponse.QuittanceInfo.ServiceDetails;
 
 public class QuittanceInfoBuilder {
 
-	private QuittanceService quittanceService;
-	private QuittanceNumberService quittanceNumberService;
-	private MasterIndexService masterIndexService;
-	private ApartmentService apartmentService;
-	private QuittancePaymentService quittancePaymentService;
-	private ConsumerAttributeTypeService attributeTypeService;
-	private SPService spService;
+    private Logger log = LoggerFactory.getLogger(getClass());
 
-	@Nullable
-	public QuittanceInfo buildInfo(Stub<Quittance> stub) throws Exception {
+    private QuittanceService quittanceService;
+    private QuittanceNumberService quittanceNumberService;
+    private MasterIndexService masterIndexService;
+    private ApartmentService apartmentService;
+    private QuittancePaymentService quittancePaymentService;
+    private ConsumerAttributeTypeService attributeTypeService;
+    private SPService spService;
 
-		Quittance q = quittanceService.readFull(stub);
-		if (q == null) {
-			return null;
-		}
+    @Nullable
+    public QuittanceInfo buildInfo(Stub<Quittance> stub) throws Exception {
 
-		QuittanceInfo info = new QuittanceInfo();
-		info.setQuittanceNumber(quittanceNumberService.getNumber(q));
-		info.setAccountNumber(q.getAccountNumber());
-		info.setCreationDate(q.getCreationDate());
-		info.setDateFrom(q.getDateFrom());
-		info.setDateTill(q.getDateTill());
-		info.setOrderNumber(q.getOrderNumber());
+        Quittance q = quittanceService.readFull(stub);
+        if (q == null) {
+            return null;
+        }
 
-		ConsumerInfo consumerInfo = q.getEircAccount().getConsumerInfo();
+        QuittanceInfo info = new QuittanceInfo();
+        info.setQuittanceNumber(quittanceNumberService.getNumber(q));
+        info.setAccountNumber(q.getAccountNumber());
+        info.setCreationDate(q.getCreationDate());
+        info.setDateFrom(q.getDateFrom());
+        info.setDateTill(q.getDateTill());
+        info.setOrderNumber(q.getOrderNumber());
 
-		Apartment apartment = q.getEircAccount().getApartment();
-		apartment = apartment != null ? apartmentService.readFull(stub(apartment)) : null;
-		if (apartment != null) {
-			info.setApartmentMasterIndex(masterIndexService.getMasterIndex(apartment));
-		} else {
-			info.setApartmentNumber(consumerInfo.getApartmentNumber());
-			info.setBuildingNumber(consumerInfo.getBuildingNumber());
-			info.setBuildingBulk(consumerInfo.getBuildingBulk());
-			info.setStreetName(consumerInfo.getStreetName());
-			info.setStreetType(consumerInfo.getStreetTypeName());
-			info.setTown(consumerInfo.getCityName());
-		}
+        ConsumerInfo consumerInfo = q.getEircAccount().getConsumerInfo();
 
-		Person person = q.getEircAccount().getPerson();
-		if (person != null) {
-			info.setPersonMasterIndex(masterIndexService.getMasterIndex(person));
-		} else {
-			info.setPersonFirstName(consumerInfo.getFirstName());
-			info.setPersonMiddleName(consumerInfo.getMiddleName());
-			info.setPersonLastName(consumerInfo.getLastName());
-		}
+        Apartment apartment = q.getEircAccount().getApartment();
+        apartment = apartment != null ? apartmentService.readFull(stub(apartment)) : null;
+        if (apartment != null) {
+            info.setApartmentMasterIndex(masterIndexService.getMasterIndex(apartment));
+        } else {
+            info.setApartmentNumber(consumerInfo.getApartmentNumber());
+            info.setBuildingNumber(consumerInfo.getBuildingNumber());
+            info.setBuildingBulk(consumerInfo.getBuildingBulk());
+            info.setStreetName(consumerInfo.getStreetName());
+            info.setStreetType(consumerInfo.getStreetTypeName());
+            info.setTown(consumerInfo.getCityName());
+        }
 
-		List<QuittancePayment> payments = quittancePaymentService.getQuittancePayments(stub(q));
+        Person person = q.getEircAccount().getPerson();
+        if (person != null) {
+            info.setPersonMasterIndex(masterIndexService.getMasterIndex(person));
+        } else {
+            info.setPersonFirstName(consumerInfo.getFirstName());
+            info.setPersonMiddleName(consumerInfo.getMiddleName());
+            info.setPersonLastName(consumerInfo.getLastName());
+        }
 
-		info.setTotalToPay(getTotalPayable(q));
-		info.setTotalPayed(getTotalPayed(q, payments));
+        List<QuittancePayment> payments = quittancePaymentService.getQuittancePayments(stub(q));
 
-		List<ServiceDetails> detailses = CollectionUtils.list();
-		// prepare summs to pay
-		for (QuittanceDetails details : q.getQuittanceDetails()) {
+        info.setTotalToPay(getTotalPayable(q));
+        info.setTotalPayed(getTotalPayed(q, payments));
 
-			Consumer consumer = details.getConsumer();
-			Service service = consumer.getService();
+        List<ServiceDetails> detailses = CollectionUtils.list();
+        // prepare summs to pay
+        if (log.isDebugEnabled()) {
+            log.debug("Total detailses: {}", q.getQuittanceDetails().size());
+        }
+        for (QuittanceDetails details : q.getQuittanceDetails()) {
 
-			ServiceDetails serviceDetails = new ServiceDetails();
-			serviceDetails.setServiceMasterIndex(masterIndexService.getMasterIndex(service));
-			serviceDetails.setServiceProviderAccount(details.getConsumer().getExternalAccountNumber());
-			serviceDetails.setIncomingBalance(details.getIncomingBalance());
-			serviceDetails.setOutgoingBalance(details.getOutgoingBalance());
-			serviceDetails.setAmount(details.getAmount());
-			serviceDetails.setExpence(details.getExpence());
-			serviceDetails.setRate(details.getRate());
-			serviceDetails.setRecalculation(details.getRecalculation());
-			serviceDetails.setBenifit(details.getBenifit());
-			serviceDetails.setSubsidy(details.getSubsidy());
-			serviceDetails.setPayment(details.getPayment());
-			serviceDetails.setPayed(getPayedSumm(details, payments));
+            Consumer consumer = details.getConsumer();
+            Service service = consumer.getService();
 
-			ConsumerInfo cinfo = consumer.getConsumerInfo();
-			serviceDetails.setFirstName(cinfo.getFirstName());
-			serviceDetails.setMiddleName(cinfo.getMiddleName());
-			serviceDetails.setLastName(cinfo.getMiddleName());
-			serviceDetails.setTown(cinfo.getCityName());
-			serviceDetails.setStreetName(cinfo.getStreetName());
-			serviceDetails.setStreetType(cinfo.getStreetTypeName());
-			serviceDetails.setBuildingNumber(cinfo.getBuildingNumber());
-			serviceDetails.setBuildingBulk(cinfo.getBuildingBulk());
-			serviceDetails.setApartmentNumber(cinfo.getApartmentNumber());
+            ServiceDetails serviceDetails = new ServiceDetails();
+            serviceDetails.setServiceMasterIndex(masterIndexService.getMasterIndex(service));
+            serviceDetails.setServiceProviderAccount(details.getConsumer().getExternalAccountNumber());
+            serviceDetails.setIncomingBalance(details.getIncomingBalance());
+            serviceDetails.setOutgoingBalance(details.getOutgoingBalance());
+            serviceDetails.setAmount(details.getAmount());
+            serviceDetails.setExpence(details.getExpence());
+            serviceDetails.setRate(details.getRate());
+            serviceDetails.setRecalculation(details.getRecalculation());
+            serviceDetails.setBenifit(details.getBenifit());
+            serviceDetails.setSubsidy(details.getSubsidy());
+            serviceDetails.setPayment(details.getPayment());
+            serviceDetails.setPayed(getPayedSumm(details, payments));
 
-			serviceDetails.setAttributes(getConsumerAttributes(consumer));
+            ConsumerInfo cinfo = consumer.getConsumerInfo();
+            serviceDetails.setFirstName(cinfo.getFirstName());
+            serviceDetails.setMiddleName(cinfo.getMiddleName());
+            serviceDetails.setLastName(cinfo.getMiddleName());
+            serviceDetails.setTown(cinfo.getCityName());
+            serviceDetails.setStreetName(cinfo.getStreetName());
+            serviceDetails.setStreetType(cinfo.getStreetTypeName());
+            serviceDetails.setBuildingNumber(cinfo.getBuildingNumber());
+            serviceDetails.setBuildingBulk(cinfo.getBuildingBulk());
+            serviceDetails.setApartmentNumber(cinfo.getApartmentNumber());
 
-			detailses.add(serviceDetails);
-		}
+            serviceDetails.setAttributes(getConsumerAttributes(consumer));
 
-		info.setDetailses(detailses.toArray(new ServiceDetails[detailses.size()]));
+            detailses.add(serviceDetails);
+        }
 
-		return info;
-	}
+        info.setDetailses(detailses.toArray(new ServiceDetails[detailses.size()]));
 
-	public ServiceAttribute[] getConsumerAttributes(Consumer consumer) {
-		List<ServiceAttribute> attrs = CollectionUtils.list();
-		for (String attrCode : ConsumerAttributes.PAYMENT_ATTRIBUTES) {
-			ConsumerAttributeTypeBase type = attributeTypeService.readByCode(attrCode);
-			if (type == null) {
-				throw new IllegalStateException("Cannot find attribute type by code: " + attrCode);
-			}
-			ConsumerAttribute attribute = consumer.getAttribute(type);
-			if (attribute == null) {
-				continue;
-			}
-			attrs.add(new ServiceAttribute(attrCode, String.valueOf(attribute.value())));
-		}
+        return info;
+    }
 
-		return attrs.toArray(new ServiceAttribute[attrs.size()]);
-	}
+    public ServiceAttribute[] getConsumerAttributes(Consumer consumer) {
+        List<ServiceAttribute> attrs = CollectionUtils.list();
+        for (String attrCode : ConsumerAttributes.PAYMENT_ATTRIBUTES) {
+            ConsumerAttributeTypeBase type = attributeTypeService.readByCode(attrCode);
+            if (type == null) {
+                throw new IllegalStateException("Cannot find attribute type by code: " + attrCode);
+            }
+            ConsumerAttribute attribute = consumer.getAttribute(type);
+            if (attribute == null) {
+                continue;
+            }
+            attrs.add(new ServiceAttribute(attrCode, String.valueOf(attribute.value())));
+        }
 
-	public BigDecimal getTotalPayed(Quittance quittance, List<QuittancePayment> payments) {
+        return attrs.toArray(new ServiceAttribute[attrs.size()]);
+    }
 
-		BigDecimal summ = new BigDecimal("0.00");
-		for (QuittanceDetails details : quittance.getQuittanceDetails()) {
-			if (getService(details).isNotSubservice()) {
-				BigDecimal summPayed = getPayedSumm(details, payments);
-				summ = summ.add(summPayed);
-			}
-		}
+    public BigDecimal getTotalPayed(Quittance quittance, List<QuittancePayment> payments) {
 
-		return summ;
-	}
+        BigDecimal summ = new BigDecimal("0.00");
+        for (QuittanceDetails details : quittance.getQuittanceDetails()) {
+            if (getService(details).isNotSubservice()) {
+                BigDecimal summPayed = getPayedSumm(details, payments);
+                summ = summ.add(summPayed);
+            }
+        }
 
-	@NotNull
-	public BigDecimal getPayedSumm(QuittanceDetails qd, List<QuittancePayment> payments) {
-		BigDecimal summ = new BigDecimal("0.00");
-		for (QuittancePayment payment : payments) {
-			QuittanceDetailsPayment detailsPayment = payment.getPayment(qd);
-			if (detailsPayment != null) {
-				summ = summ.add(detailsPayment.getAmount());
-			}
-		}
+        return summ;
+    }
 
-		return summ;
-	}
+    @NotNull
+    public BigDecimal getPayedSumm(QuittanceDetails qd, List<QuittancePayment> payments) {
+        BigDecimal summ = new BigDecimal("0.00");
+        for (QuittancePayment payment : payments) {
+            QuittanceDetailsPayment detailsPayment = payment.getPayment(qd);
+            if (detailsPayment != null) {
+                summ = summ.add(detailsPayment.getAmount());
+            }
+        }
 
-	public BigDecimal getTotalPayable(Quittance quittance) {
+        return summ;
+    }
 
-		BigDecimal total = new BigDecimal("0.00");
+    public BigDecimal getTotalPayable(Quittance quittance) {
 
-		for (QuittanceDetails qd : quittance.getQuittanceDetails()) {
-			if (getService(qd).isNotSubservice()) {
-				total = total.add(qd.getOutgoingBalance());
-			}
-		}
+        BigDecimal total = new BigDecimal("0.00");
 
-		return total;
-	}
+        for (QuittanceDetails qd : quittance.getQuittanceDetails()) {
+            if (getService(qd).isNotSubservice()) {
+                total = total.add(qd.getOutgoingBalance());
+            }
+        }
 
-	private Service getService(QuittanceDetails qd) {
-		Consumer consumer = qd.getConsumer();
-		return spService.readFull(consumer.getServiceStub());
-	}
+        return total;
+    }
 
-	@Required
-	public void setQuittanceService(QuittanceService quittanceService) {
-		this.quittanceService = quittanceService;
-	}
+    private Service getService(QuittanceDetails qd) {
+        Consumer consumer = qd.getConsumer();
+        return spService.readFull(consumer.getServiceStub());
+    }
 
-	@Required
-	public void setQuittanceNumberService(QuittanceNumberService quittanceNumberService) {
-		this.quittanceNumberService = quittanceNumberService;
-	}
+    @Required
+    public void setQuittanceService(QuittanceService quittanceService) {
+        this.quittanceService = quittanceService;
+    }
 
-	@Required
-	public void setMasterIndexService(MasterIndexService masterIndexService) {
-		this.masterIndexService = masterIndexService;
-	}
+    @Required
+    public void setQuittanceNumberService(QuittanceNumberService quittanceNumberService) {
+        this.quittanceNumberService = quittanceNumberService;
+    }
 
-	@Required
-	public void setApartmentService(ApartmentService apartmentService) {
-		this.apartmentService = apartmentService;
-	}
+    @Required
+    public void setMasterIndexService(MasterIndexService masterIndexService) {
+        this.masterIndexService = masterIndexService;
+    }
 
-	@Required
-	public void setQuittancePaymentService(QuittancePaymentService quittancePaymentService) {
-		this.quittancePaymentService = quittancePaymentService;
-	}
+    @Required
+    public void setApartmentService(ApartmentService apartmentService) {
+        this.apartmentService = apartmentService;
+    }
 
-	@Required
-	public void setAttributeTypeService(ConsumerAttributeTypeService attributeTypeService) {
-		this.attributeTypeService = attributeTypeService;
-	}
+    @Required
+    public void setQuittancePaymentService(QuittancePaymentService quittancePaymentService) {
+        this.quittancePaymentService = quittancePaymentService;
+    }
 
-	@Required
-	public void setSpService(SPService spService) {
-		this.spService = spService;
-	}
+    @Required
+    public void setAttributeTypeService(ConsumerAttributeTypeService attributeTypeService) {
+        this.attributeTypeService = attributeTypeService;
+    }
+
+    @Required
+    public void setSpService(SPService spService) {
+        this.spService = spService;
+    }
 }
