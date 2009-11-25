a22cc8d2a0cf5962a7cf30941b14832fb95df48e
pavel.sknar <pavel.sknar@f554a4e9-133d-0410-ac35-5129da6e6cf8>
2009-11-25 10:35:05 +0000

    FP-981
    Update view registry after close dialog. Remove info dialog and add info message in view registry.

--- trunk/eirc/src/org/flexpay/eirc/actions/registry/RegistryViewAction.java
+++ trunk/eirc/src/org/flexpay/eirc/actions/registry/RegistryViewAction.java
@@ -3,10 +3,24 @@ package org.flexpay.eirc.actions.registry;
 import org.flexpay.common.persistence.filter.ImportErrorTypeFilter;
 
 public class RegistryViewAction extends org.flexpay.payments.actions.registry.RegistryViewAction {
+    private boolean setupCompleted;
+
+    @Override
+    public String execute() throws Exception {
+        if (setupCompleted) {
+            addActionMessage(getText("eirc.setup_completed"));
+        }
+        return super.execute();
+    }
+
     public ImportErrorTypeFilter getImportErrorTypeFilter() {
 		if (importErrorTypeFilter == null) {
             importErrorTypeFilter = new org.flexpay.eirc.persistence.filters.ImportErrorTypeFilter();
         }
         return importErrorTypeFilter;
 	}
+
+    public void setSetupCompleted(boolean setupCompleted) {
+        this.setupCompleted = setupCompleted;
+    }
 }
