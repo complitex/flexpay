a754ee8af2a60976beadeddec76ea0dd90b8b899
mfedko <mfedko@f554a4e9-133d-0410-ac35-5129da6e6cf8>
2009-11-26 04:17:00 +0000

    FP-983
    dummy master index generation in NopeModificationListener#onCreate method.

--- common/src/org/flexpay/common/persistence/history/impl/NopeModificationListener.java
+++ common/src/org/flexpay/common/persistence/history/impl/NopeModificationListener.java
@@ -1,14 +1,26 @@
 package org.flexpay.common.persistence.history.impl;
 
+import org.flexpay.common.persistence.DataCorrection;
 import org.flexpay.common.persistence.DomainObject;
 import org.flexpay.common.persistence.history.ModificationListener;
+import org.flexpay.common.service.importexport.CorrectionsService;
+import org.flexpay.common.service.importexport.MasterIndexService;
 import org.jetbrains.annotations.NotNull;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Required;
 
 /**
- * Midificatation listener that does nothing
+ * Modification listener that does nothing
+ *
  * @param <T>
  */
 public class NopeModificationListener<T extends DomainObject> implements ModificationListener<T> {
+
+	private Logger log = LoggerFactory.getLogger(getClass());
+
+	private MasterIndexService masterIndexService;
+	private CorrectionsService correctionsService;
 
 	/**
 	 * Notify of new object created
@@ -16,6 +28,20 @@ public class NopeModificationListener<T extends DomainObject> implements Modific
 	 * @param obj New object
 	 */
 	public void onCreate(@NotNull T obj) {
+		// create dummy master index
+		String masterIndex = masterIndexService.getNewMasterIndex(obj);
+		if (masterIndex == null) {
+			log.warn("Cannot create master index for new object");
+			return;
+		}
+
+		try {
+			DataCorrection correction = correctionsService.getStub(
+					masterIndex, obj, masterIndexService.getMasterSourceDescriptionStub());
+			correctionsService.save(correction);
+		} catch (IllegalArgumentException ex) {
+			log.warn("Cannot determine object type code by class {}", obj.getClass());
+		}
 	}
 
 	/**
@@ -33,5 +59,15 @@ public class NopeModificationListener<T extends DomainObject> implements Modific
 	 * @param obj object that was deleted
 	 */
 	public void onDelete(@NotNull T obj) {
+	}
+
+	@Required
+	public void setMasterIndexService(MasterIndexService masterIndexService) {
+		this.masterIndexService = masterIndexService;
+	}
+
+	@Required
+	public void setCorrectionsService(CorrectionsService correctionsService) {
+		this.correctionsService = correctionsService;
 	}
 }
