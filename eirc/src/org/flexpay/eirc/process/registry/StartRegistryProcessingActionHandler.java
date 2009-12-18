f28b910f57487d8d621c56ee95dad1671648c576
pavel.sknar <pavel.sknar@f554a4e9-133d-0410-ac35-5129da6e6cf8>
2009-12-18 13:08:33 +0000

    FP-1031
    Create process Processing DB registry.

---
+++ eirc/src/org/flexpay/eirc/process/registry/StartRegistryProcessingActionHandler.java
@@ -0,0 +1,85 @@
+package org.flexpay.eirc.process.registry;
+
+import org.flexpay.common.exception.FlexPayException;
+import org.flexpay.common.persistence.Stub;
+import org.flexpay.common.persistence.registry.Registry;
+import org.flexpay.common.persistence.registry.workflow.RegistryWorkflowManager;
+import org.flexpay.common.persistence.registry.workflow.TransitionNotAllowed;
+import org.flexpay.common.process.handler.FlexPayActionHandler;
+import org.flexpay.common.service.RegistryService;
+import org.flexpay.eirc.persistence.exchange.ProcessingContext;
+import org.flexpay.eirc.process.registry.error.HandleError;
+import org.flexpay.eirc.service.exchange.ServiceProviderFileProcessorTx;
+import org.springframework.beans.factory.annotation.Required;
+
+import java.util.Map;
+
+public class StartRegistryProcessingActionHandler extends FlexPayActionHandler {
+	public static final String REGISTRY_ID = "registryId";
+	public static final String RECORD_IDS = "recordIds";
+
+	private ServiceProviderFileProcessorTx processorTx;
+	private RegistryService registryService;
+	private RegistryWorkflowManager registryWorkflowManager;
+	private HandleError handleError;
+
+	@Override
+	public String execute2(Map<String, Object> parameters) throws FlexPayException {
+		log.debug("start action");
+		
+		Long registryId = (Long)parameters.get(REGISTRY_ID);
+		if (registryId == null) {
+			log.error("Can not find '{}' in process parameters", REGISTRY_ID);
+			processLog.error("Inner error");
+			return RESULT_ERROR;
+		}
+		Registry registry = registryService.readWithContainers(new Stub<Registry>(registryId));
+		if (registry == null) {
+			log.error("Can not find registry '{}'", registryId);
+			processLog.error("Inner error");
+			return RESULT_ERROR;
+		}
+		try {
+			registryWorkflowManager.startProcessing(registry);
+		} catch (TransitionNotAllowed transitionNotAllowed) {
+			log.error("Inner error", transitionNotAllowed);
+			processLog.error("Inner error");
+			return RESULT_ERROR;
+		}
+
+		ProcessingContext context = new ProcessingContext();
+		context.setRegistry(registry);
+
+		try {
+			processorTx.processHeader(context);
+		} catch (Throwable t) {
+			try {
+				handleError.handleError(t, context);
+			} catch (Exception e) {
+				log.error("Inner error", e);
+			}
+			return RESULT_ERROR;
+		}
+		return RESULT_NEXT;
+	}
+
+	@Required
+	public void setProcessorTx(ServiceProviderFileProcessorTx processorTx) {
+		this.processorTx = processorTx;
+	}
+
+	@Required
+	public void setRegistryService(RegistryService registryService) {
+		this.registryService = registryService;
+	}
+
+	@Required
+	public void setRegistryWorkflowManager(RegistryWorkflowManager registryWorkflowManager) {
+		this.registryWorkflowManager = registryWorkflowManager;
+	}
+
+	@Required
+	public void setHandleError(HandleError handleError) {
+		this.handleError = handleError;
+	}
+}
---
+++ eirc/src/org/flexpay/eirc/process/registry/error/impl/RegistryHandleError.java
@@ -0,0 +1,97 @@
+package org.flexpay.eirc.process.registry.error.impl;
+
+import org.apache.commons.lang.StringUtils;
+import org.flexpay.common.exception.FlexPayException;
+import org.flexpay.common.exception.FlexPayExceptionContainer;
+import org.flexpay.common.persistence.DataSourceDescription;
+import org.flexpay.common.persistence.ImportError;
+import org.flexpay.common.persistence.registry.RegistryRecord;
+import org.flexpay.common.persistence.registry.workflow.RegistryRecordWorkflowManager;
+import org.flexpay.common.persistence.registry.workflow.RegistryWorkflowManager;
+import org.flexpay.common.service.importexport.ClassToTypeRegistry;
+import org.flexpay.eirc.persistence.Consumer;
+import org.flexpay.eirc.persistence.exchange.ProcessingContext;
+import org.flexpay.eirc.process.registry.error.HandleError;
+import org.flexpay.orgs.persistence.Organization;
+import org.flexpay.orgs.service.OrganizationService;
+import org.flexpay.payments.persistence.EircRegistryProperties;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Required;
+
+public class RegistryHandleError implements HandleError {
+	private Logger log = LoggerFactory.getLogger(getClass());
+
+	private RegistryWorkflowManager registryWorkflowManager;
+	private RegistryRecordWorkflowManager recordWorkflowManager;
+	private ClassToTypeRegistry classToTypeRegistry;
+
+	private OrganizationService organizationService;
+
+	@Override
+	@SuppressWarnings ({"ThrowableResultOfMethodCallIgnored"})
+	public void handleError(Throwable t, ProcessingContext context) throws Exception {
+		String code = "eirc.error_code.unknown_error";
+		if (t instanceof FlexPayExceptionContainer) {
+			t = ((FlexPayExceptionContainer) t).getFirstException();
+		}
+		if (t instanceof FlexPayException) {
+			FlexPayException ex = (FlexPayException) t;
+			code = StringUtils.isNotEmpty(ex.getErrorKey()) ?
+				   ex.getErrorKey() : "eirc.error_code.error_with_processing_container";
+		}
+
+		log.warn("Failed processing registry", t);
+
+		ImportError error = new ImportError();
+		error.setErrorId(code);
+		EircRegistryProperties props = (EircRegistryProperties) context.getRegistry().getProperties();
+		Organization sender = organizationService.readFull(props.getSenderStub());
+		if (sender == null) {
+			throw new IllegalStateException("Cannot find sender organization: #" + props.getSenderStub().getId());
+		}
+		DataSourceDescription sd = sender.getDataSourceDescription();
+		error.setSourceDescription(sd);
+
+		error.setDataSourceBean("consumersDataSource");
+
+		if (context.getCurrentRecord() == null) {
+			error.setSourceObjectId(String.valueOf(context.getRegistry().getId()));
+		} else {
+			error.setSourceObjectId(String.valueOf(context.getCurrentRecord().getId()));
+		}
+		error.setObjectType(classToTypeRegistry.getType(Consumer.class));
+
+		// also set error for operation records update
+		for (RegistryRecord record : context.getOperationRecords()) {
+			if (record != context.getCurrentRecord()) {
+				recordWorkflowManager.setNextErrorStatus(record, error);
+			}
+		}
+
+		// mark registry having error if processing header
+		if (context.getOperationRecords().isEmpty()) {
+			registryWorkflowManager.setNextErrorStatus(context.getRegistry(), error);
+		}
+	}
+
+	@Required
+	public void setRegistryWorkflowManager(RegistryWorkflowManager registryWorkflowManager) {
+		this.registryWorkflowManager = registryWorkflowManager;
+	}
+
+	@Required
+	public void setRecordWorkflowManager(RegistryRecordWorkflowManager recordWorkflowManager) {
+		this.recordWorkflowManager = recordWorkflowManager;
+	}
+
+	@Required
+	public void setClassToTypeRegistry(ClassToTypeRegistry classToTypeRegistry) {
+		this.classToTypeRegistry = classToTypeRegistry;
+	}
+
+	@Required
+	public void setOrganizationService(OrganizationService organizationService) {
+		this.organizationService = organizationService;
+	}
+}
