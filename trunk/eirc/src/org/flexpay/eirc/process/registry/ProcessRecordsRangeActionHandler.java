b798d16c910fddc6f7212d87c81d84939ed8552e
pavel.sknar <pavel.sknar@f554a4e9-133d-0410-ac35-5129da6e6cf8>
2010-01-28 04:11:37 +0000

    FP-1000
    add log: current record status for failed processed record

--- trunk/eirc/src/org/flexpay/eirc/process/registry/ProcessRecordsRangeActionHandler.java
+++ trunk/eirc/src/org/flexpay/eirc/process/registry/ProcessRecordsRangeActionHandler.java
@@ -73,6 +73,7 @@ public class ProcessRecordsRangeActionHandler extends FlexPayActionHandler {
 				processorTx.prepareRecordUpdates(context);
 			} catch (Throwable t) {
 				try {
+					log.debug("Try set next status for record with Id {}, current status {}", record.getId(), record.getRecordStatus());
 					handleError.handleError(t, context);
 				} catch (Exception e) {
 					log.error("Inner error. Record Id is: " + record.getId(), e);
