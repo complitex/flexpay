c1636742aff40bd54378de4a5d1ce109dce1f690
pavel.sknar <pavel.sknar@f554a4e9-133d-0410-ac35-5129da6e6cf8>
2010-03-15 07:50:26 +0000

    FP-1076
    update last modified version

--- common/etc/sql/update/2010_03_11.sql
+++ common/etc/sql/update/2010_03_11.sql
@@ -1,2 +1,4 @@
 alter table common_users_tbl
 		add column first_name varchar(255) comment 'First user name';
+INSERT INTO common_version_tbl (last_modified_date, date_version) VALUES ('2010-03-11', 0);
+
