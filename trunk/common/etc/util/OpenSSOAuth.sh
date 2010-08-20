1decfd24815f1a14e2f4f1411360c66ece5e0832
AlexuyT <AlexuyT@f554a4e9-133d-0410-ac35-5129da6e6cf8>
2010-08-20 06:54:37 +0000

    Обновлены используемые библиотеки в модуле http_tester, также обновлены библиотеки для использования htmlUnit'а, а также частично обновлены библиотеки из flexpay'я, точнее, apache-common-либы.
    Изменено местоположение конфиг-файла для модуля http_tester

--- trunk/common/etc/util/OpenSSOAuth.sh
+++ trunk/common/etc/util/OpenSSOAuth.sh
@@ -2,8 +2,8 @@
 
 LIB=../../lib/htmlunit
 LIB2=../../lib/commons
-CP=$LIB/cssparser-0.9.5.jar:$LIB/htmlunit-2.6.jar:$LIB/htmlunit-core-js-2.6.jar:$LIB/nekohtml-1.9.13.jar:$LIB/sac-1.3.jar:$LIB/serializer-2.7.1.jar:$LIB/xalan-2.7.1.jar:$LIB/xercesImpl-2.9.1.jar
-CP=$CP:$LIB2/commons-codec-1.3.jar:$LIB2/commons-io-1.4.jar:$LIB2/commons-logging-1.1.1.jar:$LIB2/commons-lang-2.4.jar:$LIB2/commons-httpclient-3.1.jar:$LIB2/commons-collections-3.2.1.jar
+CP=$LIB/apache-mime4j-0.6.jar:$LIB/cssparser-0.9.5.jar:$LIB/htmlunit-2.8.jar:$LIB/htmlunit-core-js-2.8.jar:$LIB/httpclient-4.0.1.jar:$LIB/httpcore-4.0.1.jar:$LIB/httpmime-4.0.1.jar:$LIB/nekohtml-1.9.14.jar:$LIB/sac-1.3.jar:$LIB/serializer-2.7.1.jar:$LIB/xalan-2.7.1.jar:$LIB/xercesImpl-2.9.1.jar
+CP=$CP:$LIB2/commons-codec-1.4.jar:$LIB2/commons-io-1.4.jar:$LIB2/commons-logging-1.1.1.jar:$LIB2/commons-lang-2.5.jar:$LIB2/commons-collections-3.2.1.jar
 
 echo $CP
 
