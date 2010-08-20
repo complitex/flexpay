2673b9dbcb8e21fe241e0f8f9d48f6eaec1f6ee3
AlexuyT <AlexuyT@f554a4e9-133d-0410-ac35-5129da6e6cf8>
2010-08-20 06:54:37 +0000

    Обновлены используемые библиотеки в модуле http_tester, также обновлены библиотеки для использования htmlUnit'а, а также частично обновлены библиотеки из flexpay'я, точнее, apache-common-либы.
    Изменено местоположение конфиг-файла для модуля http_tester

--- http_tester/src/org/flexpay/httptester/Client.java
+++ http_tester/src/org/flexpay/httptester/Client.java
@@ -1,11 +1,13 @@
 package org.flexpay.httptester;
 
+import org.apache.http.HttpResponse;
 import org.apache.http.client.HttpClient;
 import org.apache.http.impl.client.DefaultHttpClient;
 import org.flexpay.httptester.request.*;
 
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.Date;
 import java.util.Properties;
 
 public class Client {
@@ -15,19 +17,38 @@ public class Client {
         Properties props = loadProperties();
         HttpClient httpClient = new DefaultHttpClient();
 
-//        executeRequest(new GetQuittanceDebtInfoRequest(props), httpClient);
-//        executeRequest(new GetDebtInfoRequest(props), httpClient);
-        executeRequest(new PayDebtRequest(props), httpClient);
-//        executeRequest(new ReversalPayRequest(props), httpClient);
-//        executeRequest(new RegistryCommentRequest(props), httpClient);
-//        executeRequest(new GetRegistryListRequest(props), httpClient);
-//        executeRequest(new GetServiceListRequest(props), httpClient);
-
+        if ("true".equals(props.getProperty("request.getQuittanceDebtInfo"))) {
+            executeRequest(new GetQuittanceDebtInfoRequest(props), httpClient);
+        }
+        if ("true".equals(props.getProperty("request.getDebtInfo"))) {
+            executeRequest(new GetDebtInfoRequest(props), httpClient);
+        }
+        if ("true".equals(props.getProperty("request.payDebt"))) {
+            executeRequest(new PayDebtRequest(props), httpClient);
+        }
+        if ("true".equals(props.getProperty("request.reversalPay"))) {
+            executeRequest(new ReversalPayRequest(props), httpClient);
+        }
+        if ("true".equals(props.getProperty("request.registryComment"))) {
+            executeRequest(new RegistryCommentRequest(props), httpClient);
+        }
+        if ("true".equals(props.getProperty("request.getRegistryList"))) {
+            executeRequest(new GetRegistryListRequest(props), httpClient);
+        }
+        if ("true".equals(props.getProperty("request.getServiceList"))) {
+            executeRequest(new GetServiceListRequest(props), httpClient);
+        }
     }
 
     private static void executeRequest(Request<?> request, HttpClient httpClient) throws Exception {
 
-        request.parseResponse(httpClient.execute(request.generateRequest()));
+        System.out.println("Executing request...");
+        Date startTime = new Date();
+        HttpResponse httpResponse = httpClient.execute(request.generateRequest());
+        Date finishTime = new Date();
+        System.out.println("Execute request elapsed time: " + (finishTime.getTime() - startTime.getTime()) + "ms");
+
+        request.parseResponse(httpResponse);
         request.checkResponseSignature();
 
 /*
@@ -42,7 +63,7 @@ public class Client {
 
 	private static Properties loadProperties() throws IOException {
 		Properties props = new Properties();
-        FileReader fr = new FileReader("org/flexpay/httptester/config.properties");
+        FileReader fr = new FileReader("config.properties");
         try {
             props.load(fr);
         } finally {
