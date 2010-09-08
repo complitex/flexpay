bf386b82b08cfde3628b60def3970421542bc737
pavel.sknar <pavel.sknar@f554a4e9-133d-0410-ac35-5129da6e6cf8>
2010-09-08 06:04:12 +0000

    Override equals() and hashCode()

--- trunk/eirc/src/org/flexpay/eirc/persistence/consumer/ConsumerAttribute.java
+++ trunk/eirc/src/org/flexpay/eirc/persistence/consumer/ConsumerAttribute.java
@@ -73,6 +73,28 @@ public class ConsumerAttribute extends TemporalValueObject implements Comparable
 				.append("consumer-id", consumer == null ? null : consumer.getId());
 	}
 
+	@Override
+	public boolean equals(Object o) {
+		if (this == o) return true;
+		if (!(o instanceof ConsumerAttribute)) return false;
+		if (!super.equals(o)) return false;
+
+		ConsumerAttribute that = (ConsumerAttribute) o;
+
+		if (consumer != null ? !consumer.equals(that.consumer) : that.consumer != null) return false;
+		if (type != null ? !type.equals(that.type) : that.type != null) return false;
+
+		return true;
+	}
+
+	@Override
+	public int hashCode() {
+		int result = super.hashCode();
+		result = 31 * result + (type != null ? type.hashCode() : 0);
+		result = 31 * result + (consumer != null ? consumer.hashCode() : 0);
+		return result;
+	}
+
 	public ConsumerAttribute copy() {
 
 		ConsumerAttribute attribute = new ConsumerAttribute();
