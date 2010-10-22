80f8974069126d166a23924bc72ef16ba42fabaa
pavel.sknar <pavel.sknar@f554a4e9-133d-0410-ac35-5129da6e6cf8>
2010-10-22 07:50:07 +0000

    FP-1139 Update database version

--- trunk/common/etc/sql/create_db.sql
+++ trunk/common/etc/sql/create_db.sql
@@ -13,4 +13,4 @@ CREATE TABLE common_version_tbl (
 	date_version int NOT NULL
 );
 
-INSERT INTO common_version_tbl (last_modified_date, date_version) VALUES ('2010-09-13', 0);
+INSERT INTO common_version_tbl (last_modified_date, date_version) VALUES ('2010-10-21', 0);
