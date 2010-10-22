2676c1c5cbe032052f55e9b2619c23de4a0e4df7
pavel.sknar <pavel.sknar@f554a4e9-133d-0410-ac35-5129da6e6cf8>
2010-10-22 07:50:07 +0000

    FP-1139 Update database version

--- common/etc/sql/update/2010_10_21.sql
+++ common/etc/sql/update/2010_10_21.sql
@@ -7,3 +7,5 @@ alter table common_certificates_tbl
 	add constraint FK_common_certificates_tbl_user_preference_id
 	foreign key (user_preference_id)
 	references common_users_tbl (id);
+
+update common_version_tbl set last_modified_date='2010-10-21', date_version=0;
