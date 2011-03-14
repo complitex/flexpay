ed5c7eb65991bc2c2b8344f28247383f0f161eab
alexuyt <alexuyt@f554a4e9-133d-0410-ac35-5129da6e6cf8>
2011-03-14 07:38:45 +0000

    FP-1174 (продолжение)
    Обновление библиотек struts до версии 2.2.1
    Обновление библиотек tiles до версии 2.2.2
    Корректировка синтаксиса, в соответсвии с новыми библиотеками
    Переименование пакета actions в action
    Удаление лишних файлов sun-web.xml
    Переименование конфигурационных файлов с заменой символа "_" на "-"

--- common/src/org/apache/struts2/dispatcher/StreamResult.java
+++ common/src/org/apache/struts2/dispatcher/StreamResult.java
@@ -1,5 +1,5 @@
 /*
- * $Id: StreamResult.java 570513 2007-08-28 18:14:00Z jholmes $
+ * $Id: StreamResult.java 817366 2009-09-21 18:40:56Z wesw $
  *
  * Licensed to the Apache Software Foundation (ASF) under one
  * or more contributor license agreements.  See the NOTICE file
@@ -18,6 +18,7 @@
  * specific language governing permissions and limitations
  * under the License.
  */
+
 package org.apache.struts2.dispatcher;
 
 import java.io.InputStream;
@@ -25,10 +26,10 @@ import java.io.OutputStream;
 
 import javax.servlet.http.HttpServletResponse;
 
+import com.opensymphony.xwork2.ActionInvocation;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
-
-import com.opensymphony.xwork2.ActionInvocation;
+import com.opensymphony.xwork2.util.ValueStack;
 
 /**
  * <!-- START SNIPPET: description -->
@@ -50,9 +51,9 @@ import com.opensymphony.xwork2.ActionInvocation;
  * <li><b>contentLength</b> - the stream length in bytes (the browser displays a
  * progress bar).</li>
  *
- * <li><b>contentDispostion</b> - the content disposition header value for
+ * <li><b>contentDisposition</b> - the content disposition header value for
  * specifing the file name (default = <code>inline</code>, values are typically
- * <i>filename="document.pdf"</i>.</li>
+ * <i>attachment;filename="document.pdf"</i>.</li>
  *
  * <li><b>inputName</b> - the name of the InputStream property from the chained
  * action (default = <code>inputStream</code>).</li>
@@ -60,7 +61,17 @@ import com.opensymphony.xwork2.ActionInvocation;
  * <li><b>bufferSize</b> - the size of the buffer to copy from input to output
  * (default = <code>1024</code>).</li>
  *
+ * <li><b>allowCaching</b> if set to 'false' it will set the headers 'Pragma' and 'Cache-Control'
+ * to 'no-cahce', and prevent client from caching the content. (default = <code>true</code>)
+ *
+ * <li><b>contentCharSet</b> if set to a string, ';charset=value' will be added to the
+ * content-type header, where value is the string set. If set to an expression, the result
+ * of evaluating the expression will be used. If not set, then no charset will be set on
+ * the header</li>
  * </ul>
+ * 
+ * <p>These parameters can also be set by exposing a similarly named getter method on your Action.  For example, you can
+ * provide <code>getContentType()</code> to override that parameter for the current action.</p>
  *
  * <!-- END SNIPPET: params -->
  *
@@ -70,7 +81,7 @@ import com.opensymphony.xwork2.ActionInvocation;
  * &lt;result name="success" type="stream"&gt;
  *   &lt;param name="contentType"&gt;image/jpeg&lt;/param&gt;
  *   &lt;param name="inputName"&gt;imageStream&lt;/param&gt;
- *   &lt;param name="contentDisposition"&gt;filename="document.pdf"&lt;/param&gt;
+ *   &lt;param name="contentDisposition"&gt;attachment;filename="document.pdf"&lt;/param&gt;
  *   &lt;param name="bufferSize"&gt;1024&lt;/param&gt;
  * &lt;/result&gt;
  * <!-- END SNIPPET: example --></pre>
@@ -80,16 +91,18 @@ public class StreamResult extends StrutsResultSupport {
 
     private static final long serialVersionUID = -1468409635999059850L;
 
-    protected static final Log log = LogFactory.getLog(StreamResult.class);
+    protected static final Log LOG = LogFactory.getLog(StreamResult.class);
 
     public static final String DEFAULT_PARAM = "inputName";
 
     protected String contentType = "text/plain";
     protected String contentLength;
     protected String contentDisposition = "inline";
+    protected String contentCharSet ;
     protected String inputName = "inputStream";
     protected InputStream inputStream;
     protected int bufferSize = 1024;
+    protected boolean allowCaching = true;
 
     public StreamResult() {
         super();
@@ -98,6 +111,24 @@ public class StreamResult extends StrutsResultSupport {
     public StreamResult(InputStream in) {
         this.inputStream = in;
     }
+
+     /**
+     * @return Returns the whether or not the client should be requested to allow caching of the data stream.
+     */
+    public boolean getAllowCaching() {
+        return allowCaching;
+    }
+
+    /**
+     * Set allowCaching to <tt>false</tt> to indicate that the client should be requested not to cache the data stream.
+     * This is set to <tt>false</tt> by default
+     *
+     * @param allowCaching Enable caching.
+     */
+    public void setAllowCaching(boolean allowCaching) {
+        this.allowCaching = allowCaching;
+    }
+
 
     /**
      * @return Returns the bufferSize.
@@ -156,6 +187,20 @@ public class StreamResult extends StrutsResultSupport {
     }
 
     /**
+     * @return Returns the charset specified by the user
+     */
+    public String getContentCharSet() {
+        return contentCharSet;
+    }
+
+    /**
+     * @param contentCharSet the charset to use on the header when sending the stream
+     */
+    public void setContentCharSet(String contentCharSet) {
+        this.contentCharSet = contentCharSet;
+    }
+
+    /**
      * @return Returns the inputName.
      */
     public String getInputName() {
@@ -170,9 +215,12 @@ public class StreamResult extends StrutsResultSupport {
     }
 
     /**
-     * @see StrutsResultSupport#doExecute(String, com.opensymphony.xwork2.ActionInvocation)
+     * @see org.apache.struts2.dispatcher.StrutsResultSupport#doExecute(java.lang.String, com.opensymphony.xwork2.ActionInvocation)
      */
     protected void doExecute(String finalLocation, ActionInvocation invocation) throws Exception {
+
+        // Override any parameters using values on the stack
+        resolveParamsFromStack(invocation.getStack(), invocation);
 
         OutputStream oOutput = null;
 
@@ -185,7 +233,7 @@ public class StreamResult extends StrutsResultSupport {
             if (inputStream == null) {
                 String msg = ("Can not find a java.io.InputStream with the name [" + inputName + "] in the invocation stack. " +
                     "Check the <param name=\"inputName\"> tag specified for this action.");
-                log.error(msg);
+                LOG.error(msg);
                 throw new IllegalArgumentException(msg);
             }
 
@@ -193,7 +241,12 @@ public class StreamResult extends StrutsResultSupport {
             HttpServletResponse oResponse = (HttpServletResponse) invocation.getInvocationContext().get(HTTP_RESPONSE);
 
             // Set the content type
-            oResponse.setContentType(conditionalParse(contentType, invocation));
+            if (contentCharSet != null && ! contentCharSet.equals("")) {
+                oResponse.setContentType(conditionalParse(contentType, invocation)+";charset="+contentCharSet);
+            }
+            else {
+                oResponse.setContentType(conditionalParse(contentType, invocation));
+            }
 
             // Set the content length
             if (contentLength != null) {
@@ -206,37 +259,37 @@ public class StreamResult extends StrutsResultSupport {
                     }
                 }
                 catch(NumberFormatException e) {
-                    log.warn("failed to recongnize "+_contentLength+" as a number, contentLength header will not be set", e);
+                    LOG.warn("failed to recongnize "+_contentLength+" as a number, contentLength header will not be set", e);
                 }
             }
 
             // Set the content-disposition
             if (contentDisposition != null) {
-                oResponse.addHeader("Content-disposition", conditionalParse(contentDisposition, invocation));
+                oResponse.addHeader("Content-Disposition", conditionalParse(contentDisposition, invocation));
             }
 
-			// ie6 bugfix see http://www.coderanch.com/t/432713/Struts/Struts-Files-DownLoad-Streaming-as
-			oResponse.setHeader("Cache-Control", "no-cache");
-			oResponse.setHeader("Expires", "0");
-			oResponse.setHeader("Cache-Control", "must-revalidate, post-check=0, pre-check=0");
-			oResponse.setHeader("Pragma", "public");
+            // Set the cache control headers if neccessary
+            if (!allowCaching) {
+                oResponse.addHeader("Pragma", "no-cache");
+                oResponse.addHeader("Cache-Control", "no-cache");
+            }
 
             // Get the outputstream
             oOutput = oResponse.getOutputStream();
 
-            if (log.isDebugEnabled()) {
-                log.debug("Streaming result [" + inputName + "] type=[" + contentType + "] length=[" + contentLength +
-                    "] content-disposition=[" + contentDisposition + "]");
+            if (LOG.isDebugEnabled()) {
+                LOG.debug("Streaming result [" + inputName + "] type=[" + contentType + "] length=[" + contentLength +
+                    "] content-disposition=[" + contentDisposition + "] charset=[" + contentCharSet + "]");
             }
 
             // Copy input to output
-            log.debug("Streaming to output buffer +++ START +++");
+            LOG.debug("Streaming to output buffer +++ START +++");
             byte[] oBuff = new byte[bufferSize];
             int iSize;
             while (-1 != (iSize = inputStream.read(oBuff))) {
                 oOutput.write(oBuff, 0, iSize);
             }
-            log.debug("Streaming to output buffer +++ END +++");
+            LOG.debug("Streaming to output buffer +++ END +++");
 
             // Flush
             oOutput.flush();
@@ -244,6 +297,45 @@ public class StreamResult extends StrutsResultSupport {
         finally {
             if (inputStream != null) inputStream.close();
             if (oOutput != null) oOutput.close();
+        }
+    }
+
+    /**
+     * Tries to lookup the parameters on the stack.  Will override any existing parameters
+     *
+     * @param stack The current value stack
+     */
+    protected void resolveParamsFromStack(ValueStack stack, ActionInvocation invocation) {
+        String disposition = stack.findString("contentDisposition");
+        if (disposition != null) {
+            setContentDisposition(disposition);
+        }
+
+        String contentType = stack.findString("contentType");
+        if (contentType != null) {
+            setContentType(contentType);
+        }
+
+        String inputName = stack.findString("inputName");
+        if (inputName != null) {
+            setInputName(inputName);
+        }
+
+        String contentLength = stack.findString("contentLength");
+        if (contentLength != null) {
+            setContentLength(contentLength);
+        }
+
+        Integer bufferSize = (Integer) stack.findValue("bufferSize", Integer.class);
+        if (bufferSize != null) {
+            setBufferSize(bufferSize.intValue());
+        }
+
+        if (contentCharSet != null ) {
+            contentCharSet = conditionalParse(contentCharSet, invocation);
+        }
+        else {
+            contentCharSet = stack.findString("contentCharSet");
         }
     }
 
