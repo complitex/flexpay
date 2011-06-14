91ce417dd8d6ad27421599f999f6c879efc767c4
pavel.sknar <pavel.sknar@f554a4e9-133d-0410-ac35-5129da6e6cf8>
2011-06-14 05:57:38 +0000

    FP-1182 Migrate jbpm from 3.2 to 5.1 version. Init jbpm session in database

---
+++ common/etc/sql/update/2011_06_14.sql
@@ -0,0 +1,2 @@
+INSERT INTO `SessionInfo` VALUES (1,now(),'¬í\0wW\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\r\0MAIN\0\0\0\r\0MAIN\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0',now(),2);
+update common_version_tbl set last_modified_date='2011-06-14', date_version=0;
