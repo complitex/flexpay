accd7aa4710fbe117ca5fda0da1d51536cd71a8f
pavel.sknar@gmail.com <pavel.sknar@gmail.com@f554a4e9-133d-0410-ac35-5129da6e6cf8>
2013-10-21 08:57:23 +0000

    sql and ldap scripts 1. Workplace 2. Local Admin role

--- payments/etc/sql/flexpay_payments.ddl.sql
+++ payments/etc/sql/flexpay_payments.ddl.sql
@@ -1207,6 +1207,23 @@
         primary key (id)
     ) comment='Services';
 
+    create table payments_operation_action_logs_tbl (
+        id bigint not null auto_increment comment 'Primary key',
+        version integer not null comment 'Optimistic lock version',
+        user_name varchar(255) not null comment 'User name',
+        action_time datetime not null comment 'Action time',
+        action integer not null comment 'Action (1 - search by address, 2 - search by quittance number, 3 - search by EIRC account, 4 - print quittance)',
+        action_string varchar(300) comment 'Action string',
+        cashbox_id bigint not null comment 'Cashbox reference',
+        primary key (id)
+    ) comment='Operation action logs table';
+
+    alter table payments_operation_action_logs_tbl
+        add index FK_payments_operation_action_logs_tbl_cashbox_id (cashbox_id),
+        add constraint FK_payments_operation_action_logs_tbl_cashbox_id
+        foreign key (cashbox_id)
+        references orgs_cashboxes_tbl (id);
+
     create index indx_value on ab_apartment_numbers_tbl (value);
 
     alter table ab_apartment_numbers_tbl 
